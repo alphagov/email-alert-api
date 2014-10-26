1c6aac93113706ec0ac497ffde9e425be9b956aa
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-10-26 11:09:37 +0000

    Add finding subscriber lists by tags.

--- app/models/subscriber_list.rb
+++ app/models/subscriber_list.rb
@@ -1,6 +1,14 @@
 require 'json'
 
 class SubscriberList < ActiveRecord::Base
+  def self.with_at_least_one_tag_of_each_type(tags:)
+    where("Array[:tag_keys] @> akeys(tags)", tag_keys: tags.keys).select do |list|
+      list.tags.all? do |tag_type, tag_array|
+        (tags[tag_type] & tag_array).count > 0
+      end
+    end
+  end
+
   def tags
     @_tags ||= super.inject({}) do |hash, (tag_type, tags_json)|
       hash.merge(tag_type.to_sym => JSON.parse(tags_json))
--- spec/models/subscriber_list_spec.rb
+++ spec/models/subscriber_list_spec.rb
@@ -1,6 +1,41 @@
 require "rails_helper"
 
 RSpec.describe SubscriberList, type: :model do
+  describe ".find_with_at_least_one_tag_of_each_type" do
+    before do
+      @list1 = FactoryGirl.create(:subscriber_list, tags: {
+        topics: ["oil-and-gas/licensing"],
+        organisations: ["environment-agency", "hm-revenue-customs"]
+      })
+
+      @list2 = FactoryGirl.create(:subscriber_list, tags: {
+        topics: ["business-tax/vat", "oil-and-gas/licensing"]
+      })
+
+      FactoryGirl.create(:subscriber_list, tags: {
+        topics: ["environmental-management/boating"],
+      })
+    end
+
+    it "finds lists where at least one value of each tag in the subscription is present in the document" do
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        topics: ["oil-and-gas/licensing"]
+      })
+      expect(lists).to eq([@list2])
+
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        topics: ["business-tax/vat"],
+      })
+      expect(lists).to eq([@list2])
+
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        topics: ["oil-and-gas/licensing"],
+        organisations: ["environment-agency"]
+      })
+      expect(lists.to_set).to eq([@list1, @list2].to_set)
+    end
+  end
+
   describe "#tags" do
     it "deserializes the tag arrays" do
       list = FactoryGirl.create(:subscriber_list, tags: {
