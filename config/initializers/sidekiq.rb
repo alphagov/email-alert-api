f2486cbd4dddfe3c32cdf473106cec28bf784977
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-10-26 18:29:33 +0000

    Add a sidekiq config, loading redis & errbit.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+Sidekiq.configure_server do |config|
+  config.redis = EmailAlertAPI.config.redis_config
+  config.error_handlers << Proc.new {|ex, context_hash| Airbrake.notify(ex, context_hash) }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = EmailAlertAPI.config.redis_config
+end
