3f90b961c4c1e4e86ab0aac4aadf7520636f2faa
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-10-28 13:03:38 +0000

    Add a stricter `with_all_matching_keys` method
    Paired with @rboulton.
    
    We now check that we have all the keys rather than an overlapping subset
    of them, and use this in `where_tags_equal`.
    
    Also add passing tests of
    SubscriberList.with_at_least_one_tag_of_each_type to narrow down its
    behaviour a bit more, and tests of SubscriberList.where_tags_equal,
    which fail in some cases.

--- spec/models/subscriber_list_spec.rb
+++ spec/models/subscriber_list_spec.rb
@@ -34,19 +34,54 @@ RSpec.describe SubscriberList, type: :model do
       })
       expect(lists.to_set).to eq([@list1, @list2].to_set)
     end
+
+    it "finds lists where all the tag types in the subscription have a value present, even those there are other tag types in the document" do
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        topics: ["oil-and-gas/licensing"],
+        another_tag_thats_not_part_of_the_subscription: ["elephants"],
+      })
+      expect(lists).to eq([@list2])
+    end
+
+    it "finds lists where all the tag types in the subscription have a value present, even when they have other values which aren't present " do
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        topics: ["oil-and-gas/licensing", "elephants"],
+      })
+      expect(lists).to eq([@list2])
+    end
+
+    it "doesn't return lists which have no tag types present in the document" do
+      lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: {
+        another_tag_thats_not_part_of_any_subscription: ["elephants"],
+      })
+      expect(lists).to eq([])
+    end
   end
 
   describe ".where_tags_equal(tag_hash)" do
-    it "finds exact matches" do
-      list = FactoryGirl.create(:subscriber_list, tags: {
+    before do
+      @list = FactoryGirl.create(:subscriber_list, tags: {
         topics: ["oil-and-gas/licensing"],
         organisations: ["environment-agency", "hm-revenue-customs"]
       })
-
-      found_lists = SubscriberList.where_tags_equal({topics: ["oil-and-gas/licensing"],organisations: ["environment-agency", "hm-revenue-customs"]})
-
-      expect(found_lists).to eq([list])
     end
+
+    it "requires all tag types in the document to be present in the list" do
+      found_lists = SubscriberList.where_tags_equal({
+        topics: ["oil-and-gas/licensing"],
+      })
+      expect(found_lists).to eq([])
+    end
+
+    it "requires all tag types in the list to be present in the document" do
+      found_lists = SubscriberList.where_tags_equal({
+        topics: ["oil-and-gas/licensing"],
+        organisations: ["environment-agency", "hm-revenue-customs"],
+        other_tag_type: ["foo"],
+      })
+      expect(found_lists).to eq([])
+    end
+
   end
 
   describe "#tags" do
