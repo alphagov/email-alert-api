7982fb3476e5d3c4558ce992855e48e9102384a4
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-10-28 10:23:25 +0000

    Fix badly generated index name in generated schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,6 @@ ActiveRecord::Schema.define(version: 20141025105641) do
     t.datetime "updated_at"
   end
 
-  add_index "subscriber_lists", ["tags"], name: "tags_index_for_subtree", using: :gin
+  add_index "subscriber_lists", ["tags"], name: "index_subscriber_lists_on_tags", using: :gin
 
 end
