c50e7d55e903f80841f5dedf1989a3e5cf7ae66e
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-11-04 09:14:16 +0000

    Generate XML with nokogiri not string substitution
    The XML messages to be sent to govdelivery were being generated by
    performing string interpolation with some template messages.  This was
    prone to content injection, and also liable to generate invalid XML.
    
    Using nokogiri's XML builder should more reliably generate valid XML,
    and the code to use it is pretty clear too.
    
    Story: https://www.pivotaltracker.com/story/show/81921620

--- app/services/gov_delivery/client.rb
+++ app/services/gov_delivery/client.rb
@@ -4,11 +4,11 @@ module GovDelivery
       @options = options
     end
 
-    def create_topic(attributes)
+    def create_topic(name)
       parse_topic_response(
         post_xml(
           "topics.xml",
-          create_topic_xml(attributes),
+          create_topic_xml(name),
         )
       )
     end
@@ -41,51 +41,44 @@ module GovDelivery
       end
     end
 
-    def post_xml(path, params)
+    def post_xml(path, body)
       http_client.post(
         path,
-        params,
+        body,
         content_type: "application/xml",
       )
     end
 
-    def create_topic_xml(attributes)
-      # TODO Write a spec to prevent content injection
-      %{
-        <topic>
-          <name>%{name}</name>
-          <short-name>%{name}</short-name>
-          <visibility>Unlisted</visibility>
-          <pagewatch-enabled type="boolean">false</pagewatch-enabled>
-          <rss-feed-url nil="true"></rss-feed-url>
-          <rss-feed-title nil="true"></rss-feed-title>
-          <rss-feed-description nil="true"></rss-feed-description>
-        </topic>
-      } % attributes
+    def create_topic_xml(name)
+      Nokogiri::XML::Builder.new { |xml|
+        xml.topic {
+          xml.name name
+          xml.send(:'short-name', name)
+          xml.visibility 'Unlisted'
+          xml.send(:'pagewatch-enabled', "false", type: :boolean)
+          xml.send(:'rss-feed-url', nil: :true)
+          xml.send(:'rss-feed-title', nil: :true)
+          xml.send(:'rss-feed-description', nil: :true)
+        }
+      }.to_xml
     end
 
     def send_bulletin_xml(topic_ids, subject, body)
-      topics = topic_ids
-        .map { |id| topic_template % { id: id } }
-        .join("\n")
-
-      %{
-        <bulletin>
-          <subject>%{subject}</subject>
-          <body><![CDATA[%{body}]]></body>
-          <topics type='array'>
-            %{topics}
-          </topics>
-        </bulletin>
-       } % { subject: subject, body: body, topics: topics }
-    end
-
-    def topic_template
-      %{
-          <topic>
-            <code>%{id}</code>
-          </topic>
-      }
+      Nokogiri::XML::Builder.new { |xml|
+        xml.bulletin {
+          xml.subject subject
+          xml.body {
+            xml.cdata body
+          }
+          xml.topics(type: 'array') {
+            topic_ids.each { |id|
+              xml.topic {
+                xml.code id
+              }
+            }
+          }
+        }
+      }.to_xml
     end
 
     def parse_topic_response(response)
