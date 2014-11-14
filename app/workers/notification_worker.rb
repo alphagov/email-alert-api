a5eef1bca2b490bccd57225ac425ca290eec8c1c
James Darling <james@abscond.org>
2014-11-14 13:21:54 +0000

    Slightly more descriptive logging on notification worker

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -8,7 +8,12 @@ class NotificationWorker
 
     lists = SubscriberList.with_at_least_one_tag_of_each_type(tags: notification[:tags])
 
-    Rails.logger.info "Passing email '#{notification[:subject]}' to #{lists.count} lists in GovDelivery [#{lists.map(&:gov_delivery_id).join(', ')}]"
+    Rails.logger.info "--- Sending email to GovDelivery ---"
+    Rails.logger.info "subject: '#{notification[:subject]}'"
+    Rails.logger.info "tags: '#{notification[:tags]}'"
+    Rails.logger.info "matched #{lists.count} lists in GovDelivery: [#{lists.map(&:gov_delivery_id).join(', ')}]"
+    Rails.logger.info "notification_json: #{notification_json}"
+    Rails.logger.info "--- End email to GovDelivery ---"
 
     EmailAlertAPI.services(:gov_delivery).send_bulletin(
       lists.map(&:gov_delivery_id),
