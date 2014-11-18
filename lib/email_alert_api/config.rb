cf527fee91d5779fbe75a3d410e0a81fdb7242f2
Tom Russell <tom@cishub.co.uk>
2014-11-18 11:14:45 +0000

    Use username and password from environment variables if present.
    Otherwise, use those from the yml file. This allows overriding with real
    govdelivery credentials without the risk of committing them.

--- lib/email_alert_api/config.rb
+++ lib/email_alert_api/config.rb
@@ -11,14 +11,27 @@ module EmailAlertAPI
     end
 
     def gov_delivery
-      all_configs = YAML.load(File.open(app_root+"config/gov_delivery.yml"))
-      environment_config = all_configs.fetch(@environment)
-
       @gov_delivery ||= environment_config.symbolize_keys.freeze
     end
 
     def redis_config
       YAML.load(File.open(app_root+"config/redis.yml")).symbolize_keys
+    end
+
+  private
+    def environment_config
+      all_configs = YAML.load(File.open(app_root+"config/gov_delivery.yml"))
+
+      all_configs.fetch(@environment).tap do |env_config|
+        env_config.merge!(environment_credentials) if ENV["GOVDELIVERY_USERNAME"]
+      end
+    end
+
+    def environment_credentials
+      {
+        "username" => ENV["GOVDELIVERY_USERNAME"],
+        "password" => ENV["GOVDELIVERY_PASSWORD"],
+      }
     end
   end
 end
