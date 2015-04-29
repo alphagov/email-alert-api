c34d506a92519d366dfd462fa81e316e3870fb9d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-04-29 11:52:27 +0100

    Add slug mapping CSV to policy subscription seeding.

--- db/migrate/20150428072646_seed_policy_subscriptions.rb
+++ db/migrate/20150428072646_seed_policy_subscriptions.rb
@@ -2,17 +2,27 @@ require 'csv'
 
 class SeedPolicySubscriptions < ActiveRecord::Migration
   def change
+    slug_mapping = {}
+    mapping_csv_path = File.join(File.dirname(__FILE__), "20150428072646_seed_policy_subscriptions_mapping.csv")
+    CSV.foreach(mapping_csv_path, headers: true) do |mapping|
+      slug_mapping[mapping["old_slug"]] = mapping["new_slug"]
+    end
+
     csv_path = File.join(File.dirname(__FILE__), "20150428072646_seed_policy_subscriptions.csv")
 
     CSV.foreach(csv_path) do |(slug, gov_delivery_id)|
-      slug = slug.gsub(%r{^/}, '')
+      old_slug = slug.gsub(%r{^/}, '')
 
-      SubscriberList.create!(
-        gov_delivery_id: gov_delivery_id,
-        tags: {
-          policies: [slug]
-        },
-      )
+      if new_slug = slug_mapping[old_slug]
+        SubscriberList.create!(
+          gov_delivery_id: gov_delivery_id,
+          tags: {
+            policies: [new_slug]
+          },
+        )
+      else
+        puts "No mapping to create subscription for #{old_slug}"
+      end
     end
   end
 end
