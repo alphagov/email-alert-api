fe8d2b46d2bbcf64497c0f26301605f79cfd2e81
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2016-02-15 15:27:30 +0000

    Use content IDs instead of country slugs
    Content IDs, as minted by Travel Advice Publisher, are more stable than
    the slugs we use to refer to them as, and more future proof.
    
    I've imported the content IDs from Travel Advice Publisher, and used
    them to replace the slugs in the `links.countries` nested hash.

---
+++ db/migrate/20160215144137_add_content_i_ds_to_travel_advice_topics.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+class AddContentIDsToTravelAdviceTopics < ActiveRecord::Migration
+  def change
+    csv_path = File.join(File.dirname(__FILE__), "20160215144137_add_content_i_ds_to_travel_advice_topics.csv")
+
+    # Clean up after last migration - delete the erroneous tags
+    s = SubscriberList.find_by(gov_delivery_id: "gov_delivery_id")
+    s.destroy!
+    puts "Destroyed erroneous topic"
+
+    CSV.foreach(csv_path, {:headers => true, :return_headers => false}) do |row|
+      s = SubscriberList.find_by(gov_delivery_id: row["gov_delivery_id"])
+      s.links = {
+        :countries => [row["content_id"]]
+      }
+      s.save
+      puts "Updated #{row['gov_delivery_id']}"
+    end
+  end
+end
