bf6deccd89f77860d755adaad18762daa6d8e171
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-23 14:24:33 +0000

    Test that subscriber lists can be created in valid states

--- spec/requests/create_subscriber_list_spec.rb
+++ spec/requests/create_subscriber_list_spec.rb
@@ -60,13 +60,53 @@ RSpec.describe "Creating a subscriber list", type: :request do
     expect(response.status).to eq(422)
   end
 
-  def create_subscriber_list(tags: {}, links: {})
-    request_body = JSON.dump({
+  describe "creating a subscriber list with a document_type" do
+    it "returns a 201" do
+      create_subscriber_list(document_type: "travel_advice")
+
+      expect(response.status).to eq(201)
+    end
+
+    it "sets the document_type on the subscriber list" do
+      create_subscriber_list(
+        tags: { countries: ["andorra"] },
+        document_type: "travel_advice"
+      )
+
+      subscriber_list = SubscriberList.last
+      expect(subscriber_list.document_type).to eq("travel_advice")
+    end
+  end
+
+  context "when creating a subscriber list with no tags or links" do
+    context "and a document_type is provided" do
+      it "returns a 201" do
+        create_subscriber_list(document_type: "travel_advice")
+
+        expect(response.status).to eq(201)
+      end
+    end
+
+    context "and no document_type is provided" do
+      it "returns a 422" do
+        create_subscriber_list
+
+        expect(response.status).to eq(422);
+        expect(response.body).to match(/Must have either a document_type, tags or links/)
+      end
+    end
+  end
+
+  def create_subscriber_list(tags: {}, links: {}, document_type: nil)
+    payload = {
       title: "This is a sample title",
       gov_delivery_id: "UKGOVUK_1234",
       tags: tags,
-      links: links
-    })
+      links: links,
+    }
+
+    payload.merge!(document_type: document_type) if document_type
+    request_body = JSON.dump(payload)
 
     post "/subscriber-lists", request_body, json_headers
   end
