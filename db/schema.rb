4241855cd76e75328c6732a120133d0a65e5a1fa
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2016-09-15 11:37:13 +0100

    Switch `tags` and `links` columns to JSON from hstore.
    This uses the duplicate *_json columns as a data source.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718090427) do
+ActiveRecord::Schema.define(version: 20160905121642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,16 +19,13 @@ ActiveRecord::Schema.define(version: 20160718090427) do
   create_table "subscriber_lists", force: :cascade do |t|
     t.string   "title",           limit: 255
     t.string   "gov_delivery_id", limit: 255
-    t.hstore   "tags",                        default: {}, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.hstore   "links",                       default: {}, null: false
     t.string   "document_type",               default: "", null: false
     t.json     "tags_json",                   default: {}, null: false
     t.json     "links_json",                  default: {}, null: false
+    t.json     "tags",                        default: {}, null: false
+    t.json     "links",                       default: {}, null: false
   end
-
-  add_index "subscriber_lists", ["links"], name: "index_subscriber_lists_on_links", using: :gin
-  add_index "subscriber_lists", ["tags"], name: "index_subscriber_lists_on_tags", using: :gin
 
 end
