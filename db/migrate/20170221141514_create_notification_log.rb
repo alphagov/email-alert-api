6ee2a456da92f2dd51d6e7a9d6315080ad06170c
David Henry <david@decoybecoy.com>
2017-02-21 15:02:20 +0000

    Add notification_logs table

---
+++ db/migrate/20170221141514_create_notification_log.rb
@@ -0,0 +1,19 @@
+class CreateNotificationLog < ActiveRecord::Migration
+  def change
+    create_table :notification_logs do |t|
+      t.string :govuk_request_id, index: true, default: ''
+      t.string :content_id, default: ''
+      t.datetime :public_updated_at
+      t.json :links, default: {}
+      t.json :tags, default: {}
+      t.string :document_type, default: ''
+      t.string :emailing_app, default: '', null: false
+      t.json :gov_delivery_ids, default: []
+      t.string :publishing_app, default: ''
+
+      t.timestamps null: false
+    end
+
+    add_index :notification_logs, [:content_id, :public_updated_at]
+  end
+end
