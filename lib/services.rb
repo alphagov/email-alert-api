07833f93294d977f477b7d959443abceae68ebab
Graham Pengelly <graham@three-tier.com>
2017-09-18 15:08:58 +0100

    Remove govuk_delivery service
    This service has been removed and is no longer required.

--- lib/services.rb
+++ lib/services.rb
@@ -1,6 +1,5 @@
 require "gov_delivery/client"
 require 'gds_api/content_store'
-require 'gds_api/gov_uk_delivery'
 
 module Services
   def self.gov_delivery
@@ -9,9 +8,5 @@ module Services
 
   def self.content_store
     @content_store ||= GdsApi::ContentStore.new(Plek.new.find('content-store'))
-  end
-
-  def self.govuk_delivery
-    @govuk_delivery ||= GdsApi::GovUkDelivery.new(Plek.new.find('govuk-delivery'))
   end
 end
