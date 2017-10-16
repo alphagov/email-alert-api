d111d4be736c4682b4c9b510bdf3d468551dc50e
Graham Pengelly <graham@three-tier.com>
2017-10-16 21:08:34 +0100

    Add `DeliverToSubscriber` command
    Command to call `EmailSender` with correct arguments (currently
    `address` only) for a subscriber.

---
+++ spec/commands/deliver_to_subscriber_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe DeliverToSubscriber do
+  describe ".call" do
+    let(:email_sender){ double }
+    before do
+      allow(Services).to receive(:email_sender).and_return(
+        email_sender
+      )
+    end
+
+    it "calls email_sender with address" do
+      subscriber = double(address: "test@test.com")
+
+      expect(email_sender).to receive(:call)
+        .with(address: "test@test.com")
+
+      DeliverToSubscriber.call(
+        subscriber: subscriber,
+      )
+    end
+
+    it "requires subscriber" do
+      expect{
+        DeliverToSubscriber.call(subscriber: nil)
+      }.to raise_error(
+        ArgumentError,
+        "subscriber cannot be nil"
+      )
+    end
+  end
+end
