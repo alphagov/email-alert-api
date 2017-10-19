d781bdcc92f596d8453144f82f708f344bd5f7c0
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-10-19 08:31:24 +0100

    Make subscriber email address unique
    I can't think of a reason not to do this, and it keeps our
    database slightly cleaner.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019072332) do
+ActiveRecord::Schema.define(version: 20171019072924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171019072332) do
     t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["address"], name: "index_subscribers_on_address", unique: true
   end
 
 end
