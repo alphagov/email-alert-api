3f83e4a62de124fbbe72c43094750d6bee95367e
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-10-20 09:35:14 +0100

    Add a uniqueness validation on subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
   belongs_to :subscriber
   belongs_to :subscriber_list
+
+  validates :subscriber, uniqueness: { scope: :subscriber_list }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Subscription, type: :model do
     it "is valid for the default factory" do
       expect(subject).to be_valid
     end
+
+    it "must be unique between subscriber and subscriber lists" do
+      FactoryGirl.create(
+        :subscription,
+        subscriber: subject.subscriber,
+        subscriber_list: subject.subscriber_list
+      )
+
+      expect(subject).to be_invalid
+    end
   end
 end
