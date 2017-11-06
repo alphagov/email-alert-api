d0f15c98f47510e8c380025e025e67aa1df28403
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-11-06 09:11:58 +0000

    Log when we override email address

--- app/services/email_sender/email_sender_service.rb
+++ app/services/email_sender/email_sender_service.rb
@@ -5,6 +5,8 @@ class EmailSenderService
   end
 
   def call(address:, subject:, body:)
+    log_override(address: address, subject: subject, body: body) if email_address_override
+
     provider.call(
       address: email_address_override || address,
       subject: subject,
@@ -15,4 +17,16 @@ class EmailSenderService
 private
 
   attr_reader :provider, :email_address_override
+
+  def log_override(address:, subject:, body:)
+    overriden = " (overriden to #{email_address_override}) "
+    logger.info(%(Sending email to #{address}#{overriden}
+Subject: #{subject}
+Body: #{body}
+))
+  end
+
+  def logger
+    Rails.logger
+  end
 end
