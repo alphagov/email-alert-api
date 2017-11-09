f4d793c15bde0a2091c844a1219b8399257725b2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-11-09 08:48:21 +0000

    Extract email rendering into separate class

--- app/models/email.rb
+++ app/models/email.rb
@@ -7,21 +7,12 @@ class Email < ApplicationRecord
   end
 
   def self.build_from_params(params)
-    new.tap do |instance|
-      instance.notification_id = params[:notification_id]
-      instance.subject = params[:title]
-      instance.body = <<~BODY
-        There has been a change to *#{params[:title]}* on #{instance.format_date(params[:public_updated_at])}.
+    renderer = EmailRenderer.new(params: params)
 
-        > #{params[:description]}
-
-        **#{params[:change_note]}**
-      BODY
-    end
-  end
-
-  def format_date(date)
-    return unless date
-    date.strftime("%H:%M %-d %B %Y")
+    new(
+      notification_id: params[:notification_id],
+      subject: renderer.subject,
+      body: renderer.body
+    )
   end
 end
