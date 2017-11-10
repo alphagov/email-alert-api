aa65af28948474d452b6bfd5a9df96bfec33c90e
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-11-10 11:56:26 +0000

    Remove internal failure state
    This won't be necessary as we will consider it the same as a
    technical failure.

--- app/models/delivery_attempt.rb
+++ app/models/delivery_attempt.rb
@@ -3,19 +3,15 @@ class DeliveryAttempt < ApplicationRecord
 
   validates :email, :status, :provider, :reference, presence: true
 
-  enum status: %i(sending delivered permanent_failure temporary_failure technical_failure internal_failure)
+  enum status: %i(sending delivered permanent_failure temporary_failure technical_failure)
   enum provider: %i(psuedo notify)
 
   def failure?
-    permanent_failure? || temporary_failure? || technical_failure? || internal_failure?
-  end
-
-  def should_retry?
-    temporary_failure? || technical_failure?
+    permanent_failure? || temporary_failure? || technical_failure?
   end
 
   def should_report_failure?
-    internal_failure?
+    technical_failure?
   end
 
   def should_remove_subscriber?
