0349d1d9b0956ebbb7bef6bb1490adafa15f8c54
Graham Pengelly <graham@three-tier.com>
2017-11-24 13:41:32 +0000

    Add `mark_processed!` method to ContentChange
    This method sets `processed_at` and will be called within the
    `SubscriptionContentWorker` to indicate that the `ContentChange` has
    been processed.

--- app/models/content_change.rb
+++ app/models/content_change.rb
@@ -2,4 +2,8 @@ require_relative "extensions/symbolize_json"
 
 class ContentChange < ApplicationRecord
   include SymbolizeJSON
+
+  def mark_processed!
+    update!(processed_at: Time.now)
+  end
 end
