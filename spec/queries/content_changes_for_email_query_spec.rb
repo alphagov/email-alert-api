c4e29a6feb0792914f92b20797bed3116085397b
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-28 10:17:26 +0000

    Query class for ContentChange associated with an Email
    For a given email this will return an ActiveRecord scope of the
    ContentChanges (if any) associated with it.

---
+++ spec/queries/content_changes_for_email_query_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ContentChangesForEmailQuery do
+  describe ".call" do
+    let!(:email) { create(:email) }
+    subject(:content_changes) { described_class.call(email) }
+
+    context "when there are no subscription content associations" do
+      it "returns an empty scope" do
+        expect(content_changes.exists?).to be false
+      end
+    end
+
+    context "when there are multiple subscription content associations" do
+      let(:content_change) { create(:content_change) }
+      before do
+        create(:subscription_content, email: email, content_change: content_change)
+        create(:subscription_content, email: email, content_change: content_change)
+      end
+
+      it "returns a scope of the unique content changes" do
+        expect(content_changes.count).to eq 1
+      end
+    end
+  end
+end
