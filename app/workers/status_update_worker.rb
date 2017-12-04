a8de2eb6804fa853e953ece5c68dc0444885b744
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-04 10:35:11 +0000

    Remove status update worker
    It's no longer being used by anything.

--- app/workers/status_update_worker.rb
+++
@@ -1,8 +0,0 @@
-class StatusUpdateWorker
-  include Sidekiq::Worker
-  include Sidekiq::Symbols
-
-  def perform(reference:, status:)
-    StatusUpdateService.call(reference: reference, status: status)
-  end
-end
