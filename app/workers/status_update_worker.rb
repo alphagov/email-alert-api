ed0ef3770cfa7ee75b449c5a74db559af74f56ec
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-04 10:09:09 +0000

    Move status updating into a service
    This extracts the actual process of updating the status of an
    email away from the worker making it easier to use synchronously.

--- app/workers/status_update_worker.rb
+++ app/workers/status_update_worker.rb
@@ -3,20 +3,6 @@ class StatusUpdateWorker
   include Sidekiq::Symbols
 
   def perform(reference:, status:)
-    delivery_attempt = DeliveryAttempt.find_by!(reference: reference)
-    delivery_attempt.update!(status: status.underscore)
-
-    subscriber = lookup_subscriber(delivery_attempt)
-
-    if subscriber && delivery_attempt.permanent_failure?
-      UnsubscribeService.subscriber!(subscriber)
-    end
-  end
-
-private
-
-  def lookup_subscriber(delivery_attempt)
-    address = delivery_attempt.email.address
-    Subscriber.find_by(address: address)
+    StatusUpdateService.call(reference: reference, status: status)
   end
 end
