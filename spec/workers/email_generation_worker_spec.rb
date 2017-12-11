b83022a104a78d59249c5569c27c6037a959e1b3
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-11 13:02:50 +0000

    Add a test to check for duplicate emails being sent out
    We can't test that the correct number of emails are being sent out
    as the lock is exclusive across processes, not threads. However,
    we can test that when the lock doesn't work, we get an exception
    from the optimistic locking meaning that duplicate emails will
    still not be sent out and the worker will be retried.

--- spec/workers/email_generation_worker_spec.rb
+++ spec/workers/email_generation_worker_spec.rb
@@ -1,18 +1,18 @@
 RSpec.describe EmailGenerationWorker do
   describe ".perform" do
+    def perform_with_fake_sidekiq
+      Sidekiq::Testing.fake! do
+        DeliveryRequestWorker.jobs.clear
+        described_class.new.perform
+      end
+    end
+
     context "with a subscription content" do
       let!(:subscription_content) { create(:subscription_content) }
 
       before do
         create(:subscription_content, email: create(:email))
         create(:subscription_content, subscription: create(:subscription, subscriber: create(:subscriber, address: nil)))
-      end
-
-      def perform_with_fake_sidekiq
-        Sidekiq::Testing.fake! do
-          DeliveryRequestWorker.jobs.clear
-          described_class.new.perform
-        end
       end
 
       it "should create an email" do
@@ -32,6 +32,30 @@ RSpec.describe EmailGenerationWorker do
       it "should queue a delivery email job" do
         perform_with_fake_sidekiq
         expect(DeliveryRequestWorker.jobs.size).to eq(1)
+      end
+    end
+
+    context "with many subscription contents running concurrently" do
+      before do
+        100.times do
+          create(:subscription_content)
+        end
+      end
+
+      def run_worker
+        Sidekiq::Testing.fake! { described_class.new.perform }
+      end
+
+      def run_worker_threads
+        Rails.application.eager_load! # needed as autoload is not thread safe
+
+        3.times
+          .map { Thread.new { run_worker } }
+          .each(&:join)
+      end
+
+      it "will raise a stale object exception" do
+        expect { run_worker_threads }.to raise_error(ActiveRecord::StaleObjectError)
       end
     end
   end
