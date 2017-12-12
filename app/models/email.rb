1a5143a3b473748b482cefbbe92cd06a35feced2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-12 14:43:07 +0000

    Split email generation methods up
    This allows us to use a faster bulk insert method.

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,17 +2,21 @@ class Email < ApplicationRecord
   validates :address, :subject, :body, presence: true
 
   def self.create_from_params!(params)
-    build_from_params(params).tap(&:save!)
+    new_from_params(params).tap(&:save!)
+  end
+
+  def self.new_from_params(params)
+    new(build_from_params(params))
   end
 
   def self.build_from_params(params)
     renderer = EmailRenderer.new(params: params)
     subscriber = params.fetch(:subscriber)
 
-    new(
+    {
       address: subscriber.address,
       subject: renderer.subject,
       body: renderer.body
-    )
+    }
   end
 end
