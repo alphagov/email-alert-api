f82494e4810ea38cb5be67efde0b1d93f41e5fb7
Chris Patuzzo <chris@patuzzo.co.uk>
2017-12-12 13:49:55 +0000

    Nullify subscription_contents.subscription_id when unsubscribing

--- app/services/unsubscribe_service.rb
+++ app/services/unsubscribe_service.rb
@@ -12,12 +12,20 @@ module UnsubscribeService
 
     def unsubscribe!(subscriber, subscriptions)
       ActiveRecord::Base.transaction do
+        nullify_references_to_subscriptions!(subscriptions)
+
         subscriptions.each(&:destroy)
 
         if no_other_subscriptions?(subscriber, subscriptions)
           subscriber.nullify_address!
         end
       end
+    end
+
+    def nullify_references_to_subscriptions!(subscriptions)
+      SubscriptionContent
+        .where(subscription: subscriptions)
+        .update_all(subscription_id: nil)
     end
 
     def no_other_subscriptions?(subscriber, subscriptions)
