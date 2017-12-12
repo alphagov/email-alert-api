3e908ca09bc0c816fe94729be8c68c8d1a86b529
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-12 11:05:14 +0000

    Add a service to generate emails

---
+++ app/services/email_generation_service.rb
@@ -0,0 +1,47 @@
+class EmailGenerationService
+  LOCK_NAME = "email_generation_worker".freeze
+
+  def self.call(*args)
+    new.call(*args)
+  end
+
+  def call
+    SubscriptionContent.with_advisory_lock(LOCK_NAME, timeout_seconds: 0) do
+      subscription_contents.find_each do |subscription_content|
+        email = create_email(subscription_content: subscription_content)
+        subscription_content.update!(email_id: email.id)
+
+        DeliveryRequestWorker.perform_async_with_priority(
+          email.id, priority: subscription_content.content_change.priority.to_sym
+        )
+      end
+    end
+  end
+
+private
+
+  def subscription_contents
+    SubscriptionContent
+      .joins(:content_change, subscription: { subscriber: { subscriptions: :subscriber_list } })
+      .includes(:content_change, subscription: { subscriber: { subscriptions: :subscriber_list } })
+      .where.not(subscribers: { address: nil })
+      .where(email: nil)
+  end
+
+  def create_email(subscription_content:)
+    Email.create_from_params!(email_params(subscription_content: subscription_content))
+  end
+
+  def email_params(subscription_content:)
+    content_change = subscription_content.content_change
+
+    {
+      title: content_change.title,
+      change_note: content_change.change_note,
+      description: content_change.description,
+      base_path: content_change.base_path,
+      public_updated_at: content_change.public_updated_at,
+      subscriber: subscription_content.subscription.subscriber,
+    }
+  end
+end
