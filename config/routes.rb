20e8d27fceb9ea7a38f0bd81411cec96eb0313fd
Chris Patuzzo <chris@patuzzo.co.uk>
2017-12-14 14:14:10 +0000

    Move /subscribables route next to /subscriber-lists
    These return similar data so keep them together.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :subscriber_lists, path: "subscriber-lists", only: %i[create]
   get "/subscriber-lists", to: "subscriber_lists#show"
+  get "/subscribables/:gov_delivery_id", to: "subscribables#show"
 
   resources :notifications, only: %i[create index show]
   resources :status_updates, path: "status-updates", only: %i[create]
   resources :subscriptions, only: %i[create]
-  get "subscribables/:gov_delivery_id", to: "subscribables#show"
 
   get "/healthcheck", to: "healthcheck#check"
 
