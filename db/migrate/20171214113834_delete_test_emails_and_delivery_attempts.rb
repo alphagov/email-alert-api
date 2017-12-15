eda3ab9b697f1646239a760261c53c6bdbab56c6
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-12-15 14:16:01 +0000

    Fix test data migration
    This commit fixes the migration to delete test data by swapping the ordering to get around a foreign key violation.

--- db/migrate/20171214113834_delete_test_emails_and_delivery_attempts.rb
+++ db/migrate/20171214113834_delete_test_emails_and_delivery_attempts.rb
@@ -1,7 +1,7 @@
 class DeleteTestEmailsAndDeliveryAttempts < ActiveRecord::Migration[5.1]
   def up
     # Emails and DeliveryAttempts at this point are all test data
-    Email.destroy_all
     DeliveryAttempt.destroy_all
+    Email.destroy_all
   end
 end
