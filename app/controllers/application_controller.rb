865ad1ab704175450397fc95761da62d0d434d33
Graham Pengelly <graham@three-tier.com>
2017-12-19 11:16:45 +0000

    Add logging of missing bearer tokens
    All apps should now be sending a bearer token with `internal_app`
    permission. Prior to enforcing this, this commit adds logging which we
    can deploy for a while just to ensure we haven't missed any clients.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::API
   include GDS::SSO::ControllerMethods
+
+  before_action :log_missing_credentials
+
+private
+
+  def log_missing_credentials
+    auth_header = request.env["HTTP_AUTHORIZATION"]
+    if auth_header.present?
+      authenticate_user!
+      check_for_valid_and_permitted
+    else
+      logger.error "Missing bearer token from #{request_user_agent} - #{request_path}"
+    end
+  end
+
+  def check_for_valid_and_permitted
+    if user_signed_in?
+      unless current_user.has_permission?("internal_app")
+        logger.error "Missing internal_app permission from #{request_user_agent} - #{request_path}"
+      end
+    else
+      logger.error "Invalid bearer token from #{request_user_agent} - #{request_path}"
+    end
+  end
+
+  def request_user_agent
+    request.env["HTTP_USER_AGENT"]
+  end
+
+  def request_path
+    "#{controller_path}##{action_name}"
+  end
 end
