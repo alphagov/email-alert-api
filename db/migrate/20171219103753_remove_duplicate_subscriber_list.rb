159e130c9378ba8d8dd2706a384f823f5ff9710d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-19 10:48:19 +0000

    Add a migration to delete the one duplicate subscriber list
    We have two subscriber lists with a duplicate title, the rest are
    all unique. This is causing a problem with the data sync task as
    it relies on unique titles and gov delivery IDs to do a difference
    on arrays to decide which subscriber lists to delete.

---
+++ db/migrate/20171219103753_remove_duplicate_subscriber_list.rb
@@ -0,0 +1,15 @@
+class RemoveDuplicateSubscriberList < ActiveRecord::Migration[5.1]
+  def change
+    # production & staging
+    SubscriberList.where(
+      title: "news stories related to UK Visas and Immigration and China",
+      gov_delivery_id: "UKGOVUK_35116"
+    ).delete_all
+
+    # integration
+    SubscriberList.where(
+      title: "news stories related to UK Visas and Immigration and China",
+      gov_delivery_id: "UKGOVUKDUP_35116"
+    ).delete_all
+  end
+end
