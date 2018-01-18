84303d486e5183aeeeb8476bcbaff96a65d1e34b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-01-18 11:22:37 +0000

    Accept frequency when creating subscriptions
    This allows the user to specify if they want this subscription to
    be a daily or weekly digest, or if they want to receive emails
    immediately.

--- spec/integration/create_subscription_spec.rb
+++ spec/integration/create_subscription_spec.rb
@@ -13,6 +13,33 @@ RSpec.describe "Creating a subscription", type: :request do
 
         expect(response.status).to eq(201)
       end
+
+      context "with a frequency setting" do
+        it "returns a 201 and sets the frequency" do
+          params = JSON.dump(address: "test@example.com", subscribable_id: subscribable.id, frequency: "daily")
+          post "/subscriptions", params: params, headers: JSON_HEADERS
+
+          expect(response.status).to eq(201)
+
+          expect(Subscription.first.daily?).to be_truthy
+        end
+
+        context "with an existing subscription" do
+          it "updates the frequency" do
+            params = JSON.dump(address: "test@example.com", subscribable_id: subscribable.id, frequency: "daily")
+            post "/subscriptions", params: params, headers: JSON_HEADERS
+
+            expect(response.status).to eq(201)
+
+            params = JSON.dump(address: "test@example.com", subscribable_id: subscribable.id, frequency: "weekly")
+            post "/subscriptions", params: params, headers: JSON_HEADERS
+
+            expect(response.status).to eq(200)
+
+            expect(Subscription.first.weekly?).to be_truthy
+          end
+        end
+      end
     end
   end
 
