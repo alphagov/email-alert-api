fe8b4b1e5b16042e512bb8babfd2766641bd9f40
Graham Pengelly <graham@three-tier.com>
2018-01-22 11:20:15 +0000

    Add WeeklyDigestSchedulerService
    Adds another scheduler for initiating weekly digests.

--- app/services/daily_digest_scheduler_service.rb
+++ app/services/daily_digest_scheduler_service.rb
@@ -1,27 +1,5 @@
 class DailyDigestSchedulerService
+  include DigestSchedulerService
   LOCK_NAME = "daily_digest_scheduler".freeze
-
-  def self.call(*args)
-    new.call(*args)
-  end
-
-  def call
-    run_with_advisory_lock do
-      digest_run = DigestRun.find_or_initialize_by(
-        date: Date.current, range: DigestRun::DAILY
-      )
-      return if digest_run.persisted?
-      digest_run.save!
-    end
-
-    #enqueue the digest creation workers TBC
-  end
-
-private
-
-  def run_with_advisory_lock
-    DigestRun.with_advisory_lock(LOCK_NAME, timeout_seconds: 0) do
-      yield
-    end
-  end
+  RANGE = DigestRun::DAILY.freeze
 end
