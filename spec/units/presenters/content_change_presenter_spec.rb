ff299539694f332fee2a1d462cc9eec6ba5bd891
Graham Pengelly <graham@three-tier.com>
2018-01-25 09:26:32 +0000

    Add ContentChangePresenter
    Both digest and immediate emails use the same content change format.
    This commit extracts the presentation of this into its own presenter so
    as it can be reused.

---
+++ spec/units/presenters/content_change_presenter_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe ContentChangePresenter do
+  let(:content_change) {
+    build(
+      :content_change, title: "Change title",
+      base_path: "/government/test-slug",
+      change_note: "Test change note",
+      description: "Test description",
+      public_updated_at: Time.parse("10:00 1/1/2018")
+    )
+  }
+
+  describe ".call" do
+    it "returns a presenter content change" do
+      expected = <<~CONTENT_CHANGE
+        [Change title](http://www.dev.gov.uk/government/test-slug)
+
+        Test change note: Test description
+
+        Updated at 10:00 am on 1 January 2018
+      CONTENT_CHANGE
+
+      expect(described_class.call(content_change)).to eq(expected)
+    end
+  end
+end
