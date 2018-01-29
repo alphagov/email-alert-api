d822cfdf5efbb3ff29b67363afc9037f21a7e492
Graham Pengelly <graham@three-tier.com>
2018-01-29 15:14:46 +0000

    Add incomplete_for_run scope to DigestRunSubscriber
    This returns `DigestRunSubscriber objects that are incomplete for a
    provided `digest_run_id`. When this returns 0 records the `DigestRun`
    can be marked complete.

--- app/models/digest_run_subscriber.rb
+++ app/models/digest_run_subscriber.rb
@@ -3,6 +3,8 @@ class DigestRunSubscriber < ApplicationRecord
   belongs_to :digest_run
   belongs_to :subscriber
 
+  scope :incomplete_for_run, ->(digest_run_id) { where(digest_run_id: digest_run_id).where(completed_at: nil) }
+
   def mark_complete!
     update_attributes!(completed_at: Time.now)
   end
