9ed07e56c7210a95b94d63f526b01937d10f6bba
Graham Pengelly <graham@three-tier.com>
2018-01-29 12:06:20 +0000

    Refactor DigestInitiatorService
    Extract some methods from `.call` to slim it down a bit.

--- app/services/digest_initiator_service.rb
+++ app/services/digest_initiator_service.rb
@@ -8,36 +8,52 @@ class DigestInitiatorService
   end
 
   def call
-    digest_run = nil
+    digest_run = create_digest_run
+    return if digest_run.nil?
 
+    subscribers = DigestRunSubscriberQuery.call(digest_run: digest_run)
+
+    digest_run_subscriber_params = build_digest_run_subscriber_params(
+      digest_run,
+      subscribers
+    )
+
+    digest_run_subscriber_ids = import_digest_run_subscribers(
+      digest_run_subscriber_params
+    )
+
+    enqueue_jobs(digest_run_subscriber_ids)
+  end
+
+private
+
+  attr_reader :range
+
+  def create_digest_run
     run_with_advisory_lock do
       digest_run = DigestRun.find_or_initialize_by(
         date: Date.current, range: range
       )
       return if digest_run.persisted?
       digest_run.save!
+      digest_run
     end
+  end
 
-    #TODO think about this being retried
-    #
-    subscribers = DigestRunSubscriberQuery.call(digest_run: digest_run)
-
-    digest_run_subscriber_params = subscribers.map do |subscriber|
+  def build_digest_run_subscriber_params(digest_run, subscribers)
+    subscribers.map do |subscriber|
       {
         subscriber_id: subscriber.id,
         digest_run_id: digest_run.id
       }
     end
+  end
 
-    import_result = DigestRunSubscriber.import!(digest_run_subscriber_params)
-    import_result.ids.each do |digest_run_subscriber_id|
+  def enqueue_jobs(digest_run_subscriber_ids)
+    Array(digest_run_subscriber_ids).each do |digest_run_subscriber_id|
       DigestEmailGenerationWorker.perform_async(digest_run_subscriber_id)
     end
   end
-
-private
-
-  attr_reader :range
 
   def run_with_advisory_lock
     DigestRun.with_advisory_lock(lock_name, timeout_seconds: 0) do
@@ -47,5 +63,9 @@ private
 
   def lock_name
     "#{range}_digest_initiator"
+  end
+
+  def import_digest_run_subscribers(params)
+    DigestRunSubscriber.import!(params).ids
   end
 end
