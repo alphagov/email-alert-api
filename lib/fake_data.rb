02ee34a0d0464f6fc3ffe93efecdf81f0cb0ebe8
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-01-30 10:14:12 +0000

    Add some methods for loading fake subscriptions
    The fake subscription data comes from a full data dump that
    GovDelivery provided us in July 2016. It provides a distribution
    of subscriptions, mapping number of subscribers with number of
    subscriptions.

--- lib/fake_data.rb
+++ lib/fake_data.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class FakeData
   def initialize; end
 
@@ -5,5 +7,50 @@ class FakeData
     new(*args).call
   end
 
-  def call; end
+  def call
+    20.times do
+      p pick_subscriber_list
+    end
+
+    20.times do
+      p pick_subscription_frequency
+    end
+
+    subscriptions.each do |subscription|
+      p subscription
+    end
+  end
+
+private
+
+  SubscriptionStat = Struct.new(:number, :count)
+
+  def pick_subscriber_list(limit: 1)
+    SubscriberList.order("RANDOM()").limit(limit)
+  end
+
+  def pick_subscription_frequency
+    random = Random.new
+    choice = random.rand(1.0)
+
+    if choice < 0.15
+      :weekly
+    elsif choice < 0.5
+      :daily
+    else
+      :immediately
+    end
+  end
+
+  def subscriptions
+    @subscriptions ||= begin
+      CSV.read(fake_subscription_data_path).map do |(number, count)|
+        SubscriptionStat.new(number, count)
+      end
+    end
+  end
+
+  def fake_subscription_data_path
+    File.join(File.dirname(__FILE__), "fake_subscription_data.csv")
+  end
 end
