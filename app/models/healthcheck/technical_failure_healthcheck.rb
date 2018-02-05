fe0f22608d2e695daddccccb43afc0a79759b084
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-05 15:01:17 +0000

    Cache technical failure queries

--- app/models/healthcheck/technical_failure_healthcheck.rb
+++ app/models/healthcheck/technical_failure_healthcheck.rb
@@ -7,9 +7,9 @@ class Healthcheck
     def status
       return :ok unless expect_status_update_callbacks?
 
-      if failures_since(1.hour.ago).exists?
+      if failures_since(1.hour).exists?
         :critical
-      elsif failures_since(1.day.ago).exists?
+      elsif failures_since(24.hours).exists?
         :warning
       else
         :ok
@@ -18,17 +18,20 @@ class Healthcheck
 
     def details
       [1, 12, 24].each_with_object({}) do |n, hash|
-        hash[:"last_#{n}_hours"] = failures_since(n.hours.ago).count
+        hash[:"last_#{n}_hours"] = failures_since(n.hours).count
       end
     end
 
   private
 
-    def failures_since(datetime)
-      DeliveryAttempt
-        .latest_per_email
-        .where(status: :technical_failure)
-        .where("updated_at > ?", datetime)
+    def failures_since(hours)
+      @failures_since_cache ||= {}
+      @failures_since_cache[hours] ||= begin
+        DeliveryAttempt
+          .latest_per_email
+          .where(status: :technical_failure)
+          .where("updated_at > ?", hours.ago)
+      end
     end
 
     def expect_status_update_callbacks?
