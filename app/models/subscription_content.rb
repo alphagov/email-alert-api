d67ca229e8ba8e413f95a7191226679db11e4623
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-05 11:29:54 +0000

    Add scopes on SubscriptionContent for digest or immediate
    This allows users to specify whether they would only like to see
    subscription contents that were part of a digest or part of an
    immediate email while querying.

--- app/models/subscription_content.rb
+++ app/models/subscription_content.rb
@@ -2,4 +2,7 @@ class SubscriptionContent < ApplicationRecord
   belongs_to :subscription
   belongs_to :content_change
   belongs_to :email, optional: true
+
+  scope :immediate, -> { where(digest_run_subscriber_id: nil) }
+  scope :digest, -> { where.not(digest_run_subscriber_id: nil) }
 end
