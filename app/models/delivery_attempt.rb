ab379c3c24e2d82ee8ad12622c92d45957d7c960
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-06 11:39:53 +0000

    Remove DeliveryAttempt#latest_per_email
    It's not used anywhere.

--- app/models/delivery_attempt.rb
+++ app/models/delivery_attempt.rb
@@ -6,10 +6,6 @@ class DeliveryAttempt < ApplicationRecord
   enum status: { sending: 0, delivered: 1, permanent_failure: 2, temporary_failure: 3, technical_failure: 4 }
   enum provider: { pseudo: 0, notify: 1 }
 
-  def self.latest_per_email
-    from("(SELECT DISTINCT ON (email_id) * FROM delivery_attempts ORDER BY email_id, updated_at DESC) AS delivery_attempts")
-  end
-
   def failure?
     permanent_failure? || temporary_failure? || technical_failure?
   end
