04d1b02d38a97ca4ac8435b7a44d574bbf1e6f58
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-06 11:37:51 +0000

    Make technical failure healthcheck work on proportions
    This will now alert as critical if within an hour 10% of delivery
    attempts are recorded as having had a technical failure, and a
    warning if 5% are.

--- spec/models/healthcheck/technical_failure_healthcheck_spec.rb
+++ spec/models/healthcheck/technical_failure_healthcheck_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Healthcheck::TechnicalFailureHealthcheck do
-  def create_delivery_attempt(status, updated, email = create(:email))
-    create(:delivery_attempt, status: status, updated_at: updated, email: email)
+  def create_delivery_attempt(status, created, email = create(:email))
+    create(:delivery_attempt, status: status, created_at: created, email: email)
   end
 
   context "when status update callbacks are not expected" do
     before do
       allow(subject).to receive(:expect_status_update_callbacks?).and_return(false)
-      create_delivery_attempt(:technical_failure, 59.minutes.ago)
+      create_delivery_attempt(:technical_failure, 30.minutes.ago)
     end
     specify { expect(subject.status).to eq(:ok) }
   end
@@ -16,44 +16,49 @@ RSpec.describe Healthcheck::TechnicalFailureHealthcheck do
     specify { expect(subject.status).to eq(:ok) }
   end
 
-  context "when there are technical failures" do
-    context "within the last hour" do
-      before { create_delivery_attempt(:technical_failure, 59.minutes.ago) }
-      specify { expect(subject.status).to eq(:critical) }
+  context "when a proportion of delivery attempts are technical failures" do
+    context "at 2%" do
+      before do
+        1.times { create_delivery_attempt(:technical_failure, 15.minutes.ago) }
+        49.times { create_delivery_attempt(:delivered, 15.minutes.ago) }
+      end
+      specify { expect(subject.status).to eq(:ok) }
     end
 
-    context "within the last day" do
-      before { create_delivery_attempt(:technical_failure, 23.hours.ago) }
+    context "at 5%" do
+      before do
+        1.times { create_delivery_attempt(:technical_failure, 15.minutes.ago) }
+        19.times { create_delivery_attempt(:delivered, 15.minutes.ago) }
+      end
       specify { expect(subject.status).to eq(:warning) }
     end
 
-    context "older than a day" do
-      before { create_delivery_attempt(:technical_failure, 25.hours.ago) }
-      specify { expect(subject.status).to eq(:ok) }
+    context "at 10%" do
+      before do
+        1.times { create_delivery_attempt(:technical_failure, 15.minutes.ago) }
+        9.times { create_delivery_attempt(:delivered, 15.minutes.ago) }
+      end
+      specify { expect(subject.status).to eq(:critical) }
     end
-  end
-
-  it "ignores delivery attempts that are superseded by newer ones" do
-    email = create(:email)
-
-    create_delivery_attempt(:technical_failure, 30.minutes.ago, email)
-    create_delivery_attempt(:delivered, 5.minutes.ago, email)
-
-    expect(subject.status).to eq(:ok)
   end
 
   describe "#details" do
     before do
-      3.times { create_delivery_attempt(:technical_failure, 30.minutes.ago) }
-      5.times { create_delivery_attempt(:technical_failure, 90.minutes.ago) }
+      2.times { create_delivery_attempt(:technical_failure, 15.minutes.ago) }
+      4.times { create_delivery_attempt(:delivered, 15.minutes.ago) }
+      4.times { create_delivery_attempt(:sending, 15.minutes.ago) }
     end
 
-    it "counts how many failures there have been over the course of a day" do
-      details = subject.details
+    it "shows the totals" do
+      totals = subject.details.fetch(:totals)
 
-      expect(details.fetch(:last_1_hours)).to eq(3)
-      expect(details.fetch(:last_12_hours)).to eq(8)
-      expect(details.fetch(:last_24_hours)).to eq(8)
+      expect(totals.fetch("failing")).to eq(2)
+      expect(totals.fetch("other")).to eq(8)
+    end
+
+    it "shows the proportions" do
+      expect(subject.details.fetch(:failing)).to eq(0.2)
+      expect(subject.details.fetch(:other)).to eq(0.8)
     end
   end
 end
