29060503bee2b93fa622d74b69e2b418e0748fb9
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-15 15:32:47 +0000

    Fix a migration that doesn't run properly
    I'd made the change after running locally, and in CI the database
    comes straight from the schema.rb file.

--- db/migrate/20180215144634_add_case_insensitive_index_on_emails.rb
+++ db/migrate/20180215144634_add_case_insensitive_index_on_emails.rb
@@ -1,6 +1,6 @@
 class AddCaseInsensitiveIndexOnEmails < ActiveRecord::Migration[5.1]
   def change
-    remove_index :subscribers, :address, unique: true
+    remove_index :subscribers, :address
     add_index :subscribers, 'LOWER(address)', name: 'index_subscribers_on_lower_address', unique: true
   end
 end
