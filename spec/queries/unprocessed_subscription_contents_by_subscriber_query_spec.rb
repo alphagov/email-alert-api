c461b47773ec998390b528404c01cb3ae746ee10
Graham Pengelly <graham@three-tier.com>
2018-02-19 13:46:39 +0000

    Add UnprocessedSubscriptionContentsBySubscriberQuery
    This query accepts an array of subscriber IDs and returns all of their
    matching `SubscriptionContent` records that have not had an email created for
    them. The results are returned as hash with the following structure:
    
    {
      <subscriber_id>: {
        <content_change_id>: [<#SubscriptionContent>, <#SubscriptionContent>],
        <content_change_id>: [<#SubscriptionContent>]
      },
      <subscriber_id>: {
        <content_change_id: [<#SubscriptionContent>]
      },
    }
    
    This will be used within the ImmediateEmailGenerationWorker to construct
    the emails in batches.

---
+++ spec/queries/unprocessed_subscription_contents_by_subscriber_query_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe UnprocessedSubscriptionContentsBySubscriberQuery do
+  let(:subscriber_one) { create(:subscriber) }
+  let(:subscriber_two) { create(:subscriber) }
+  let(:content_change_one) { create(:content_change) }
+  let(:content_change_two) { create(:content_change) }
+
+  before do
+    create(:subscription_content, subscription: create(:subscription, subscriber: subscriber_one), content_change: content_change_one)
+    create(:subscription_content, subscription: create(:subscription, subscriber: subscriber_one), content_change: content_change_one)
+    create(:subscription_content, subscription: create(:subscription, subscriber: subscriber_one), content_change: content_change_two)
+  end
+
+  subject { described_class.call(Subscriber.pluck(:id)) }
+
+  it "returns a hash keyed by subscriber_id" do
+    expect(subject.keys).to eq(Subscriber.pluck(:id))
+  end
+
+  it "creates a hash keyed by content_change_id per subscriber key" do
+    expect(subject[subscriber_one.id].keys).to eq(ContentChange.pluck(:id))
+  end
+
+  it "creates an array of subscription contents at each content change key" do
+    expect(subject[subscriber_one.id][content_change_one.id])
+      .to eq(SubscriptionContent.where(content_change: content_change_one))
+  end
+end
