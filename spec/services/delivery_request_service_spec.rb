ac42c7783917067eeb0a38c8306e101720788344
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-20 13:19:26 +0000

    Add ability to limit email addresses only to the whitelist

--- spec/services/delivery_request_service_spec.rb
+++ spec/services/delivery_request_service_spec.rb
@@ -195,6 +195,32 @@ RSpec.describe DeliveryRequestService do
         end
       end
 
+      context "when an override address is set and whitelist addresses are set and only whitelist emails should be sent" do
+        let(:config) do
+          {
+            email_address_override: "overriden@example.com",
+            email_address_override_whitelist: ["whitelist@example.com"],
+            email_address_override_whitelist_only: true,
+          }
+        end
+
+        context "when the argument is a whitelist address" do
+          let(:address) { "whitelist@example.com" }
+
+          it "returns the whitelisted address" do
+            expect(destination_address).to eq("whitelist@example.com")
+          end
+        end
+
+        context "when the argument is not a whitelist address" do
+          let(:address) { "original@example.com" }
+
+          it "returns a nil address" do
+            expect(destination_address).to be_nil
+          end
+        end
+      end
+
       context "when an override address is not set and whitelist addresses are set" do
         let(:config) do
           { email_address_override_whitelist: ["whitelist@example.com"] }
