406e0f62682ba1ddda6235ce7e633a0905e0490a
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-02-23 15:29:09 +0000

    Add more email address validation
    This commit adds more validation for email addresses. They must have:
    * Exactly one “@“ sign
    * At least one “.” in the domain or an IP address in “[]”
    * No commas

---
+++ app/validators/email_address_validator.rb
@@ -0,0 +1,44 @@
+class EmailAddressValidator < ActiveModel::Validator
+  def validate(record)
+    email_address = record.address
+    unless email_address.nil? || valid_email_address?(email_address)
+      record.errors.add(:address, 'is not an email address')
+    end
+  end
+
+private
+
+  def valid_email_address?(email_address)
+    contains_one_at_sign?(email_address) &&
+      contains_a_valid_domain?(email_address) &&
+      is_a_single_email_address?(email_address) &&
+      does_not_contain_newline_characters?(email_address)
+  end
+
+  def contains_one_at_sign?(email_address)
+    email_address.scan('@').length == 1
+  end
+
+  def contains_a_valid_domain?(email_address)
+    domain = email_address.split('@')[1]
+    return false if domain.nil?
+    domain_contains_at_least_one_dot?(domain) ||
+      domain_is_an_ip_address?(domain)
+  end
+
+  def is_a_single_email_address?(email_address)
+    email_address.scan(',').empty?
+  end
+
+  def does_not_contain_newline_characters?(email_address)
+    email_address !~ /\n/
+  end
+
+  def domain_contains_at_least_one_dot?(domain)
+    !domain.scan('.').empty?
+  end
+
+  def domain_is_an_ip_address?(domain)
+    domain.start_with?('[') && domain.end_with?(']')
+  end
+end
