97a7e2939b53d01a33ef9071092f13b6bea194d3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-02-28 16:18:27 +0000

    Include DigestRunSubsciber association in SubscriptionContent
    This allows us to join to DigestRunSubsciber table to work out which
    digest runs are associated with a SubscriptionContent.

--- app/models/subscription_content.rb
+++ app/models/subscription_content.rb
@@ -1,6 +1,7 @@
 class SubscriptionContent < ApplicationRecord
   belongs_to :subscription
   belongs_to :content_change
+  belongs_to :digest_run_subscriber, optional: true
   belongs_to :email, optional: true
 
   scope :immediate, -> { where(digest_run_subscriber_id: nil) }
