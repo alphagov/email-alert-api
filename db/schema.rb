b42496b04b43b977572bf9c8ad10ba3959e9683a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-03-13 08:18:27 +0000

    Make gov_delivery_id field not nullable
    There are no null instances of this, and there never should be.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308105331) do
+ActiveRecord::Schema.define(version: 20180313081630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,7 +119,7 @@ ActiveRecord::Schema.define(version: 20180308105331) do
 
   create_table "subscriber_lists", id: :serial, force: :cascade do |t|
     t.string "title", limit: 1000, null: false
-    t.string "gov_delivery_id", limit: 255
+    t.string "gov_delivery_id", limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "document_type", default: "", null: false
