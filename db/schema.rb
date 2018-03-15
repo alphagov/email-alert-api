e938e08e8ce56a29226af5b0b55a47d8ea219041
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-03-15 08:51:20 +0000

    Add a unique index on subscription contents
    This will prevent multiple subscription contents for the same
    content change and subscription from being created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315000001) do
+ActiveRecord::Schema.define(version: 20180315084923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20180315000001) do
     t.index ["content_change_id"], name: "index_subscription_contents_on_content_change_id"
     t.index ["digest_run_subscriber_id"], name: "index_subscription_contents_on_digest_run_subscriber_id"
     t.index ["email_id"], name: "index_subscription_contents_on_email_id"
+    t.index ["subscription_id", "content_change_id"], name: "index_subscription_contents_on_subscription_and_content_change", unique: true
     t.index ["subscription_id"], name: "index_subscription_contents_on_subscription_id"
   end
 
