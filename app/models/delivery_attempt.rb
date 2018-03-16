d86f8b1f83be3199d951a1d299fcf830ce5faac1
Kevin Dew <kevindew@me.com>
2018-03-16 21:55:04 +0000

    Remove methods that are not used

--- app/models/delivery_attempt.rb
+++ app/models/delivery_attempt.rb
@@ -8,18 +8,6 @@ class DeliveryAttempt < ApplicationRecord
   enum status: { sending: 0, delivered: 1, permanent_failure: 2, temporary_failure: 3, technical_failure: 4 }
   enum provider: { pseudo: 0, notify: 1 }
 
-  def failure?
-    permanent_failure? || temporary_failure? || technical_failure?
-  end
-
-  def should_report_failure?
-    technical_failure?
-  end
-
-  def should_remove_subscriber?
-    permanent_failure?
-  end
-
   def has_final_status?
     self.class.final_status?(status)
   end
