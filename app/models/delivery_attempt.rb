af54bc20304348067e7ee2d0503d30c670e9f8c8
Kevin Dew <kevindew@me.com>
2018-03-16 11:18:13 +0000

    Retries exhausted failure state for DeliveryAttempt
    This is a state that is used to signify that a delivery attempt will no
    longer be attempted as it had too many failures.

--- app/models/delivery_attempt.rb
+++ app/models/delivery_attempt.rb
@@ -3,13 +3,28 @@ class DeliveryAttempt < ApplicationRecord
 
   validates :email, :status, :provider, presence: true
 
-  FINAL_STATUSES = %i[delivered permanent_failure].freeze
+  FINAL_STATUSES = %i[
+    delivered
+    permanent_failure
+    retries_exhausted_failure
+  ].freeze
 
-  enum status: { sending: 0, delivered: 1, permanent_failure: 2, temporary_failure: 3, technical_failure: 4 }
+  enum status: {
+    sending: 0,
+    delivered: 1,
+    permanent_failure: 2,
+    temporary_failure: 3,
+    technical_failure: 4,
+    retries_exhausted_failure: 5
+  }
+
   enum provider: { pseudo: 0, notify: 1 }
 
   def failure?
-    permanent_failure? || temporary_failure? || technical_failure?
+    permanent_failure? ||
+      temporary_failure? ||
+      technical_failure? ||
+      retries_exhausted_failure?
   end
 
   def should_report_failure?
