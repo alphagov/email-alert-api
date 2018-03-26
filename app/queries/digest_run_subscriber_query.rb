bc04d37b41fb73d4c49868b859c26991dd0bf529
Kevin Dew <kevindew@me.com>
2018-03-26 11:04:17 +0100

    Don't send digests to ended subscriptions
    This fixes the DigestRunSubscriberQuery to only pull out active
    subscriptions which was lost when I switched the scopes around on
    Subscriber.
    
    I've added a test so similar tweaks can be caught.

--- app/queries/digest_run_subscriber_query.rb
+++ app/queries/digest_run_subscriber_query.rb
@@ -1,7 +1,11 @@
 class DigestRunSubscriberQuery
   def self.call(digest_run:)
     Subscriber
-      .joins(subscriptions: { subscriber_list: { matched_content_changes: :content_change } })
+      .joins(
+        active_subscriptions: {
+          subscriber_list: { matched_content_changes: :content_change }
+        }
+      )
       .where("content_changes.created_at >= ?", digest_run.starts_at)
       .where("content_changes.created_at < ?", digest_run.ends_at)
       .where("subscriptions.frequency": digest_run.range)
