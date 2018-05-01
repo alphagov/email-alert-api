a5f5e508743c5b6ccab88e0590f637e3fa332dea
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2018-05-01 17:20:15 +0100

    Update Land Registry titles
    In email subscription alerts about Land Registry, there is some inconsistency where some titles are displayed as 'Land registry' or 'HM Land Registry'.
    
    This migration finds all titles containing 'Land Registry' and replaces them with 'HM Land Registry', and skips those which are already correct.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321125113) do
+ActiveRecord::Schema.define(version: 2018_05_01_155601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
