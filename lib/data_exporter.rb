f0691241016992399256d6682e675f6808216f2a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-05-04 08:50:59 +0100

    Add a task to export statistics for Living in EU taxons

--- lib/data_exporter.rb
+++ lib/data_exporter.rb
@@ -1,6 +1,16 @@
 class DataExporter
   CSV_HEADERS = %i(id title count)
 
+  EU_COUNTRIES = %w(
+    austria belgium bulgaria croatia cyprus czech-republic denmark estonia finland france germany greece hungary
+    ireland italy latvia lithuania luxembourg malta netherlands poland portugal slovakia slovenia spain sweeden
+  )
+
+  def living_in_eu_subscriber_lists
+    slugs = EU_COUNTRIES.map { |country| "living-in-#{country}" }
+    SubscriberList.where(slug: slugs)
+  end
+
   def present_subscriber_list(subscriber_list)
     {
       id: subscriber_list.id,
@@ -9,11 +19,19 @@ class DataExporter
     }
   end
 
-  def export_csv(list_ids)
+  def export_csv(subscriber_lists)
     CSV($stdout, headers: CSV_HEADERS, write_headers: true) do |csv|
-      SubscriberList
-        .where(id: list_ids)
-        .find_each { |subscriber_list| csv << present_subscriber_list(subscriber_list) }
+      subscriber_lists.find_each do |subscriber_list|
+        csv << present_subscriber_list(subscriber_list)
+      end
     end
+  end
+
+  def export_csv_from_ids(ids)
+    export_csv(SubscriberList.where(id: ids))
+  end
+
+  def export_csv_from_living_in_eu
+    export_csv(living_in_eu_subscriber_lists)
   end
 end
--- lib/tasks/export.rake
+++ lib/tasks/export.rake
@@ -1,10 +1,15 @@
 namespace :export do
-  desc "Export the number of subscribers for a collection of lists as a csv, accepts multiple arguments"
-  task :csv, [:subscriber_list_id] => :environment do |_, args|
-    DataExporter.new.export_csv(args.to_a)
+  desc "Export the number of subscriptions for a collection of lists as a csv, accepts multiple arguments"
+  task :csv_from_ids, [] => :environment do |_, args|
+    DataExporter.new.export_csv_from_ids(args.to_a)
   end
 
-  desc "Export the number of subscribers for a list"
+  desc "Export the number of subscriptions for the 'Living in' taxons for EU countries"
+  task :csv_from_living_in_eu, [] => :environment do |_, args|
+    DataExporter.new.export_csv_from_living_in_eu
+  end
+
+  desc "Export the number of subscriptions for a list"
   task :count, [:subscriber_list_id] => :environment do |_, args|
     puts SubscriberList.find(args[:subscriber_list_id]).active_subscriptions_count
   end
