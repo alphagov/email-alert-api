d574b9851bf11e19229deb326607374ced9e14e2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-06-28 10:40:58 +0100

    Revert "Temporarily disable all new healthchecks"
    This reverts commit bcd8267929ddf75a8385fa53706f37c0943299e0.

--- spec/integration/healthcheck_spec.rb
+++ spec/integration/healthcheck_spec.rb
@@ -35,6 +35,8 @@ RSpec.describe "Healthcheck", type: :request do
 
     expect(data.fetch(:checks)).to include(
       database_connectivity: { status: "ok" },
+      content_changes:       { status: "ok", critical: 0, warning: 0 },
+      digest_runs:           { status: "ok", critical: 0, warning: 0 },
       queue_latency:         { status: "ok", queues: a_kind_of(Hash) },
       queue_size:            { status: "ok", queues: a_kind_of(Hash) },
       redis_connectivity:    { status: "ok" },
