d6fb1d8859f8de921be69a953ed7aad86520d5e8
Kevin Dew <kevindew@me.com>
2018-06-29 17:39:50 +0100

    Integrate with EmailArchiveWorker
    This configures the EmailArchiveWorker to send data to S3 as part of the
    archiving process. Once this work is complete this should only archive
    to S3 and not use the EmailArchive table.

--- app/workers/email_archive_worker.rb
+++ app/workers/email_archive_worker.rb
@@ -12,7 +12,7 @@ class EmailArchiveWorker
 
       EmailArchiveQuery.call.in_batches do |batch|
         Email.transaction do
-          archived_count += archive_batch(batch)
+          archived_count += archive_batch(batch.as_json)
         end
       end
 
@@ -24,47 +24,34 @@ private
 
   def archive_batch(batch)
     archived_at = Time.zone.now
+    exported_at = nil
 
-    import_batch(batch, archived_at)
-    Email.where(id: batch.pluck(:id)).update_all(archived_at: archived_at)
+    if ENV.include?("EMAIL_ARCHIVE_S3_ENABLED")
+      send_to_s3(batch, archived_at)
+      exported_at = archived_at
+    end
+
+    import_batch(batch, archived_at, exported_at)
+    mark_emails_as_archived(batch.pluck("id"), archived_at)
   end
 
-  def import_batch(batch, archived_at)
-    to_import = batch.as_json.map { |e| build_email_archive(e, archived_at) }
-    columns = to_import.first.keys.map(&:to_s)
-    values = to_import.map(&:values)
+  def import_batch(batch, archived_at, exported_at)
+    archive = batch.map do |b|
+      EmailArchivePresenter.for_db(b, archived_at, exported_at)
+    end
+
+    columns = archive.first.keys.map(&:to_s)
+    values = archive.map(&:values)
     EmailArchive.import(columns, values, validate: false)
   end
 
-  def build_email_archive(email_data, archived_at)
-    content_change = build_content_change(email_data)
-
-    {
-      archived_at: archived_at,
-      content_change: content_change,
-      created_at: email_data.fetch("created_at"),
-      finished_sending_at: email_data.fetch("finished_sending_at"),
-      id: email_data.fetch("id"),
-      sent: email_data.fetch("sent"),
-      subject: email_data.fetch("subject"),
-      subscriber_id: email_data.fetch("subscriber_id"),
-    }
+  def send_to_s3(batch, archived_at)
+    archive = batch.map { |b| EmailArchivePresenter.for_s3(b, archived_at) }
+    S3EmailArchiveService.call(archive)
   end
 
-  def build_content_change(email_data)
-    return if email_data.fetch("content_change_ids").empty?
-
-    if email_data.fetch("digest_run_ids").count > 1
-      error = "Email with id: #{email_data['id']} is associated with "\
-        "multiple digest runs: #{email_data['digest_run_ids'].join(', ')}"
-      GovukError.notify(error)
-    end
-
-    {
-      content_change_ids: email_data.fetch("content_change_ids"),
-      digest_run_id: email_data.fetch("digest_run_ids").first,
-      subscription_ids: email_data.fetch("subscription_ids"),
-    }
+  def mark_emails_as_archived(ids, archived_at)
+    Email.where(id: ids).update_all(archived_at: archived_at)
   end
 
   def log_complete(archived, start_time, end_time)
