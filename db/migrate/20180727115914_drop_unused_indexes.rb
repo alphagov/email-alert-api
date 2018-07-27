dae7336bea80971cb4c34ef1a9f3b0998b7f71d3
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-07-27 13:03:26 +0100

    Remove unused indexes
    This removes unused indexes saving about 6 GB of space. These were
    identified by using https://wiki.postgresql.org/wiki/Index_Maintenance#Index_size.2Fusage_statistics
    and the results are available at https://gist.github.com/boffbowsh/b52630ddebd51ec4af8ff0dd18ab70e5.

---
+++ db/migrate/20180727115914_drop_unused_indexes.rb
@@ -0,0 +1,11 @@
+class DropUnusedIndexes < ActiveRecord::Migration[5.2]
+  def up
+    remove_index :content_changes, name: :index_content_changes_on_updated_at
+    remove_index :delivery_attempts, name: :index_delivery_attempts_on_updated_at
+    remove_index :emails, name: :index_emails_on_status
+    remove_index :emails, name: :index_emails_on_status_and_archived_at
+    remove_index :emails, name: :index_emails_on_updated_at
+    remove_index :subscription_contents, name: :index_subscription_contents_on_created_at
+    remove_index :subscriptions, name: :index_subscriptions_on_updated_at
+  end
+end
