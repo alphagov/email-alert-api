27cf056a8616338aecfbc0b2abf50719a2292326
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-08-08 10:38:34 +0100

    Introduce a SQL injection vulnerability
    This is to test and show off Brakeman.

--- app/queries/content_change_immediate_subscription_query.rb
+++ app/queries/content_change_immediate_subscription_query.rb
@@ -2,7 +2,7 @@ class ContentChangeImmediateSubscriptionQuery
   def self.call(content_change:)
     Subscription
       .joins(subscriber_list: :matched_content_changes)
-      .where(matched_content_changes: { content_change_id: content_change.id })
+      .where("matched_content_changes.content_change_id = #{content_change.id}")
       .where(frequency: "immediately")
       .active
   end
