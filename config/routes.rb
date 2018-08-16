5b39b397a8318fab444572a4b0aa144100526d0f
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-08-16 11:35:14 +0100

    Move rubocop directives in routes.rb to rubocop.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
+Rails.application.routes.draw do
   scope format: false, defaults: { format: :json } do
     root "welcome#index"
     resources :subscriber_lists, path: "subscriber-lists", only: %i[create]
