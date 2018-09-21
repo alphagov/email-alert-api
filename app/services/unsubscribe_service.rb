7901e03e6ab2d5929770622ad7f66a6806bdee7d
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-09-21 13:05:56 +0100

    Add subscriptions! to the UnsubscribeService
    To support the needs of the BulkUnsubscribeService.

--- app/services/unsubscribe_service.rb
+++ app/services/unsubscribe_service.rb
@@ -8,6 +8,19 @@ module UnsubscribeService
       unsubscribe!(subscription.subscriber, [subscription], reason)
     end
 
+    def subscriptions!(subscriber, subscriptions, reason, ended_email_id: nil)
+      subscription_subscriber_ids = subscriptions.map(&:subscriber_id).uniq
+      raise "Can't process subscriptions for multiple subscribers" unless subscription_subscriber_ids.length == 1
+      raise "Subscriptions don't match subscriber" unless subscription_subscriber_ids.first == subscriber.id
+
+      unsubscribe!(
+        subscriber,
+        subscriptions,
+        reason,
+        ended_email_id: ended_email_id
+      )
+    end
+
     def spam_report!(delivery_attempt)
       subscriber_id = delivery_attempt.email.subscriber_id
       subscriber = Subscriber.find(subscriber_id)
@@ -21,10 +34,16 @@ module UnsubscribeService
 
   private
 
-    def unsubscribe!(subscriber, subscriptions, reason, email_marked_as_spam: nil)
+    def unsubscribe!(
+          subscriber,
+          subscriptions,
+          reason,
+          email_marked_as_spam: nil,
+          ended_email_id: nil
+        )
       ActiveRecord::Base.transaction do
         subscriptions.each do |subscription|
-          subscription.end(reason: reason)
+          subscription.end(reason: reason, ended_email_id: ended_email_id)
         end
 
         email_marked_as_spam.update!(marked_as_spam: true) if email_marked_as_spam && reason == :marked_as_spam
