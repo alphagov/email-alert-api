92a455e0bfb14c700eb4f09d5193f9538d97e057
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-11-21 11:03:23 +0000

    Add the content change injector for business readiness

---
+++ lib/business_readiness/content_change_injector.rb
@@ -0,0 +1,20 @@
+module BusinessReadiness
+  class ContentChangeInjector
+    def initialize(base_paths_with_tags)
+      @base_paths_with_tags = base_paths_with_tags
+    end
+
+    def inject(base_path, existing_tags)
+      new_tags = base_paths_with_tags[base_path]
+      return existing_tags unless new_tags
+
+      existing_tags.merge(new_tags) do |key, value1, value2|
+        value1.concat(value2)
+      end
+    end
+
+  private
+
+    attr_reader :base_paths_with_tags
+  end
+end
---
+++ spec/lib/business_readiness/content_change_injector_spec.rb
@@ -0,0 +1,56 @@
+RSpec.describe BusinessReadiness::ContentChangeInjector do
+  let(:base_path) { "/example" }
+  let(:base_paths_with_tags) do
+    {
+      "/example" => {
+        "appear_in_find_eu_exit_guidance_business_finder" => "yes",
+        "sector_business_area" => [
+          "aerospace",
+          "automotive",
+        ],
+        "intellectual_property" => [
+          "copyright",
+          "patents",
+        ]
+      }
+    }
+  end
+
+  subject { described_class.new(base_paths_with_tags).inject(base_path, existing_tags) }
+
+  context "with no existing tags" do
+    let(:existing_tags) { {} }
+
+    it "should have all the new tags" do
+      expect(subject).to eq(base_paths_with_tags["/example"])
+    end
+  end
+
+  context "with some unrelated existing tags" do
+    let(:existing_tags) do
+      {
+        "unrelated_tag" => ["test"]
+      }
+    end
+
+    it "should have all the new tags and the existing tags" do
+      expect(subject).to eq(
+        base_paths_with_tags["/example"].merge("unrelated_tag" => ["test"])
+      )
+    end
+  end
+
+  context "with some related existing tags" do
+    let(:existing_tags) do
+      {
+        "sector_business_area" => ["chemicals"]
+      }
+    end
+
+    it "should have both the old and new values" do
+      expect(subject["sector_business_area"]).to match_array(
+        %w(aerospace automotive chemicals)
+      )
+    end
+  end
+end
