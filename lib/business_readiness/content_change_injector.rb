dd1003b75060790732431cd18245af32b75b72b4
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-11-21 11:54:07 +0000

    Add more tests for the business readiness injector

--- lib/business_readiness/content_change_injector.rb
+++ lib/business_readiness/content_change_injector.rb
@@ -8,8 +8,14 @@ module BusinessReadiness
       new_tags = base_paths_with_tags[base_path]
       return existing_tags unless new_tags
 
+      return new_tags unless existing_tags
+
       existing_tags.merge(new_tags) do |_, value1, value2|
-        value1.concat(value2)
+        if value2.is_a?(Array)
+          Array(value1).concat(value2)
+        else
+          value2
+        end
       end
     end
 
--- spec/lib/business_readiness/content_change_injector_spec.rb
+++ spec/lib/business_readiness/content_change_injector_spec.rb
@@ -20,6 +20,14 @@ RSpec.describe BusinessReadiness::ContentChangeInjector do
     end
   end
 
+  context "with nil existing tags" do
+    let(:existing_tags) { nil }
+
+    it "should have all the new tags" do
+      expect(subject).to eq(base_paths_with_tags["/example"])
+    end
+  end
+
   context "with some unrelated existing tags" do
     let(:existing_tags) do
       {
@@ -35,16 +43,78 @@ RSpec.describe BusinessReadiness::ContentChangeInjector do
   end
 
   context "with some related existing tags" do
-    let(:existing_tags) do
-      {
-        "sector_business_area" => %w(chemicals)
-      }
+    context "as an array" do
+      let(:existing_tags) do
+        { "sector_business_area" => %w(chemicals) }
+      end
+
+      it "should have both the old and new values" do
+        expect(subject["sector_business_area"]).to match_array(
+          %w(aerospace automotive chemicals)
+        )
+      end
     end
 
-    it "should have both the old and new values" do
-      expect(subject["sector_business_area"]).to match_array(
-        %w(aerospace automotive chemicals)
-      )
+    context "as a string" do
+      let(:existing_tags) do
+        { "sector_business_area" => "chemicals" }
+      end
+
+      it "should have both the old and new values" do
+        expect(subject["sector_business_area"]).to match_array(
+          %w(aerospace automotive chemicals)
+        )
+      end
+    end
+
+    context "as nil" do
+      let(:existing_tags) do
+        { "sector_business_area" => nil }
+      end
+
+      it "should have the new values" do
+        expect(subject["sector_business_area"]).to match_array(
+          %w(aerospace automotive)
+        )
+      end
+    end
+
+    context "and appear_in_find_eu_exit_guidance_business_finder tag" do
+      let(:existing_tags) do
+        {
+          "appear_in_find_eu_exit_guidance_business_finder" => "no"
+        }
+      end
+
+      it "should come back as yes" do
+        expect(subject["appear_in_find_eu_exit_guidance_business_finder"]).to eq("yes")
+      end
+    end
+  end
+
+  context "without matching base path" do
+    let(:base_path) { "/no-example" }
+
+    context "it passes back whatever was given to it" do
+      context "when nil" do
+        let(:existing_tags) { nil }
+        it { is_expected.to eq(nil) }
+      end
+
+      context "when an empty hash" do
+        let(:existing_tags) { {} }
+        it { is_expected.to eq({}) }
+      end
+
+      context "when a non-empty hash" do
+        let(:existing_tags) { { tag: %w(value) } }
+        it { is_expected.to eq(tag: %w(value)) }
+      end
+
+      context "when a string" do
+        let(:existing_tags) { "tag" }
+        it { is_expected.to eq("tag") }
+      end
     end
   end
 end
