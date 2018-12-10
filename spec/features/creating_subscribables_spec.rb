f11895ea13425ebb595186930e0da9c1e426c7a4
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-12-10 15:51:12 +0000

    Ensure that creating and finding subscriber lists with legacy params work
    Many clients still use legacy style parameters to create and find
    subscriber lists. This commit inserts an 'any' key in those params
    so that all clients continue to work

--- spec/features/creating_subscribables_spec.rb
+++ spec/features/creating_subscribables_spec.rb
@@ -18,4 +18,28 @@ RSpec.describe "Creating subscribables", type: :request do
     gov_delivery_id = data.dig(:subscriber_list, :gov_delivery_id)
     expect(gov_delivery_id).to eq("example")
   end
+
+  scenario "creating and looking up legacy subscribables" do
+    login_with(%w(internal_app status_updates))
+
+    legacy_params = { title: "example", tags: {}, links: { person: ["test-123"] } }
+    new_params = { title: "example", tags: {}, links: { person: { any: ["test-123"] } } }
+
+    lookup_subscribable("example", expected_status: 404)
+    lookup_subscriber_list(legacy_params, expected_status: 404)
+
+    create_subscribable(title: "example", links: { person: ["test-123"] })
+
+    lookup_subscribable("example", expected_status: 200)
+    expect(data.fetch(:subscribable)).to include(new_params)
+
+    lookup_subscriber_list(new_params, expected_status: 200)
+    expect(data.fetch(:subscriber_list)).to include(new_params)
+
+    lookup_subscriber_list(legacy_params, expected_status: 200)
+    expect(data.fetch(:subscriber_list)).to include(new_params)
+
+    gov_delivery_id = data.dig(:subscriber_list, :gov_delivery_id)
+    expect(gov_delivery_id).to eq("example")
+  end
 end
