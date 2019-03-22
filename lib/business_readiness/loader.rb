5339a2e93c42e7df2874534c735015d4121a3684
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-03-22 11:17:19 +0000

    Only add filter tag to content items
    Only add appear_in_find_eu_exit_guidance_business_finder tag to content
    items.
    
    Users can now only subscribe to the full list of items returned by the
    business readiness finder. That means that email-alert-api no longer
    needs to update content changes with the facets from the finder.
    The only tag that still needs to be applied is
    appear_in_find_eu_exit_guidance_business_finder.

--- lib/business_readiness/loader.rb
+++ lib/business_readiness/loader.rb
@@ -2,21 +2,15 @@ require "csv"
 
 module BusinessReadiness
   class Loader
-    def initialize(filename, facets_filename)
+    def initialize(filename)
       @rows = CSV.read(filename, converters: lambda { |v| v || "" })
-      @facets = {}
-
-      if File.exist?(facets_filename)
-        facet_config = YAML.load_file(facets_filename)
-        @facets = facet_config["details"]["facets"]
-      end
     end
 
     def base_paths_with_tags
       rows.each_with_object({}) do |row, hash|
         base_path = row[0]
 
-        tags = tags_for_row(row)
+        tags = {}
         tags["appear_in_find_eu_exit_guidance_business_finder"] = "yes"
 
         hash[base_path] = tags
@@ -25,18 +19,6 @@ module BusinessReadiness
 
   private
 
-    attr_reader :facets, :rows
-
-    def tags_for_row(row)
-      tags = {}
-      facets.each_with_index do |facet, index|
-        row_index = index + 1
-        tags[facet["key"]] = row.fetch(row_index, "").split(",").map(&:strip)
-      end
-
-      tags.reject do |_, value|
-        value == []
-      end
-    end
+    attr_reader :rows
   end
 end
