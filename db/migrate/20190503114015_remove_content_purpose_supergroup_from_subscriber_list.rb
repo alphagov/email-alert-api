329b8cc29935fb1587a412cc52230acc35cac63e
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-05-15 11:09:25 +0100

    Remove remove_content_purpose_supergroup_from_subscriber_list migration
    This migration cannot be simply deployed in production because
    Rails caches these columns and accessing them will cause errors
    when hot-deploying

--- db/migrate/20190503114015_remove_content_purpose_supergroup_from_subscriber_list.rb
+++
@@ -1,5 +0,0 @@
-class RemoveContentPurposeSupergroupFromSubscriberList < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :subscriber_lists, :content_purpose_supergroup, :string, limit: 100
-  end
-end
