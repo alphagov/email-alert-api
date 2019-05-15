8fc83a06446dc46dc6dc4da773640088c1623d03
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-05-15 16:07:16 +0100

    Implements FindLatestMatchingSubscription query & tests
    This writes a query which fetches the most recent subscription that
    matches the given subscription, i.e. has the same subscriber, subscription
    list and frequency. In most cases this will simply return the same
    subscription, but it's intended to be used to detect unsubscribe requests
    from old subscription emails that the user may have clicked on, as this is
    the same subscription conceptually.
    
    Assumptions:
    
    - When a user changes their subscription through frequency change
    - And when a user changes through unsubscribing and resubscribing
    - A new subscription is created every time, with a more recent 'created_at'
    - In other words, it's not possible for an older record that has become
    inactive to become active again. It will always be a new subscription

--- app/queries/find_latest_matching_subscription.rb
+++ app/queries/find_latest_matching_subscription.rb
@@ -1,9 +1,11 @@
 class FindLatestMatchingSubscription
-  def self.call
-    new.call
-  end
-
-  def call(subscription)
-    # TODO - find the latest subscription that matches the given subscription's subscription_list and email address
+  def self.call(original_subscription)
+    subscriber_list_id = original_subscription.subscriber_list_id
+    subscriber_id = original_subscription.subscriber_id
+    frequency = original_subscription.frequency
+    Subscription.
+      where(subscriber_list_id: subscriber_list_id, subscriber_id: subscriber_id, frequency: frequency).
+      order("created_at DESC")
+      .first
   end
 end
