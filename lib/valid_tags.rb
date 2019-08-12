eec9d2a8533a04d4cc260410eb7bde8e6e160c87
William Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-08-12 16:03:52 +0000

    Add additional permitted tags
    This is the result of an audit of the applications that can
    send tags downstream to email alert api. It looks like many
    of the permitted tags haven't been used yet.
    
    See https://gov-uk.atlassian.net/wiki/spaces/TECH/pages/17989732/Email+alert+technical+overview
    for more details of the architecture.

--- lib/valid_tags.rb
+++ lib/valid_tags.rb
@@ -1,7 +1,79 @@
 class ValidTags
-  ALLOWED_TAGS = %i(vessel_type format case_type sector_business_area business_activity
-                    employ_eu_citizens personal_data intellectual_property public_sector_procurement
-                    location eu_uk_government_funding alert_type tribunal_decision_category policies
-                    topics content_store_document_type content_purpose_supergroup railway_type
-                    aircraft_category content_purpose_subgroup appear_in_find_eu_exit_guidance_business_finder).freeze
+  ALLOWED_TAGS = %i(
+    aircraft_category
+    aircraft_type
+    alert_type
+    appear_in_find_eu_exit_guidance_business_finder
+    assessment_date
+    business_activity
+    business_sizes
+    business_stages
+    case_state
+    case_type
+    certificate_status
+    closed_date
+    closing_date
+    commodity_type
+    content_purpose_subgroup
+    content_purpose_supergroup
+    content_store_document_type
+    continuation_link
+    country
+    date_of_occurrence
+    destination_country
+    development_sector
+    dfid_authors
+    dfid_document_type
+    dfid_review_status
+    dfid_theme
+    eligible_entities
+    employ_eu_citizens
+    eu_uk_government_funding
+    first_published_at
+    format
+    fund_state
+    fund_type
+    funding_amount
+    funding_source
+    grant_type
+    hidden_indexable_content
+    industries
+    intellectual_property
+    issued_date
+    laid_date
+    land_use
+    location
+    market_sector
+    medical_specialism
+    opened_date
+    outcome_type
+    personal_data
+    policies
+    public_sector_procurement
+    railway_type
+    regions
+    registration
+    report_type
+    sector_business_area
+    sift_end_date
+    sifting_status
+    subject
+    therapeutic_area
+    tiers_or_standalone_items
+    topics
+    tribunal_decision_categories
+    tribunal_decision_category
+    tribunal_decision_country
+    tribunal_decision_decision_date
+    tribunal_decision_judges
+    tribunal_decision_landmark
+    tribunal_decision_reference_number
+    tribunal_decision_sub_categories
+    tribunal_decision_sub_category
+    types_of_support
+    value_of_funding
+    vessel_type
+    will_continue_on
+    withdrawn_date
+  ).freeze
 end
