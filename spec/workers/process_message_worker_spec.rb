a6f7c62ee0dce07398514aabd6cfc6c054730fa8
Kevin Dew <kevindew@me.com>
2019-08-16 11:50:54 +0100

    Add incomplete ProcessMessageWorker
    This is used to generate SubscriptionContent for messages. This class
    can be completed once we have means to generate message emails.

---
+++ spec/workers/process_message_worker_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe ProcessMessageWorker do
+  let(:message) { create(:message, tags: { topics: { any: ["oil-and-gas/licensing"] } }) }
+  let(:email) { create(:email) }
+
+  context "with a subscription" do
+    let(:subscriber) { create(:subscriber) }
+    let(:subscriber_list) { create(:subscriber_list, tags: { topics: { any: ["oil-and-gas/licensing"] } }) }
+    let!(:matched_message) { create(:matched_message, subscriber_list: subscriber_list, message: message) }
+    let!(:subscription) { create(:subscription, subscriber: subscriber, subscriber_list: subscriber_list) }
+
+    it "creates subscription content for the message" do
+      expect { subject.perform(message.id) }
+        .to change { SubscriptionContent.count }
+        .by(1)
+    end
+
+    it "marks the message as processed" do
+      expect { subject.perform(message.id) }
+        .to change { message.reload.processed? }
+        .to(true)
+    end
+
+    context "when the subscription content has already been imported for the message" do
+      before do
+        create(:subscription_content,
+               :with_message,
+               message: message,
+               subscription: subscription)
+      end
+
+      it "doesn't create another subscription content" do
+        expect { subject.perform(message.id) }
+          .to_not(change { SubscriptionContent.count })
+      end
+    end
+  end
+
+  context "with an already processed message" do
+    before { message.mark_processed! }
+
+    it "shouldn't process the message" do
+      expect(message).to_not receive(:mark_processed!)
+      subject.perform(message.id)
+    end
+  end
+end
