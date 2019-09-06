02e82556800bcde7494e57ed6477d2cb7e22a9ca
Kevin Dew <kevindew@me.com>
2019-09-06 17:52:10 +0100

    Cater for absolute URLs in message presenter
    This doesn't change the host and scheme of existing absolute URLs. It
    adds analytics tracking to URLs in the current GOV.UK environment.
    Finally it doesn't add analytics query string params if a URL already
    has some of these.

--- app/presenters/message_presenter.rb
+++ app/presenters/message_presenter.rb
@@ -23,7 +23,24 @@ private
 
   delegate :title, :body, :url, to: :message
 
-  def content_url
+  def absolute_url
+    return unless url
+
+    parsed = URI.join(Plek.new.website_root, url)
+    add_ga_query_params(parsed)
+
+    parsed.to_s
+  rescue URI::InvalidURIError
+    nil
+  end
+
+  def add_ga_query_params(uri)
+    return unless uri.to_s.start_with?(Plek.new.website_root)
+
+    query_params = Rack::Utils.parse_nested_query(uri.query)
+
+    return if query_params.keys.any? { |k| k.match(/\Autm/) }
+
     query = {
       utm_source: message.id,
       utm_medium: "email",
@@ -31,14 +48,16 @@ private
       utm_content: frequency,
     }.to_query
 
-    tracked_url = url + (url.include?("?") ? "&" : "?") + query
-
-    PublicUrlService.url_for(base_path: tracked_url)
+    if uri.query
+      uri.query += "&#{query}"
+    else
+      uri.query = query
+    end
   end
 
   def title_markdown
-    return title unless url
+    return title unless (destination = absolute_url)
 
-    "[#{title}](#{content_url})"
+    "[#{title}](#{destination})"
   end
 end
--- spec/presenters/message_presenter_spec.rb
+++ spec/presenters/message_presenter_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe MessagePresenter do
   describe ".call" do
+    around do |example|
+      ClimateControl.modify(GOVUK_APP_DOMAIN: 'gov.uk') { example.run }
+    end
+
     it "returns a presenter message" do
       message = create(:message,
                        title: "My title",
@@ -24,7 +28,7 @@ RSpec.describe MessagePresenter do
                        body: "Some information\nfor a user")
 
       expected = <<~MESSAGE
-        [My title](http://www.dev.gov.uk/my-page?#{message_utm_params(message.id, 'immediate')})
+        [My title](https://www.gov.uk/my-page?#{message_utm_params(message.id, 'immediate')})
 
         Some information
         for a user
@@ -40,7 +44,55 @@ RSpec.describe MessagePresenter do
                        body: "Some information\nfor a user")
 
       expected = <<~MESSAGE
-        [My title](http://www.dev.gov.uk/my-page?my-query=this&#{message_utm_params(message.id, 'immediate')})
+        [My title](https://www.gov.uk/my-page?my-query=this&#{message_utm_params(message.id, 'immediate')})
+
+        Some information
+        for a user
+      MESSAGE
+
+      expect(described_class.call(message)).to eq(expected)
+    end
+
+    it "doesn't replace the host of an absolute URL" do
+      message = create(:message,
+                       title: "My title",
+                       url: "https://other-government-service/test",
+                       body: "Some information\nfor a user")
+
+      expected = <<~MESSAGE
+        [My title](https://other-government-service/test)
+
+        Some information
+        for a user
+      MESSAGE
+
+      expect(described_class.call(message)).to eq(expected)
+    end
+
+    it "doesn't add Google Analytics params to a URL which already has this" do
+      message = create(:message,
+                       title: "My title",
+                       url: "/test?utm_source=custom-source",
+                       body: "Some information\nfor a user")
+
+      expected = <<~MESSAGE
+        [My title](https://www.gov.uk/test?utm_source=custom-source)
+
+        Some information
+        for a user
+      MESSAGE
+
+      expect(described_class.call(message)).to eq(expected)
+    end
+
+    it "adds Google Analytics params to an absolute GOV.UK URL" do
+      message = create(:message,
+                       title: "My title",
+                       url: "https://www.gov.uk/test",
+                       body: "Some information\nfor a user")
+
+      expected = <<~MESSAGE
+        [My title](https://www.gov.uk/test?#{message_utm_params(message.id, 'immediate')})
 
         Some information
         for a user
