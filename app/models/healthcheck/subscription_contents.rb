b12851feb11a8e07dcd31938f46864d35297d9f2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-01 08:30:36 +0100

    Increase subscription contents latency thresholds
    During periods of scheduled publishing we often see this alert trigger
    because the thresholds are currently set too low.

--- app/models/healthcheck/subscription_contents.rb
+++ app/models/healthcheck/subscription_contents.rb
@@ -68,11 +68,11 @@ module Healthcheck
     end
 
     def critical_latency
-      is_scheduled_publishing_time? ? 45.minutes : 15.minutes
+      is_scheduled_publishing_time? ? 50.minutes : 15.minutes
     end
 
     def warning_latency
-      is_scheduled_publishing_time? ? 30.minutes : 10.minutes
+      is_scheduled_publishing_time? ? 35.minutes : 10.minutes
     end
 
     # There's a lot of scheduled publishing at 09:30 UK time, so we
