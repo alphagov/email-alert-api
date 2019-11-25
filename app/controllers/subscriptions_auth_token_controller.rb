ed7682f0939b9f10ea7641863be00da35b2a07c5
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-11-25 15:58:55 +0000

    Validate address, frequency and topic_id params
    As part of sending a double-opt-in email, we need a well formed
    email address, a valid frequency and a topic.

--- app/controllers/subscriptions_auth_token_controller.rb
+++ app/controllers/subscriptions_auth_token_controller.rb
@@ -1,5 +1,23 @@
 class SubscriptionsAuthTokenController < ApplicationController
+  before_action :validate_params
+
   def auth_token
     render json: {}, status: :ok
+  end
+
+  def validate_params
+    ParamsValidator.new(expected_params).validate!
+  end
+
+  def expected_params
+    params.permit(:address, :topic_id, :frequency)
+  end
+
+  class ParamsValidator < OpenStruct
+    include ActiveModel::Validations
+
+    validates :address, email_address: true, presence: true
+    validates :topic_id, presence: true
+    validates :frequency, presence: true, inclusion: { in: Subscription.frequencies.keys }
   end
 end
--- spec/integration/subscriptions_auth_token_spec.rb
+++ spec/integration/subscriptions_auth_token_spec.rb
@@ -3,10 +3,61 @@ RSpec.describe "Subscriptions auth token", type: :request do
 
   describe "creating an auth token" do
     let(:path) { "/subscriptions/auth-token" }
+    let(:address) { "test@example.com" }
+    let(:topic_id) { "business-tax-corporation-tax" }
+    let(:frequency) { "weekly" }
+    let(:params) do
+      {
+        address: address,
+        topic_id: topic_id,
+        frequency: frequency,
+      }
+    end
 
     it "returns 200" do
-      post path
+      post path, params: params
       expect(response.status).to eq(200)
+    end
+
+    context "when we're provided with no email address" do
+      let(:address) { nil }
+
+      it "returns a 422" do
+        post path, params: params
+        expect(response.status).to eq(422)
+      end
+    end
+    context "when we're provided with a badly formatted email address" do
+      let(:address) { "wrong.bad" }
+
+      it "returns a 422" do
+        post path, params: params
+        expect(response.status).to eq(422)
+      end
+    end
+    context "when we're provided with no topic_id" do
+      let(:topic_id) { nil }
+
+      it "returns a 422" do
+        post path, params: params
+        expect(response.status).to eq(422)
+      end
+    end
+    context "when we're provided with no frequency" do
+      let(:frequency) { nil }
+
+      it "returns a 422" do
+        post path, params: params
+        expect(response.status).to eq(422)
+      end
+    end
+    context "when we're provided with a bad frequency" do
+      let(:frequency) { "something_else" }
+
+      it "returns a 422" do
+        post path, params: params
+        expect(response.status).to eq(422)
+      end
     end
   end
 end
