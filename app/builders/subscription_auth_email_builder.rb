718cf72e194a069d08d1fde5433786424af59c05
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-11-27 15:47:56 +0000

    Add subscription email confirmation builder
    This allows an email to be constructed to ask the user to
    confirm their subscription.

---
+++ app/builders/subscription_auth_email_builder.rb
@@ -0,0 +1,55 @@
+class SubscriptionAuthEmailBuilder
+  def initialize(address:, token:, topic_id:, frequency:)
+    @address = address
+    @token = token
+    @topic_id = topic_id
+    @frequency = frequency
+  end
+
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def call
+    Email.create!(
+      subject: subject,
+      body: body,
+      address: address,
+      )
+  end
+
+  private_class_method :new
+
+private
+
+  attr_reader :address, :token, :frequency, :topic_id
+
+  def subject
+    "Confirm your subscription"
+  end
+
+  def body
+    <<~BODY
+      # Click the link to confirm your subscription
+
+      ^[Confirm your subscription](#{link})^
+
+      This link will stop working in 7 days.
+
+      **Didn’t request this email?**
+
+      Ignore or delete this email if you didn’t request it.
+
+      [Read our privacy policy (opens in a new tab)](https://www.gov.uk/help/privacy-notice) to find out how we use and protect your data.
+
+      [Contact GOV.UK](https://www.gov.uk/contact/govuk) if you have any problems with your email subscriptions.
+    BODY
+  end
+
+  def link
+    Plek.new.website_uri.tap do |uri|
+      uri.path = "/email/subscriptions/authenticate"
+      uri.query = "token=#{token}&topic_id=#{topic_id}&frequency=#{frequency}"
+    end
+  end
+end
---
+++ spec/builders/subscription_auth_email_builder_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe SubscriptionAuthEmailBuilder do
+  describe ".call" do
+    let(:address) { "test@gov.uk" }
+    let(:token) { "secret" }
+    let(:frequency) { "weekly" }
+    let(:topic_id) { "business-tax-corporation-tax" }
+
+    subject(:call) do
+      described_class.call(
+        address: address,
+        token: token,
+        topic_id: topic_id,
+        frequency: frequency,
+        )
+    end
+
+    it { is_expected.to be_instance_of(Email) }
+
+    it "creates an email" do
+      expect { call }.to change(Email, :count).by(1)
+    end
+
+    it "has a link to authenticate" do
+      link = "http://www.dev.gov.uk/email/subscriptions/authenticate?token=#{token}&topic_id=business-tax-corporation-tax&frequency=weekly"
+      email = call
+      expect(email.body).to include(link)
+    end
+  end
+end
