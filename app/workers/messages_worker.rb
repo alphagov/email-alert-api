83d4cdd0befc703afd9e2dd555ceeb9ade5af971
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-01-08 15:25:51 +0000

    Record stats on messages
    We are moving away from using healthchecks to monitor specific
    things as they were putting unnecessary load on the database on
    each machine and triggering duplicate Icinga alerts.
    
    This sidekiq job will run every 1 minute and send the total number
    of unprocessed messages that were created over 5 minutes ago (`warning`)
    and over 10 minutes ago (`critical`).
    
    We can then configure an Icinga alert in `Puppet` which will trigger
    when the number is more than 0. This will no longer be machine
    specific and come through from the monitoring machine.

---
+++ app/workers/messages_worker.rb
@@ -0,0 +1,35 @@
+class MessagesWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :cleanup
+
+  def perform
+    GlobalMetricsService.critical_messages_total(critical_messages)
+    GlobalMetricsService.warning_messages_total(warning_messages)
+  end
+
+private
+
+  def critical_messages
+    @critical_messages ||= count_messages(critical_latency)
+  end
+
+  def warning_messages
+    @warning_messages ||= count_messages(warning_latency)
+  end
+
+  def count_messages(age)
+    Message
+    .where("created_at < ?", age.ago)
+    .where(processed_at: nil)
+    .count
+  end
+
+  def critical_latency
+    10.minutes
+  end
+
+  def warning_latency
+    5.minutes
+  end
+end
