0c17e757179cb4b0308bf0b49bb3b19842c15bc2
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-01-09 10:44:18 +0000

    Remove Messages from healthcheck
    The `email-alert-api app healthcheck not ok` icinga alert often
    fails due to a timeour error because some of the checks are slow.
    
    We are removing this healthcheck in favour of a Sidekiq job and
    Icinga check. This will allow us to send data to Graphite so that
    we can monitor the number of messages with `critical` and `warning`
    latency.

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -5,7 +5,6 @@ class HealthcheckController < ApplicationController
     healthcheck = GovukHealthcheck.healthcheck([
                     GovukHealthcheck::SidekiqRedis,
                     GovukHealthcheck::ActiveRecord,
-                    Healthcheck::Messages,
                     Healthcheck::DigestRuns,
                     Healthcheck::QueueLatency,
                     Healthcheck::QueueSize,
