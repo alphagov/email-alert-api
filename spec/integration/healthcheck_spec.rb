ba16df5aba9dace81b3ea3666a5e668ccd542efd
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-01-17 09:08:49 +0000

    Remove queue latency from healthcheck
    The email-alert-api app healthcheck not ok icinga alert often
    fails due to a timeour error because some of the checks are slow.
    
    We are replacing this healthcheck with individual Icinga checks in
    puppet, see https://github.com/alphagov/govuk-puppet/pull/10069
    for more information.

--- spec/integration/healthcheck_spec.rb
+++ spec/integration/healthcheck_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe "Healthcheck", type: :request do
     expect(data.fetch(:checks)).to include(
       database_connectivity: { status: "ok" },
       redis_connectivity:    { status: "ok" },
-      sidekiq_queue_latency: hash_including(status: "ok", queues: a_kind_of(Hash)),
       sidekiq_retry_size:    hash_including(status: "ok", value: 0),
     )
   end
