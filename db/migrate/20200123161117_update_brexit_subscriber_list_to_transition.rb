48d6ef176f78061025c528c46c5dd83e1a72a874
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-01-23 16:15:23 +0000

    Update the brexit subscriber list to transition
    Need to update the slug and title from brexit to transition

---
+++ db/migrate/20200123161117_update_brexit_subscriber_list_to_transition.rb
@@ -0,0 +1,17 @@
+class UpdateBrexitSubscriberListToTransition < ActiveRecord::Migration[5.2]
+  def up
+    subscriber_list = SubscriberList.find_by_slug("brexit-p")
+    return if subscriber_list.nil?
+    subscriber_list.slug = "transition-p"
+    subscriber_list.title = "Transition"
+    subscriber_list.save
+  end
+
+  def down
+    subscriber_list = SubscriberList.find_by_slug("transition-p")
+    return if subscriber_list.nil?
+    subscriber_list.slug = "brexit-p"
+    subscriber_list.title = "Brexit"
+    subscriber_list.save
+  end
+end
