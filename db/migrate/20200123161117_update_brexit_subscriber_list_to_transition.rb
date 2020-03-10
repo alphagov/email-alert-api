28f826afa734aa20681e9a9f1b79dd2c0bc57e31
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-03-10 14:00:25 +0000

    Rubocop: fix issues automatically with -a

--- db/migrate/20200123161117_update_brexit_subscriber_list_to_transition.rb
+++ db/migrate/20200123161117_update_brexit_subscriber_list_to_transition.rb
@@ -2,6 +2,7 @@ class UpdateBrexitSubscriberListToTransition < ActiveRecord::Migration[5.2]
   def up
     subscriber_list = SubscriberList.find_by_slug("brexit-p")
     return if subscriber_list.nil?
+
     subscriber_list.slug = "transition-p"
     subscriber_list.title = "Transition"
     subscriber_list.save
@@ -10,6 +11,7 @@ class UpdateBrexitSubscriberListToTransition < ActiveRecord::Migration[5.2]
   def down
     subscriber_list = SubscriberList.find_by_slug("transition-p")
     return if subscriber_list.nil?
+
     subscriber_list.slug = "brexit-p"
     subscriber_list.title = "Brexit"
     subscriber_list.save
