4eefade4520f4ceca5b17c7a62092c1df5031a6f
Kevin Dew <kevindew@me.com>
2020-06-10 12:02:19 +0100

    Remove ContentChangesForEmailQuery
    It isn't used by anything anymore.

--- app/queries/content_changes_for_email_query.rb
+++
@@ -1,8 +0,0 @@
-class ContentChangesForEmailQuery
-  def self.call(email)
-    content_change_ids = SubscriptionContent
-        .where(email: email)
-        .select(:content_change_id)
-    ContentChange.where(id: content_change_ids)
-  end
-end
