1aff0ff0c391a574c2adf7b11714f668386ab5b0
Kevin Dew <kevindew@me.com>
2020-06-15 14:28:15 +0100

    Remove unnecessary test
    This was testing part of the activerecord-import gem rather than code
    we'd wrote.

--- spec/models/subscription_content_spec.rb
+++ spec/models/subscription_content_spec.rb
@@ -26,40 +26,4 @@ RSpec.describe SubscriptionContent do
       expect(subscription_content).to be_invalid
     end
   end
-
-  describe ".import!" do
-    let(:content_changes) { create_list(:content_change, 15) }
-    let(:subscription) { create(:subscription) }
-    let(:columns) { %i[content_change_id subscription_id] }
-    let(:rows) { content_changes.map { |c| [c.id, subscription.id] } }
-
-    it "can import a lot of items" do
-      expect {
-        described_class.import!(columns,
-                                rows,
-                                on_duplicate_key_ignore: true,
-                                batch_size: 5)
-      }
-        .to change { SubscriptionContent.count }
-        .by(15)
-    end
-
-    it "can recover when there are duplicates" do
-      rows.shuffle.take(5).each do |(content_change_id, subscription_id)|
-        create(
-          :subscription_content,
-          content_change_id: content_change_id,
-          subscription_id: subscription_id,
-        )
-      end
-
-      expect {
-        described_class.import!(columns,
-                                rows,
-                                on_duplicate_key_ignore: true)
-      }
-        .to change { SubscriptionContent.count }
-        .by(10)
-    end
-  end
 end
