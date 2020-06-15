03d616ee6c2f53ec8345f2f00e3c16328b3378bc
Kevin Dew <kevindew@me.com>
2020-06-15 14:46:11 +0100

    Remove unnecessary BulkEmailSenderService
    This service seemed to be rather strange in how little work it did so
    I've moved it's code to be handled in the rake task.

--- spec/lib/tasks/bulk_spec.rb
+++ spec/lib/tasks/bulk_spec.rb
@@ -12,16 +12,32 @@ RSpec.describe "bulk" do
       end
     end
 
-    it "sends emails to a subscription list" do
+    it "builds emails for a subscriber list" do
       subscriber_list = create(:subscriber_list)
 
-      expect(BulkEmailSenderService).to receive(:call).with(
-        bulk_email_builder: BulkEmailBuilder.call(
-          subject: "subject",
-          body: "body",
-          subscriber_lists: subscriber_list,
-        ),
-      )
+      expect(BulkEmailBuilder)
+        .to receive(:call)
+        .with(subject: "subject",
+              body: "body",
+              subscriber_lists: [subscriber_list])
+        .and_call_original
+
+      Rake::Task["bulk:email"].invoke(subscriber_list.id)
+    end
+
+    it "enqueues the emails for delivery" do
+      subscriber_list = create(:subscriber_list)
+
+      allow(BulkEmailBuilder).to receive(:call)
+                              .and_return([1, 2])
+
+      expect(DeliveryRequestWorker)
+        .to receive(:perform_async_in_queue)
+        .with(1, queue: :delivery_immediate)
+
+      expect(DeliveryRequestWorker)
+        .to receive(:perform_async_in_queue)
+        .with(2, queue: :delivery_immediate)
 
       Rake::Task["bulk:email"].invoke(subscriber_list.id)
     end
