4c0e479cc5d4af5b1694f264d9ec459126b16d6d
Kevin Dew <kevindew@me.com>
2020-06-15 15:02:26 +0100

    Remove the SubscriptionContent presence validation
    This validation no longer has an effect since switching to use
    ActiveRecord#insert_all to insert records. It's a bit annoying to lose
    this as this is used to maintain a degree of database consistency.
    
    Ideally, we'd replace this with a PostgreSQL constraint check, for
    example: `CHECK (num_nonnulls(content_change_id, message_id) = 1);` [1]
    however we can't apply a PostgreSQL constraint without switching from
    the regular db/schema.rb to db/schema.sql which is less conventional and
    much harder to work with (for example it specifies db version which
    makes it difficult to upgrade the db in a backwards compatible way).
    
    I'm thinking the likelihood of this check to be needed is low and it's
    mostly just a safety net for code mistakes, I'm thinking we can probably
    survive without it. If it becomes a problem we could always add a gem
    [2] if we feel we need it.
    
    [1]: https://stackoverflow.com/a/55223096
    [2]: https://github.com/on-site/active_record-postgres-constraints

--- app/models/subscription_content.rb
+++ app/models/subscription_content.rb
@@ -1,25 +1,12 @@
 class SubscriptionContent < ApplicationRecord
   belongs_to :subscription
-  belongs_to :content_change, optional: true
-  belongs_to :message, optional: true
   belongs_to :digest_run_subscriber, optional: true
   belongs_to :email, optional: true
 
+  # A subscription content should always have one of these and not both
+  belongs_to :content_change, optional: true
+  belongs_to :message, optional: true
+
   scope :immediate, -> { where(digest_run_subscriber_id: nil) }
   scope :digest, -> { where.not(digest_run_subscriber_id: nil) }
-
-  validate :presence_of_content_change_or_message
-
-  def presence_of_content_change_or_message
-    has_content_change = content_change_id.present? || content_change.present?
-    has_message = message_id.present? || message.present?
-
-    if has_content_change && has_message
-      errors.add(:base, "cannot be associated with a content_change and a message")
-    end
-
-    if !has_content_change && !has_message
-      errors.add(:base, "must be associated with a content_change or a message")
-    end
-  end
 end
