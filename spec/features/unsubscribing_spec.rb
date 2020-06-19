43131f94329163e4d26434e68fb37fde213ba61e
Kevin Dew <kevindew@me.com>
2020-06-19 20:39:30 +0100

    Refactor tests to avoid nested time freezing
    ActiveSupport::Testing::TimeHelpers raises an error when you nested a
    frozen time inside another. This seems a good thing since tests that do
    this can be very hard to understand. I've refactored this test to change
    the time helper and hopefully made it a little easier to follow.

--- spec/features/unsubscribing_spec.rb
+++ spec/features/unsubscribing_spec.rb
@@ -4,37 +4,31 @@ RSpec.describe "Unsubscribing from a subscriber_list", type: :request do
   end
 
   scenario "unsubscribing from an email uuid, then no longer receiving emails" do
-    Timecop.freeze "2019-06-26 10:00" do
-      login_with_internal_app
+    login_with_internal_app
+    subscriber_list_id = create_subscriber_list
+    subscribe_to_subscriber_list(subscriber_list_id, frequency: "daily")
 
-      subscriber_list_id = create_subscriber_list
-      subscribe_to_subscriber_list(subscriber_list_id, frequency: "daily")
+    travel_to(Time.zone.yesterday.midday) { create_content_change }
 
-      Timecop.freeze "2019-06-25 09:30:00" do
-        create_content_change(public_updated_at: "2019-06-25 10:00:00")
-      end
-
+    travel_to(Time.zone.today.midday) do
       DailyDigestInitiatorWorker.new.perform
       Sidekiq::Worker.drain_all
-
-      email_data = expect_an_email_was_sent
-
-      id = extract_unsubscribe_id(email_data)
-      unsubscribe_from_subscriber_list(id, expected_status: 204)
-
-      clear_any_requests_that_have_been_recorded!
-
-      Timecop.freeze "2019-06-25 09:30:01" do
-        create_content_change(public_updated_at: "2019-06-25 10:00:00")
-      end
-
-      DailyDigestInitiatorWorker.new.perform
-      Sidekiq::Worker.drain_all
-
-      expect_an_email_was_not_sent
-
-      unsubscribe_from_subscriber_list(id, expected_status: 404)
-      unsubscribe_from_subscriber_list("missing", expected_status: 404)
     end
+
+    email_data = expect_an_email_was_sent
+    id = extract_unsubscribe_id(email_data)
+    unsubscribe_from_subscriber_list(id, expected_status: 204)
+    clear_any_requests_that_have_been_recorded!
+
+    travel_to(Time.zone.today.midnight) { create_content_change }
+
+    travel_to(Time.zone.tomorrow.midday) do
+      DailyDigestInitiatorWorker.new.perform
+      Sidekiq::Worker.drain_all
+    end
+
+    expect_an_email_was_not_sent
+    unsubscribe_from_subscriber_list(id, expected_status: 404)
+    unsubscribe_from_subscriber_list("missing", expected_status: 404)
   end
 end
