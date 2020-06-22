25da69da31b5ca6e8cf43fa462811667ce91d12d
Kevin Dew <kevindew@me.com>
2020-06-22 21:43:55 +0100

    Move EmailAddressOverrider to separate file
    Since this file has it's own unit tests it doesn't appear to be a
    private class, thus it makes sense to be a file in it's own right.
    
    I did debate about whether to modernise the tests somewhat but this
    whole override system seems somewhat crazy (see config/email_service.yml
    for further evidence) so felt it was best to just copy verbatim.

--- app/services/delivery_request_service.rb
+++ app/services/delivery_request_service.rb
@@ -96,24 +96,4 @@ private
 
     MetricsService.content_change_created_to_first_delivery_attempt(content_change_created_at, now)
   end
-
-  class EmailAddressOverrider
-    attr_reader :override_address, :whitelist_addresses, :whitelist_only
-
-    def initialize(config)
-      @override_address = config[:email_address_override]
-      @whitelist_addresses = Array(config[:email_address_override_whitelist])
-      @whitelist_only = config[:email_address_override_whitelist_only]
-    end
-
-    def destination_address(address)
-      return address unless override_address
-
-      if whitelist_addresses.include?(address)
-        address
-      else
-        whitelist_only ? nil : override_address
-      end
-    end
-  end
 end
---
+++ spec/services/delivery_request_service/email_address_overrider_spec.rb
@@ -0,0 +1,89 @@
+RSpec.describe DeliveryRequestService::EmailAddressOverrider do
+  let(:config) { EmailAlertAPI.config.email_service }
+
+  describe "#destination_address" do
+    subject(:destination_address) do
+      described_class.new(config).destination_address(address)
+    end
+
+    context "when an override address is set" do
+      let(:config) { { email_address_override: "overriden@example.com" } }
+      let(:address) { "original@example.com" }
+
+      it "returns the overridden address" do
+        expect(destination_address).to eq("overriden@example.com")
+      end
+    end
+
+    context "when an override address is not set" do
+      let(:address) { "original@example.com" }
+
+      it "returns the original address" do
+        expect(destination_address).to eq("original@example.com")
+      end
+    end
+
+    context "when an override address is set and whitelist addresses are set" do
+      let(:config) do
+        {
+          email_address_override: "overriden@example.com",
+          email_address_override_whitelist: ["whitelist@example.com"],
+        }
+      end
+
+      context "when the argument is a whitelist address" do
+        let(:address) { "whitelist@example.com" }
+
+        it "returns the whitelisted address" do
+          expect(destination_address).to eq("whitelist@example.com")
+        end
+      end
+
+      context "when the argument is not a whitelist address" do
+        let(:address) { "original@example.com" }
+
+        it "returns the overriden address" do
+          expect(destination_address).to eq("overriden@example.com")
+        end
+      end
+    end
+
+    context "when an override address is set and whitelist addresses are set and only whitelist emails should be sent" do
+      let(:config) do
+        {
+          email_address_override: "overriden@example.com",
+          email_address_override_whitelist: ["whitelist@example.com"],
+          email_address_override_whitelist_only: true,
+        }
+      end
+
+      context "when the argument is a whitelist address" do
+        let(:address) { "whitelist@example.com" }
+
+        it "returns the whitelisted address" do
+          expect(destination_address).to eq("whitelist@example.com")
+        end
+      end
+
+      context "when the argument is not a whitelist address" do
+        let(:address) { "original@example.com" }
+
+        it "returns a nil address" do
+          expect(destination_address).to be_nil
+        end
+      end
+    end
+
+    context "when an override address is not set and whitelist addresses are set" do
+      let(:config) do
+        { email_address_override_whitelist: ["whitelist@example.com"] }
+      end
+
+      let(:address) { "original@example.com" }
+
+      it "returns the original address" do
+        expect(destination_address).to eq("original@example.com")
+      end
+    end
+  end
+end
