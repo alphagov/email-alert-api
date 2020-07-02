ab215f6e66e007ba8803a0031a0dd8403669f52d
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2020-07-02 20:01:20 +0100

    Test no recovery of work in progress

--- spec/workers/recover_lost_jobs_worker_spec.rb
+++ spec/workers/recover_lost_jobs_worker_spec.rb
@@ -4,47 +4,85 @@ RSpec.describe RecoverLostJobsWorker do
   end
 
   describe ".perform" do
-    it "does not requeue incomplete content changes that are under 1-hour old" do
-      travel_to(59.minutes.ago) do
-        2.times { create(:content_change) }
+    describe "ContentChange recovery" do
+      it "does not requeue incomplete content changes that are under 1-hour old" do
+        travel_to(59.minutes.ago) do
+          2.times { create(:content_change) }
+        end
+
+        travel_to(Time.zone.now) do
+          expect(ProcessContentChangeWorker).not_to receive(:perform_async)
+          subject.perform(ContentChange, ProcessContentChangeWorker)
+        end
       end
 
-      travel_to(Time.zone.now) do
-        expect(ProcessContentChangeWorker).not_to receive(:perform_async)
-        subject.perform(ContentChange, ProcessContentChangeWorker)
+      it "does not requeue incomplete content changes that are still in the queue" do
+        travel_to(1.hour.ago) do
+          create(:content_change)
+
+          ContentChange.all do |content_change|
+            ProcessContentChangeWorker.perform(content_change.id)
+          end
+
+          expect(ProcessContentChangeWorker).to receive(:perform_async)
+        end
+
+        travel_to(Time.zone.now) do
+          expect(ProcessContentChangeWorker).not_to receive(:perform_async)
+          subject.perform(ContentChange, ProcessContentChangeWorker)
+        end
+      end
+
+      it "requeues incomplete content changes that are over 1-hour old" do
+        travel_to(1.hour.ago) do
+          2.times { create(:content_change) }
+        end
+
+        travel_to(Time.zone.now) do
+          expect(ProcessContentChangeWorker).to receive(:perform_async).twice
+          subject.perform(ContentChange, ProcessContentChangeWorker)
+        end
       end
     end
 
-    it "requeues incomplete content changes that are over 1-hour old" do
-      travel_to(1.hour.ago) do
-        2.times { create(:content_change) }
+    describe "Message recovery" do
+      it "does not requeue incomplete messages that are under 1-hour old" do
+        travel_to(59.minutes.ago) do
+          2.times { create(:message) }
+        end
+
+        travel_to(Time.zone.now) do
+          expect(ProcessMessageWorker).not_to receive(:perform_async)
+          subject.perform(Message, ProcessMessageWorker)
+        end
       end
 
-      travel_to(Time.zone.now) do
-        expect(ProcessContentChangeWorker).to receive(:perform_async).twice
-        subject.perform(ContentChange, ProcessContentChangeWorker)
-      end
-    end
+      it "does not requeue incomplete messages that are still in the queue" do
+        travel_to(1.hour.ago) do
+          create(:message)
 
-    it "does not requeue incomplete messages that are under 1-hour old" do
-      travel_to(59.minutes.ago) do
-        2.times { create(:message) }
-      end
+          Message.all do |message|
+            ProcessMessageWorker.perform(message.id)
+          end
 
-      travel_to(Time.zone.now) do
-        expect(ProcessMessageWorker).not_to receive(:perform_async)
-        subject.perform(Message, ProcessMessageWorker)
-      end
-    end
+          expect(ProcessMessageWorker).to receive(:perform_async)
+        end
 
-    it "requeues incomplete messages that are over 1-hour old" do
-      travel_to(1.hour.ago) do
-        2.times { create(:message) }
+        travel_to(Time.zone.now) do
+          expect(ProcessMessageWorker).not_to receive(:perform_async)
+          subject.perform(Message, ProcessMessageWorker)
+        end
       end
 
-      travel_to(Time.zone.now) do
-        expect(ProcessMessageWorker).to receive(:perform_async).twice
-        subject.perform(Message, ProcessMessageWorker)
+      it "requeues incomplete messages that are over 1-hour old" do
+        travel_to(1.hour.ago) do
+          2.times { create(:message) }
+        end
+
+        travel_to(Time.zone.now) do
+          expect(ProcessMessageWorker).to receive(:perform_async).twice
+          subject.perform(Message, ProcessMessageWorker)
+        end
       end
     end
   end
