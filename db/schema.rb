38ada1265fdccdca2842dfa82e1c4647ce0bb7cd
Kevin Dew <kevindew@me.com>
2020-07-30 18:26:02 +0100

    Migrate all DeliveryAttempts with invalid statuses to valid ones
    In https://github.com/alphagov/email-alert-api/commit/0b1d675ff908be937c1b41710a656a65667b6d7a
    we changed the status values for a DeliveryAttempt. This data migration
    converts any past ones into ones that are currently valid.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_170816) do
+ActiveRecord::Schema.define(version: 2020_07_30_172117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
