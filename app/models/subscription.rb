dfbf3e0b0cd4e38ebc69d143ce5a4d9d82f45030
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-08-04 14:55:51 +0100

    Add task to switch users to daily digest emails
    This adds a new task as part of an experiment to switch immediate
    subscribers of specific lists to daily digest emails. For each
    subscriber, we send a summary email of the changes made. In order to
    track the changes, we use a new reason for start/ending a subscription.
    
    We process each subscriber individually, using the same locking approach
    as when a subscriber is manually updating their subscriptions. I decided
    to avoid doing a single, bulk change, as it's not clear that a roll back
    would be possible in the presence of one or more race conditions.
    
    The task is best effort: if a race condition causes the change to fail
    for a particular subscriber, we can always rerun the task.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,8 +5,8 @@ class Subscription < ApplicationRecord
   has_many :subscription_contents, dependent: :destroy
 
   enum frequency: { immediately: 0, daily: 1, weekly: 2 }
-  enum source: { user_signed_up: 0, frequency_changed: 1, imported: 2, subscriber_list_changed: 3 }, _prefix: true
-  enum ended_reason: { unsubscribed: 0, non_existent_email: 1, frequency_changed: 2, subscriber_list_changed: 3, marked_as_spam: 4, unpublished: 5 }, _prefix: :ended
+  enum source: { user_signed_up: 0, frequency_changed: 1, imported: 2, subscriber_list_changed: 3, bulk_immediate_to_digest: 4 }, _prefix: true
+  enum ended_reason: { unsubscribed: 0, non_existent_email: 1, frequency_changed: 2, subscriber_list_changed: 3, marked_as_spam: 4, unpublished: 5, bulk_immediate_to_digest: 6 }, _prefix: :ended
 
   validates :subscriber, uniqueness: { scope: :subscriber_list, conditions: -> { active } }
 
