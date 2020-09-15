3e9edb013d284337b16b2e90010fd3c85ec9df4c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-15 18:11:13 +0100

    Retry digest runs if records are missing
    Previously there was a risk of losing all memory of a digest run
    if the scheduled job is lost before it could create the DigestRun
    record [1]. While this is unlikely, we want to automate recovery
    of this scenario for completeness.
    
    [1]: https://github.com/mperham/sidekiq/wiki/Reliability

--- spec/workers/recover_lost_jobs_worker_spec.rb
+++ spec/workers/recover_lost_jobs_worker_spec.rb
@@ -37,10 +37,39 @@ RSpec.describe RecoverLostJobsWorker do
       work1 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :daily)
       work2 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :weekly)
 
+      allow(DailyDigestInitiatorWorker).to receive(:perform_async)
       expect(DailyDigestInitiatorWorker).to receive(:perform_async).with(work1.date.to_s)
       expect(WeeklyDigestInitiatorWorker).to receive(:perform_async).with(work2.date.to_s)
 
       subject.perform
+    end
+
+    it "can create missing work for the week" do
+      tuesday = Time.zone.parse("2017-01-10 10:30")
+      travel_to tuesday
+
+      subject.perform
+      expect(DigestRun.where(range: :daily).where("date > ?", 7.days.ago).count).to eq 7
+      expect(DigestRun.where(range: :weekly).where("date > ?", 7.days.ago).count).to eq 1
+    end
+
+    it "does not create duplicate work" do
+      tuesday = Time.zone.parse("2017-01-10 10:30")
+      travel_to tuesday
+
+      create(:digest_run, range: :daily, date: 2.days.ago)
+      create(:digest_run, range: :weekly, date: 3.days.ago)
+
+      subject.perform
+      expect(DigestRun.where(date: 2.days.ago, range: :daily).count).to eq 1
+      expect(DigestRun.where(date: 3.days.ago, range: :weekly).count).to eq 1
+    end
+
+    it "does not create work prematurely" do
+      early_saturday = Time.zone.parse("2017-01-07 05:30")
+      travel_to early_saturday
+      subject.perform
+      expect(DigestRun.where(date: Date.current).count).to eq 0
     end
   end
 end
