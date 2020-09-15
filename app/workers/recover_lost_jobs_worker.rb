ede82b4125d0cf0f646ad13839aef919d2b5c33a
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-15 16:29:27 +0100

    DRY-up recovery logistics

--- app/workers/recover_lost_jobs_worker.rb
+++ app/workers/recover_lost_jobs_worker.rb
@@ -2,30 +2,20 @@ class RecoverLostJobsWorker
   include Sidekiq::Worker
 
   def perform
-    old_unprocessed(ContentChange).pluck(:id).each do |content_change_id|
-      ProcessContentChangeWorker.perform_async(content_change_id)
-    end
-
-    old_unprocessed(Message).pluck(:id).each do |message_id|
-      ProcessMessageWorker.perform_async(message_id)
-    end
-
-    old_unprocessed(DigestRunSubscriber).pluck(:id).each do |digest_run_subscriber_id|
-      DigestEmailGenerationWorker.perform_async(digest_run_subscriber_id)
-    end
-
-    old_unprocessed(DigestRun.daily).pluck(:date).each do |date|
-      DailyDigestInitiatorWorker.perform_async(date.to_s)
-    end
-
-    old_unprocessed(DigestRun.weekly).pluck(:date).each do |date|
-      WeeklyDigestInitiatorWorker.perform_async(date.to_s)
-    end
+    recover(ProcessContentChangeWorker, old_unprocessed(ContentChange).pluck(:id))
+    recover(ProcessMessageWorker, old_unprocessed(Message).pluck(:id))
+    recover(DigestEmailGenerationWorker, old_unprocessed(DigestRunSubscriber).pluck(:id))
+    recover(DailyDigestInitiatorWorker, old_unprocessed(DigestRun.daily).pluck(:date).map(&:to_s))
+    recover(WeeklyDigestInitiatorWorker, old_unprocessed(DigestRun.weekly).pluck(:date).map(&:to_s))
   end
 
 private
 
   def old_unprocessed(scope)
     scope.where(processed_at: nil).where("created_at <= ?", 1.hour.ago)
+  end
+
+  def recover(worker, work)
+    work.each { |arg| worker.perform_async(arg) }
   end
 end
