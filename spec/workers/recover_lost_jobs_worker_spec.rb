6a08b7840245c2e064b0760e5f6e839e90d954cc
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-15 16:05:24 +0100

    DRY-up recovering lost work specs

--- spec/workers/recover_lost_jobs_worker_spec.rb
+++ spec/workers/recover_lost_jobs_worker_spec.rb
@@ -1,68 +1,46 @@
 RSpec.describe RecoverLostJobsWorker do
   describe ".perform" do
-    describe "ContentChange recovery" do
-      it "does not requeue incomplete content changes that are under 1-hour old" do
-        create(:content_change, created_at: 59.minutes.ago)
-        expect(ProcessContentChangeWorker).not_to receive(:perform_async)
-        subject.perform
-      end
-
-      it "requeues incomplete content changes that are over 1-hour old" do
-        create(:content_change, created_at: 1.hour.ago)
-        expect(ProcessContentChangeWorker).to receive(:perform_async)
-        subject.perform
-      end
+    it "recovers unprocessed work over an hour old" do
+      work = create(:content_change, created_at: 1.hour.ago, processed_at: nil)
+      expect(ProcessContentChangeWorker).to receive(:perform_async).with(work.id)
+      subject.perform
     end
 
-    describe "Message recovery" do
-      it "does not requeue incomplete messages that are under 1-hour old" do
-        create(:message, created_at: 59.minutes.ago)
-        expect(ProcessMessageWorker).not_to receive(:perform_async)
-        subject.perform
-      end
-
-      it "requeues incomplete messages that are over 1-hour old" do
-        create(:message, created_at: 1.hour.ago)
-        expect(ProcessMessageWorker).to receive(:perform_async)
-        subject.perform
-      end
+    it "does not recover more recent unprocessed work" do
+      create(:content_change, created_at: 59.minutes.ago, processed_at: nil)
+      expect(ProcessContentChangeWorker).not_to receive(:perform_async)
+      subject.perform
     end
 
-    describe "DigestRunSubscriber recovery" do
-      it "does not requeue unprocessed work that is under 1-hour old" do
-        create(:digest_run_subscriber, created_at: 59.minutes.ago)
-        expect(DigestEmailGenerationWorker).not_to receive(:perform_async)
-        subject.perform
-      end
-
-      it "requeues incomplete work that is over 1-hour old" do
-        create(:digest_run_subscriber, created_at: 1.hour.ago)
-        expect(DigestEmailGenerationWorker).to receive(:perform_async)
-        subject.perform
-      end
+    it "does not recover work that is already processed" do
+      create(:content_change, created_at: 2.hours.ago, processed_at: 1.hour.ago)
+      expect(ProcessContentChangeWorker).not_to receive(:perform_async)
+      subject.perform
     end
 
-    describe "DigestRun recovery" do
-      before do
-        saturday = Time.zone.parse("2017-01-07 10:30")
-        travel_to saturday
-      end
+    it "can also recover Messages" do
+      work = create(:message, created_at: 1.hour.ago, processed_at: nil)
+      expect(ProcessMessageWorker).to receive(:perform_async).with(work.id)
+      subject.perform
+    end
 
-      it "does not requeue unprocessed work that is under 1-hour old" do
-        create(:digest_run, created_at: 59.minutes.ago, date: Date.current, range: :daily)
-        create(:digest_run, created_at: 59.minutes.ago, date: Date.current, range: :weekly)
-        expect(DailyDigestInitiatorWorker).not_to receive(:perform_async)
-        expect(WeeklyDigestInitiatorWorker).not_to receive(:perform_async)
-        subject.perform
-      end
+    it "can also recover DigestRunSubscribers" do
+      work = create(:digest_run_subscriber, created_at: 1.hour.ago, processed_at: nil)
+      expect(DigestEmailGenerationWorker).to receive(:perform_async).with(work.id)
+      subject.perform
+    end
 
-      it "requeues incomplete work that is over 1-hour old" do
-        create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :daily)
-        create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :weekly)
-        expect(DailyDigestInitiatorWorker).to receive(:perform_async)
-        expect(WeeklyDigestInitiatorWorker).to receive(:perform_async)
-        subject.perform
-      end
+    it "can also recover DigestRuns" do
+      saturday = Time.zone.parse("2017-01-07 10:30")
+      travel_to saturday
+
+      work1 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :daily)
+      work2 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :weekly)
+
+      expect(DailyDigestInitiatorWorker).to receive(:perform_async).with(work1.date.to_s)
+      expect(WeeklyDigestInitiatorWorker).to receive(:perform_async).with(work2.date.to_s)
+
+      subject.perform
     end
   end
 end
