89ad16e6a6b6ffff22fdc46e9382e2c2d7ffdfbe
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-09-16 17:04:31 +0100

    Ignore columns that will be removed soon

--- app/models/delivery_attempt.rb
+++ app/models/delivery_attempt.rb
@@ -1,4 +1,6 @@
 class DeliveryAttempt < ApplicationRecord
+  self.ignored_columns = %w[sent_at completed_at]
+
   belongs_to :email
 
   enum status: { sent: 0, delivered: 1, undeliverable_failure: 3, provider_communication_failure: 4 }
--- app/models/email.rb
+++ app/models/email.rb
@@ -2,6 +2,8 @@ class Email < ApplicationRecord
   # Any validations added this to this model won't be applied on record
   # creation as this table is populated by the #insert_all bulk method
 
+  self.ignored_columns = %w[finished_sending_at]
+
   COURTESY_EMAIL = "govuk-email-courtesy-copies@digital.cabinet-office.gov.uk".freeze
   has_many :delivery_attempts
 
