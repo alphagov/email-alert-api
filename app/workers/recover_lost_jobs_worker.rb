469efe788a290d1a2205d8f870c6aafb4c8c9b00
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-16 10:16:13 +0100

    Refactor recovery worker into separate concerns
    This splits the code of the worker into two nested classes that
    better identify the two different kinds of recovery it does.

--- app/workers/recover_lost_jobs_worker.rb
+++ app/workers/recover_lost_jobs_worker.rb
@@ -2,41 +2,7 @@ class RecoverLostJobsWorker
   include Sidekiq::Worker
 
   def perform
-    recover(ProcessContentChangeWorker, old_unprocessed(ContentChange).pluck(:id))
-    recover(ProcessMessageWorker, old_unprocessed(Message).pluck(:id))
-    recover(DigestEmailGenerationWorker, old_unprocessed(DigestRunSubscriber).pluck(:id))
-    recover(DailyDigestInitiatorWorker, old_unprocessed(DigestRun.daily).pluck(:date).map(&:to_s))
-    recover(WeeklyDigestInitiatorWorker, old_unprocessed(DigestRun.weekly).pluck(:date).map(&:to_s))
-    recover(DailyDigestInitiatorWorker, non_existent_daily_digests.map(&:date).map(&:to_s))
-    recover(WeeklyDigestInitiatorWorker, non_existent_weekly_digests.map(&:date).map(&:to_s))
-  end
-
-private
-
-  def old_unprocessed(scope)
-    scope.where(processed_at: nil).where("created_at <= ?", 1.hour.ago)
-  end
-
-  def non_existent_daily_digests
-    expected_digest_week
-      .map { |date| DigestRun.find_or_initialize_by(date: date, range: :daily) }
-      .reject(&:persisted?)
-  end
-
-  def non_existent_weekly_digests
-    [expected_digest_week.find(&:saturday?)]
-      .map { |date| DigestRun.find_or_initialize_by(date: date, range: :weekly) }
-      .reject(&:persisted?)
-  end
-
-  def expected_digest_week
-    digestion_time = Time.zone.parse("#{DigestRun::DIGEST_RANGE_HOUR}:00")
-    cutoff_with_delay = digestion_time + 1.hour
-    end_date = Time.zone.now > cutoff_with_delay ? Time.zone.today : Time.zone.yesterday
-    (end_date - 6.days)..end_date
-  end
-
-  def recover(worker, work)
-    work.each { |arg| worker.perform_async(arg) }
+    RecoverLostJobsWorker::UnprocessedCheck.new.call
+    RecoverLostJobsWorker::DigestRunsCheck.new.call
   end
 end
--- spec/workers/recover_lost_jobs_worker/unprocessed_check_spec.rb
+++ spec/workers/recover_lost_jobs_worker/unprocessed_check_spec.rb
@@ -1,33 +1,33 @@
-RSpec.describe RecoverLostJobsWorker do
-  describe ".perform" do
+RSpec.describe RecoverLostJobsWorker::UnprocessedCheck do
+  describe "#call" do
     it "recovers unprocessed work over an hour old" do
       work = create(:content_change, created_at: 1.hour.ago, processed_at: nil)
       expect(ProcessContentChangeWorker).to receive(:perform_async).with(work.id)
-      subject.perform
+      subject.call
     end
 
     it "does not recover more recent unprocessed work" do
       create(:content_change, created_at: 59.minutes.ago, processed_at: nil)
       expect(ProcessContentChangeWorker).not_to receive(:perform_async)
-      subject.perform
+      subject.call
     end
 
     it "does not recover work that is already processed" do
       create(:content_change, created_at: 2.hours.ago, processed_at: 1.hour.ago)
       expect(ProcessContentChangeWorker).not_to receive(:perform_async)
-      subject.perform
+      subject.call
     end
 
     it "can also recover Messages" do
       work = create(:message, created_at: 1.hour.ago, processed_at: nil)
       expect(ProcessMessageWorker).to receive(:perform_async).with(work.id)
-      subject.perform
+      subject.call
     end
 
     it "can also recover DigestRunSubscribers" do
       work = create(:digest_run_subscriber, created_at: 1.hour.ago, processed_at: nil)
       expect(DigestEmailGenerationWorker).to receive(:perform_async).with(work.id)
-      subject.perform
+      subject.call
     end
 
     it "can also recover DigestRuns" do
@@ -37,39 +37,10 @@ RSpec.describe RecoverLostJobsWorker do
       work1 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :daily)
       work2 = create(:digest_run, created_at: 1.hour.ago, date: Date.current, range: :weekly)
 
-      allow(DailyDigestInitiatorWorker).to receive(:perform_async)
       expect(DailyDigestInitiatorWorker).to receive(:perform_async).with(work1.date.to_s)
       expect(WeeklyDigestInitiatorWorker).to receive(:perform_async).with(work2.date.to_s)
 
-      subject.perform
-    end
-
-    it "can create missing work for the week" do
-      tuesday = Time.zone.parse("2017-01-10 10:30")
-      travel_to tuesday
-
-      subject.perform
-      expect(DigestRun.where(range: :daily).where("date > ?", 7.days.ago).count).to eq 7
-      expect(DigestRun.where(range: :weekly).where("date > ?", 7.days.ago).count).to eq 1
-    end
-
-    it "does not create duplicate work" do
-      tuesday = Time.zone.parse("2017-01-10 10:30")
-      travel_to tuesday
-
-      create(:digest_run, range: :daily, date: 2.days.ago)
-      create(:digest_run, range: :weekly, date: 3.days.ago)
-
-      subject.perform
-      expect(DigestRun.where(date: 2.days.ago, range: :daily).count).to eq 1
-      expect(DigestRun.where(date: 3.days.ago, range: :weekly).count).to eq 1
-    end
-
-    it "does not create work prematurely" do
-      early_saturday = Time.zone.parse("2017-01-07 05:30")
-      travel_to early_saturday
-      subject.perform
-      expect(DigestRun.where(date: Date.current).count).to eq 0
+      subject.call
     end
   end
 end
