5884c582a12b42ba6b052c4c6b9ef2dbc1176da0
Kevin Dew <kevindew@me.com>
2020-09-18 17:17:51 +0100

    Delete unique job digest when a job is marked as dead
    Without this any unique jobs cannot be re-enqueued to retry them as they
    will be marked as already existing.
    
    Approach is taken from: https://github.com/mhenrixon/sidekiq-unique-jobs/tree/v6.0.22#cleanup-dead-locks

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
-Sidekiq.configure_server do
+Sidekiq.configure_server do |config|
   # Calls to Rails.logger in a sidekiq process will use Sidekiq's logger
   Rails.logger = Sidekiq::Logging.logger
+
+  config.death_handlers << lambda { |job, _error|
+    digest = job["unique_digest"]
+    SidekiqUniqueJobs::Digests.delete_by_digest(digest) if digest
+  }
 end
