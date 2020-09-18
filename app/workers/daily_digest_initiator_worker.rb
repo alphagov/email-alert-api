6d845c997f8d514379af3c58593d74abb3f96d19
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-18 12:44:32 +0100

    Fix non-serializable argument to digest workers
    Previously we assumed the argument for initiating a digest run was
    a Date object, but this is false if the job is scheduled with a
    specific date for recovery: the argument will be a string. This
    changes the type of the argument to be a string.

--- app/workers/daily_digest_initiator_worker.rb
+++ app/workers/daily_digest_initiator_worker.rb
@@ -1,5 +1,5 @@
 class DailyDigestInitiatorWorker < DigestInitiatorWorker
-  def perform(date = Date.current)
-    DigestInitiatorService.call(date: date, range: Frequency::DAILY)
+  def perform(date = Date.current.to_s)
+    DigestInitiatorService.call(date: Date.parse(date), range: Frequency::DAILY)
   end
 end
--- app/workers/weekly_digest_initiator_worker.rb
+++ app/workers/weekly_digest_initiator_worker.rb
@@ -1,5 +1,5 @@
 class WeeklyDigestInitiatorWorker < DigestInitiatorWorker
-  def perform(date = Date.current)
-    DigestInitiatorService.call(date: date, range: Frequency::WEEKLY)
+  def perform(date = Date.current.to_s)
+    DigestInitiatorService.call(date: Date.parse(date), range: Frequency::WEEKLY)
   end
 end
