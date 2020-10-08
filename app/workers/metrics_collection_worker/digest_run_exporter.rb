c09e57c018fda4197dd600d4dac289bebf981daf
Kevin Dew <kevindew@me.com>
2020-10-08 10:14:17 +0100

    Fix test that fails before 8am
    As DigestRun models only validate when they are created at 8am this test
    will fail on any PR's raised before then (normally Dependabot, thankfully
    none of us work through the night).
    
    This also applies some minor refactoring where the class and test were
    more fragmented than needed.

--- app/workers/metrics_collection_worker/digest_run_exporter.rb
+++ app/workers/metrics_collection_worker/digest_run_exporter.rb
@@ -1,22 +1,9 @@
 class MetricsCollectionWorker::DigestRunExporter < MetricsCollectionWorker::BaseExporter
   def call
+    critical_digest_runs = DigestRun.where("created_at < ?", 2.hours.ago)
+                                    .where(completed_at: nil)
+                                    .count
+
     GovukStatsd.gauge("digest_runs.critical_total", critical_digest_runs)
-  end
-
-private
-
-  def critical_digest_runs
-    @critical_digest_runs ||= count_digest_runs(critical_latency)
-  end
-
-  def count_digest_runs(age)
-    DigestRun
-      .where("created_at < ?", age.ago)
-      .where(completed_at: nil)
-      .count
-  end
-
-  def critical_latency
-    2.hours
   end
 end
