5ee7dd418f49c91fd5f41308ed0f4a086da32fbd
Kevin Dew <kevindew@me.com>
2020-10-21 12:30:04 +0100

    Remove rescheduled statsd metric
    We don't actively use this data so it is being removed we can add it
    back in if we're interested in actively pursuing how frequently we hit a
    rate limit. In the meantime this could still be monitored by frequency
    of the log message.

--- app/workers/send_email_worker.rb
+++ app/workers/send_email_worker.rb
@@ -13,7 +13,6 @@ class SendEmailWorker < ApplicationWorker
   def perform(email_id, metrics, queue)
     if rate_limit_exceeded?
       logger.warn("Rescheduling email #{email_id} due to exceeding rate limit")
-      GovukStatsd.increment("send_email_worker.rescheduled")
       SendEmailWorker.set(queue: queue || "send_email_immediate")
                            .perform_in(5.minutes, email_id, metrics, queue)
       return
