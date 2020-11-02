52aeada00e50f8badc32d97a44d80ad7ff696bd5
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-11-02 13:01:58 +0000

    Remove all empty subscriber lists over 7 days old
    Previously empty subscriber lists were deleted after a year of creation.
    This alters the behaviour so they are deleted after 7 days since this is
    the [limit] for a user confirming their subscription.
    
    Changing the list retention period will remove approx 8433 empty lists
    from the db.
    
    Trello:
    https://trello.com/c/tfu9zXuy/534-delete-unused-data-after-a-year
    
    [limit]: https://github.com/alphagov/email-alert-api/blob/f0c61539f7244bdb52db54d277a46412b728d642/app/services/auth_token_generator_service.rb#L7

--- app/workers/historical_data_deletion_worker.rb
+++ app/workers/historical_data_deletion_worker.rb
@@ -1,27 +1,31 @@
 class HistoricalDataDeletionWorker < ApplicationWorker
   def perform
     # cascades matched content changes
-    delete_and_log("content changes") { ContentChange.where("created_at < ?", retention_period) }
+    delete_and_log("content changes") { ContentChange.where("created_at < ?", max_retention_period) }
 
     # cascades matched messages
-    delete_and_log("messages") { Message.where("created_at < ?", retention_period) }
+    delete_and_log("messages") { Message.where("created_at < ?", max_retention_period) }
 
     # cascades digest run subscribers
-    delete_and_log("digest runs") { DigestRun.where("created_at < ?", retention_period) }
+    delete_and_log("digest runs") { DigestRun.where("created_at < ?", max_retention_period) }
 
     # deleting subscriptions must be done before deleting subscriber lists or subscribers
-    delete_and_log("subscriptions") { Subscription.where("ended_at < ?", retention_period) }
+    delete_and_log("subscriptions") { Subscription.where("ended_at < ?", max_retention_period) }
 
     delete_and_log("subscriber lists") { empty_subscriber_lists }
 
     # restricts deletion if emails are present
-    delete_and_log("subscribers") { Subscriber.where("deactivated_at < ?", retention_period) }
+    delete_and_log("subscribers") { Subscriber.where("deactivated_at < ?", max_retention_period) }
   end
 
 private
 
-  def retention_period
-    @retention_period ||= 1.year.ago
+  def max_retention_period
+    @max_retention_period ||= 1.year.ago
+  end
+
+  def empty_list_retention_period
+    @empty_list_retention_period ||= 7.days.ago
   end
 
   def delete_and_log(model)
@@ -35,7 +39,7 @@ private
 
   def empty_subscriber_lists
     SubscriberList.left_outer_joins(:subscriptions)
-                  .where("subscriber_lists.created_at < ?", retention_period)
+                  .where("subscriber_lists.created_at < ?", empty_list_retention_period)
                   .where("subscriptions.id": nil)
   end
 end
