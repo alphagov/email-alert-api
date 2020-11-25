101f813d97c3838199e40643e681f1aca6adf67b
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-11-25 12:04:04 +0000

    Recover old pending emails
    This commit adds the recovery of old pending emails.
    
    The fetching of pending emails is done in batches to prevent a large
    amount being loaded into memory all at once. A scenario where this might
    happen would be if we experienced a high volume of communication failure
    with Notify which could lead to a sizeable amount of pending emails
    being recovered.

--- app/workers/recover_lost_jobs_worker.rb
+++ app/workers/recover_lost_jobs_worker.rb
@@ -2,5 +2,6 @@ class RecoverLostJobsWorker < ApplicationWorker
   def perform
     RecoverLostJobsWorker::UnprocessedCheck.new.call
     RecoverLostJobsWorker::MissingDigestRunsCheck.new.call
+    RecoverLostJobsWorker::OldPendingEmailsCheck.new.call
   end
 end
---
+++ app/workers/recover_lost_jobs_worker/old_pending_emails_check.rb
@@ -0,0 +1,18 @@
+class RecoverLostJobsWorker::OldPendingEmailsCheck
+  def call
+    old_pending_emails = Email.where(status: :pending)
+                              .where("created_at <= ?", 1.hour.ago)
+
+    recover(old_pending_emails)
+  end
+
+private
+
+  def recover(old_pending_emails)
+    old_pending_emails.in_batches do |relation|
+      relation.pluck(:id).each do |id|
+        SendEmailWorker.perform_async_in_queue(id, queue: :send_email_immediate)
+      end
+    end
+  end
+end
