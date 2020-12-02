27266d646e9c7437f4374b9bdce2f9e1e38f9673
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-12-02 16:20:51 +0000

    Remove custom retry logic from initiators
    Removed the custom retry count to use the global retry count (9). As
    both of these workers run with locks we're protected against concurrent
    running between the worker's native retries and the retries from within
    the RecoverLostJobsWorker.
    
    This custom retry logic is slightly odd as the initiator workers are the
    only workers to use this approach so the relating code has been removed.

--- app/workers/daily_digest_initiator_worker.rb
+++ app/workers/daily_digest_initiator_worker.rb
@@ -1,10 +1,4 @@
 class DailyDigestInitiatorWorker < ApplicationWorker
-  sidekiq_retry_in do |count|
-    60 * (count + 1)
-  end
-
-  sidekiq_options retry: 3
-
   def perform(date = Date.current.to_s)
     run_with_advisory_lock(DigestRun, "#{date}-#{Frequency::DAILY}") do
       DigestInitiatorService.call(date: Date.parse(date), range: Frequency::DAILY)
--- app/workers/weekly_digest_initiator_worker.rb
+++ app/workers/weekly_digest_initiator_worker.rb
@@ -1,10 +1,4 @@
 class WeeklyDigestInitiatorWorker < ApplicationWorker
-  sidekiq_retry_in do |count|
-    60 * (count + 1)
-  end
-
-  sidekiq_options retry: 3
-
   def perform(date = Date.current.to_s)
     run_with_advisory_lock(DigestRun, "#{date}-#{Frequency::WEEKLY}") do
       DigestInitiatorService.call(date: Date.parse(date), range: Frequency::WEEKLY)
