bc7a0e8699b87863494a63299629e8f118260429
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-12-08 16:07:31 +0000

    Reinstate confirmation email for subscriptions
    Previously we removed this because it adds no value for a standard
    GOV.UK subscription. However, we want it back for subscriptions to
    Brexit Checker emails since the list description included in the
    email contains a link back to the results page.
    
    Note this makes a few changes to the original functionality:
    
    - We now send the email if a previous subscription was overridden,
    since the user clearly has no awareness of this.
    
    - We now have the ability to skip sending the email, to prevent a
    duplicate for users who signed up with a GOV.UK Account.

---
+++ app/builders/subscription_confirmation_email_builder.rb
@@ -0,0 +1,67 @@
+class SubscriptionConfirmationEmailBuilder
+  def initialize(subscription:)
+    @subscription = subscription
+  end
+
+  def self.call(**args)
+    new(**args).call
+  end
+
+  def call
+    Email.create!(
+      subject: subject,
+      body: body,
+      address: subscriber.address,
+      subscriber_id: subscriber.id,
+    )
+  end
+
+  private_class_method :new
+
+private
+
+  attr_reader :subscription
+
+  def subscriber
+    @subscriber ||= subscription.subscriber
+  end
+
+  def subscriber_list
+    @subscriber_list ||= subscription.subscriber_list
+  end
+
+  def subject
+    "You've subscribed to #{subscriber_list.title}"
+  end
+
+  def body
+    <<~BODY
+      You’ll get an email each time there are changes to #{title}.
+
+      #{subscriber_list.description}
+
+      ---
+
+      #{ManageSubscriptionsLinkPresenter.call(subscriber.address)}
+    BODY
+  end
+
+  def title
+    return subscriber_list.title unless subscriber_list.url
+
+    "[#{subscriber_list.title}](#{title_url})"
+  end
+
+  def title_url
+    query = {
+      utm_source: subscriber_list.slug,
+      utm_medium: "email",
+      utm_campaign: "govuk-notifications-subscription-confirmation",
+    }.to_query
+
+    url = subscriber_list.url
+    tracked_url = url + (url.include?("?") ? "&" : "?") + query
+
+    PublicUrls.url_for(base_path: tracked_url)
+  end
+end
