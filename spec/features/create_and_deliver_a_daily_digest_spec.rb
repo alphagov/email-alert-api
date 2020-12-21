0355931de16d2ae55324b01d4a139bd754a479fc
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-12-21 11:46:02 +0000

    Rewrite feature specs for sending digest emails
    Previously these tested almost the entire content of the sent emails,
    which made them brittle and duplicated effort from other, lower-level
    specs. This rewrites them to focus on the main functionality, similar
    to the spec for sending an individual-type email.

--- spec/features/create_and_deliver_a_daily_digest_spec.rb
+++ spec/features/create_and_deliver_a_daily_digest_spec.rb
@@ -1,51 +1,35 @@
 RSpec.describe "create and deliver a daily digest", type: :request do
-  include UTMHelpers
+  let(:list_one_topic_id) { "0eb5d0f0-d384-4f27-9da8-3f9e9b22a820" }
+  let(:list_two_taxon_id) { "6416e4e0-c0c1-457a-8337-4bf8ed9d5f80" }
 
-  scenario do
-    login_with_internal_app
-
-    # create two subscriber lists with different links
-    list_one_topic_id = "0eb5d0f0-d384-4f27-9da8-3f9e9b22a820"
-    list_two_topic_id = "a915e039-070b-4633-813d-187af61cad7a"
-
-    subscriber_list_one_id = create_subscriber_list(
+  let(:subscriber_list_one_id) do
+    create_subscriber_list(
       title: "Subscriber list one",
       links: {
         topics: { any: [list_one_topic_id] },
       },
     )
+  end
 
-    subscriber_list_two_id = create_subscriber_list(
+  let(:subscriber_list_two_id) do
+    create_subscriber_list(
       title: "Subscriber list two",
       links: {
-        topics: { any: [list_two_topic_id] },
+        taxon_tree: { all: [list_two_taxon_id] },
       },
     )
+  end
 
-    # create two daily subscribers, one subscribed to daily digests for both
-    # subscriber_lists and the other for daily for subscriber_list_one only
-    subscriber_one_address = "test-one@example.com"
-    subscriber_two_address = "test-two@example.com"
+  before do
+    login_with_internal_app
+  end
 
+  scenario "single list" do
     subscribe_to_subscriber_list(
       subscriber_list_one_id,
-      address: subscriber_one_address,
       frequency: Frequency::DAILY,
     )
 
-    subscribe_to_subscriber_list(
-      subscriber_list_two_id,
-      address: subscriber_one_address,
-      frequency: Frequency::DAILY,
-    )
-
-    subscribe_to_subscriber_list(
-      subscriber_list_one_id,
-      address: subscriber_two_address,
-      frequency: Frequency::DAILY,
-    )
-
-    # publish two items to each list
     travel_to(Time.zone.parse("2017-01-01 09:30")) do
       create_content_change(
         title: "Title one",
@@ -58,197 +42,63 @@ RSpec.describe "create and deliver a daily digest", type: :request do
 
     travel_to(Time.zone.parse("2017-01-01 09:31")) do
       create_message(
-        title: "Title two",
+        body: "Body",
         criteria_rules: [
           { type: "link", key: "topics", value: list_one_topic_id },
         ],
       )
     end
 
+    travel_to(Time.zone.parse("2017-01-02 10:00")) do
+      DailyDigestInitiatorWorker.new.perform
+      Sidekiq::Worker.drain_all
+    end
+
+    email_data = expect_an_email_was_sent(
+      subject: "Daily update from GOV.UK for: Subscriber list one"
+    )
+
+    body = email_data.dig(:personalisation, :body)
+    expect(body).to include("Title one")
+    expect(body).to include("gov.uk/base-path")
+    expect(body).to include("Change note one")
+    expect(body).to include("Description one")
+    expect(body).to include("Body")
+    expect(body).to include("10:00am, 1 January 2017")
+    expect(body).to include("[Unsubscribe](http://www.dev.gov.uk/email/unsubscribe")
+    expect(body).to include("gov.uk/email/manage/authenticate?address=")
+  end
+
+  scenario "multiple lists" do
+    subscribe_to_subscriber_list(
+      subscriber_list_one_id,
+      frequency: Frequency::DAILY,
+    )
+
+    subscribe_to_subscriber_list(
+      subscriber_list_two_id,
+      frequency: Frequency::DAILY,
+    )
+
+    travel_to(Time.zone.parse("2017-01-01 09:30")) do
+      create_content_change(links: { topics: [list_one_topic_id] })
+    end
+
     travel_to(Time.zone.parse("2017-01-01 09:32")) do
-      create_content_change(
-        title: "Title three",
-        description: "Description three",
-        change_note: "Change note three",
-        public_updated_at: "2017-01-01 09:00:00",
-        links: {
-          topics: [list_two_topic_id],
-        },
-      )
+      create_content_change(links: { taxon_tree: [list_two_taxon_id] })
     end
-
-    travel_to(Time.zone.parse("2017-01-01 09:33")) do
-      create_content_change(
-        title: "Title four",
-        description: "Description four",
-        change_note: "Change note four",
-        public_updated_at: "2017-01-01 09:30:00",
-        links: {
-          topics: [list_two_topic_id],
-        },
-      )
-    end
-
-    content_changes = ContentChange.order(:created_at)
-
-    first_digest_stub = stub_notify_request(
-      subscriber_one_address,
-      first_expected_email_body(
-        content_changes[0],
-      ),
-      "Subscriber list one",
-    )
-
-    second_digest_stub = stub_notify_request(
-      subscriber_one_address,
-      second_expected_email_body(
-        content_changes[1],
-        content_changes[2],
-      ),
-      "Subscriber list two",
-    )
-
-    third_digest_stub = stub_notify_request(
-      subscriber_two_address,
-      third_expected_email_body(
-        content_changes[0],
-      ),
-      "Subscriber list one",
-    )
 
     travel_to(Time.zone.parse("2017-01-02 10:00")) do
       DailyDigestInitiatorWorker.new.perform
       Sidekiq::Worker.drain_all
     end
 
-    expect(first_digest_stub).to have_been_requested
-    expect(second_digest_stub).to have_been_requested
-    expect(third_digest_stub).to have_been_requested
-  end
-
-  def url
-    "http://www.dev.gov.uk/base-path?"
-  end
-
-  def stub_notify_request(email_address, email_body, title)
-    body = hash_including(
-      email_address: email_address,
-      personalisation: hash_including(
-        "subject" => "Daily update from GOV.UK for: #{title}",
-        "body" => include(email_body),
-      ),
+    expect_an_email_was_sent(
+      subject: "Daily update from GOV.UK for: Subscriber list one"
     )
 
-    stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/email")
-      .with(body: body)
-      .to_return(body: {}.to_json)
-  end
-
-  def first_expected_email_body(content_change_one)
-    <<~BODY
-      Daily update from GOV.UK for:
-
-      # Subscriber list one
-
-      ---
-
-      # [Title one](#{url}#{utm_params(content_change_one.id, 'daily')})
-
-      Page summary:
-      Description one
-
-      Change made:
-      Change note one
-
-      Time updated:
-      10:00am, 1 January 2017
-
-      ---
-
-      Body
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a day about:
-
-      Subscriber list one
-    BODY
-  end
-
-  def second_expected_email_body(content_change_two, content_change_three)
-    <<~BODY
-      Daily update from GOV.UK for:
-
-      # Subscriber list two
-
-      ---
-
-      # [Title three](#{url}#{utm_params(content_change_two.id, 'daily')})
-
-      Page summary:
-      Description three
-
-      Change made:
-      Change note three
-
-      Time updated:
-      9:00am, 1 January 2017
-
-      ---
-
-      # [Title four](#{url}#{utm_params(content_change_three.id, 'daily')})
-
-      Page summary:
-      Description four
-
-      Change made:
-      Change note four
-
-      Time updated:
-      9:30am, 1 January 2017
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a day about:
-
-      Subscriber list two
-    BODY
-  end
-
-  def third_expected_email_body(content_change_one)
-    <<~BODY
-      Daily update from GOV.UK for:
-
-      # Subscriber list one
-
-      ---
-
-      # [Title one](#{url}#{utm_params(content_change_one.id, 'daily')})
-
-      Page summary:
-      Description one
-
-      Change made:
-      Change note one
-
-      Time updated:
-      10:00am, 1 January 2017
-
-      ---
-
-      Body
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a day about:
-
-      Subscriber list one
-    BODY
+    expect_an_email_was_sent(
+      subject: "Daily update from GOV.UK for: Subscriber list two"
+    )
   end
 end
--- spec/features/create_and_deliver_a_weekly_digest_spec.rb
+++ spec/features/create_and_deliver_a_weekly_digest_spec.rb
@@ -1,47 +1,32 @@
-RSpec.describe "create and delive a weekly digest", type: :request do
-  include UTMHelpers
+RSpec.describe "create and deliver a weekly digest", type: :request do
+  let(:list_one_topic_id) { "0eb5d0f0-d384-4f27-9da8-3f9e9b22a820" }
+  let(:list_two_taxon_id) { "6416e4e0-c0c1-457a-8337-4bf8ed9d5f80" }
 
-  scenario do
-    login_with_internal_app
-
-    # create two subscriber lists with different links
-    list_one_topic_id = "0eb5d0f0-d384-4f27-9da8-3f9e9b22a820"
-    list_two_taxon_id = "6416e4e0-c0c1-457a-8337-4bf8ed9d5f80"
-
-    subscriber_list_one_id = create_subscriber_list(
+  let(:subscriber_list_one_id) do
+    create_subscriber_list(
       title: "Subscriber list one",
       links: {
         topics: { any: [list_one_topic_id] },
       },
     )
+  end
 
-    subscriber_list_two_id = create_subscriber_list(
+  let(:subscriber_list_two_id) do
+    create_subscriber_list(
       title: "Subscriber list two",
       links: {
         taxon_tree: { all: [list_two_taxon_id] },
       },
     )
+  end
 
-    # create two daily subscribers, one subscribed to daily digests for both
-    # subscriber_lists and the other for daily for subscriber_list_one only
-    subscriber_one_address = "test-one@example.com"
-    subscriber_two_address = "test-two@example.com"
+  before do
+    login_with_internal_app
+  end
 
+  scenario "single list" do
     subscribe_to_subscriber_list(
       subscriber_list_one_id,
-      address: subscriber_one_address,
-      frequency: Frequency::WEEKLY,
-    )
-
-    subscribe_to_subscriber_list(
-      subscriber_list_two_id,
-      address: subscriber_one_address,
-      frequency: Frequency::WEEKLY,
-    )
-
-    subscribe_to_subscriber_list(
-      subscriber_list_one_id,
-      address: subscriber_two_address,
       frequency: Frequency::WEEKLY,
     )
 
@@ -61,202 +46,63 @@ RSpec.describe "create and delive a weekly digest", type: :request do
 
     travel_to(Time.zone.parse("2017-01-01 09:31")) do
       create_message(
-        title: "Title two",
-        url: "/base-path",
+        body: "Body",
         criteria_rules: [
           { type: "link", key: "topics", value: list_one_topic_id },
         ],
       )
     end
 
+    travel_to(Time.zone.parse("2017-01-07 10:00")) do
+      WeeklyDigestInitiatorWorker.new.perform
+      Sidekiq::Worker.drain_all
+    end
+
+    email_data = expect_an_email_was_sent(
+      subject: "Weekly update from GOV.UK for: Subscriber list one"
+    )
+
+    body = email_data.dig(:personalisation, :body)
+    expect(body).to include("Title one")
+    expect(body).to include("gov.uk/base-path")
+    expect(body).to include("Change note one")
+    expect(body).to include("Description one")
+    expect(body).to include("Body")
+    expect(body).to include("10:00am, 1 January 2017")
+    expect(body).to include("[Unsubscribe](http://www.dev.gov.uk/email/unsubscribe")
+    expect(body).to include("gov.uk/email/manage/authenticate?address=")
+  end
+
+  scenario "multiple lists" do
+    subscribe_to_subscriber_list(
+      subscriber_list_one_id,
+      frequency: Frequency::WEEKLY,
+    )
+
+    subscribe_to_subscriber_list(
+      subscriber_list_two_id,
+      frequency: Frequency::WEEKLY,
+    )
+
+    travel_to(Time.zone.parse("2017-01-01 09:30")) do
+      create_content_change(links: { topics: [list_one_topic_id] })
+    end
+
     travel_to(Time.zone.parse("2017-01-04 09:32")) do
-      create_content_change(
-        title: "Title three",
-        content_id: SecureRandom.uuid,
-        description: "Description three",
-        change_note: "Change note three",
-        public_updated_at: "2017-01-04 09:00:00",
-        links: {
-          taxon_tree: [list_two_taxon_id],
-        },
-      )
+      create_content_change(links: { taxon_tree: [list_two_taxon_id] })
     end
-
-    travel_to(Time.zone.parse("2017-01-06 09:33")) do
-      create_content_change(
-        title: "Title four",
-        content_id: SecureRandom.uuid,
-        description: "Description four",
-        change_note: "Change note four",
-        public_updated_at: "2017-01-06 09:30:00",
-        links: {
-          taxon_tree: [SecureRandom.uuid,
-                       list_two_taxon_id,
-                       SecureRandom.uuid],
-        },
-      )
-    end
-
-    content_changes = ContentChange.order(:created_at)
-
-    first_digest_stub = stub_notify_request(
-      subscriber_one_address,
-      first_expected_email_body(
-        content_changes[0],
-      ),
-      "Subscriber list one",
-    )
-
-    second_digest_stub = stub_notify_request(
-      subscriber_one_address,
-      second_expected_email_body(
-        content_changes[1],
-        content_changes[2],
-      ),
-      "Subscriber list two",
-    )
-
-    third_digest_stub = stub_notify_request(
-      subscriber_two_address,
-      third_expected_email_body(
-        content_changes[0],
-      ),
-      "Subscriber list one",
-    )
 
     travel_to(Time.zone.parse("2017-01-07 10:00")) do
       WeeklyDigestInitiatorWorker.new.perform
       Sidekiq::Worker.drain_all
     end
 
-    expect(first_digest_stub).to have_been_requested
-    expect(second_digest_stub).to have_been_requested
-    expect(third_digest_stub).to have_been_requested
-  end
-
-  def url
-    "http://www.dev.gov.uk/base-path?"
-  end
-
-  def stub_notify_request(email_address, email_body, title)
-    body = hash_including(
-      email_address: email_address,
-      personalisation: hash_including(
-        "subject" => "Weekly update from GOV.UK for: #{title}",
-        "body" => include(email_body),
-      ),
+    expect_an_email_was_sent(
+      subject: "Weekly update from GOV.UK for: Subscriber list one"
     )
 
-    stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/email")
-      .with(body: body)
-      .to_return(body: {}.to_json)
-  end
-
-  def first_expected_email_body(content_change_one)
-    <<~BODY
-      Weekly update from GOV.UK for:
-
-      # Subscriber list one
-
-      ---
-
-      # [Title one](#{url}#{utm_params(content_change_one.id, 'weekly')})
-
-      Page summary:
-      Description one
-
-      Change made:
-      Change note one
-
-      Time updated:
-      10:00am, 1 January 2017
-
-      ---
-
-      Body
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a week about:
-
-      Subscriber list one
-    BODY
-  end
-
-  def second_expected_email_body(content_change_two, content_change_three)
-    <<~BODY
-      Weekly update from GOV.UK for:
-
-      # Subscriber list two
-
-      ---
-
-      # [Title three](#{url}#{utm_params(content_change_two.id, 'weekly')})
-
-      Page summary:
-      Description three
-
-      Change made:
-      Change note three
-
-      Time updated:
-      9:00am, 4 January 2017
-
-      ---
-
-      # [Title four](#{url}#{utm_params(content_change_three.id, 'weekly')})
-
-      Page summary:
-      Description four
-
-      Change made:
-      Change note four
-
-      Time updated:
-      9:30am, 6 January 2017
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a week about:
-
-      Subscriber list two
-    BODY
-  end
-
-  def third_expected_email_body(content_change_one)
-    <<~BODY
-      Weekly update from GOV.UK for:
-
-      # Subscriber list one
-
-      ---
-
-      # [Title one](#{url}#{utm_params(content_change_one.id, 'weekly')})
-
-      Page summary:
-      Description one
-
-      Change made:
-      Change note one
-
-      Time updated:
-      10:00am, 1 January 2017
-
-      ---
-
-      Body
-
-      ---
-
-      # Why am I getting this email?
-
-      You asked GOV.UK to send you one email a week about:
-
-      Subscriber list one
-    BODY
+    expect_an_email_was_sent(
+      subject: "Weekly update from GOV.UK for: Subscriber list two"
+    )
   end
 end
