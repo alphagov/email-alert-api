ae1b7be7179decd1781b722cf94a255008f46f4e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-04 10:06:32 +0000

    Restructure bulk email builder spec
    Previously that had multiple levels of "let" statements, with a
    rather confusing mixture of "import" and "email". This consolidates
    the "lets" and re-focusses the spec on Email objects and specific
    subscriptions that lead to their creation.

--- spec/builders/bulk_subscriber_list_email_builder_spec.rb
+++ spec/builders/bulk_subscriber_list_email_builder_spec.rb
@@ -1,50 +1,48 @@
 RSpec.describe BulkSubscriberListEmailBuilder do
-  let(:email_subject) { "email subject" }
-  let(:body) { "email body" }
-
   describe ".call" do
-    subject(:email_import) do
-      described_class.call(subject: email_subject, body: body, subscriber_lists: subscriber_lists)
+    let(:subscriber) { create(:subscriber) }
+    let(:subscriber_lists) { create_list(:subscriber_list, 2) }
+
+    let(:email) do
+      email_ids = described_class.call(
+        subject: "email subject",
+        body: "email body",
+        subscriber_lists: subscriber_lists,
+      )
+
+      Email.find(email_ids).first
     end
 
-    context "with one subscriber" do
-      let(:subscriber_lists) { [create(:subscription).subscriber_list] }
-
-      it "returns an email import" do
-        expect(email_import.count).to eq(1)
+    context "with one subscription" do
+      before do
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.first)
       end
 
-      let(:email) { Email.find(email_import.first) }
-
-      it "sets the subject" do
+      it "creates an email" do
         expect(email.subject).to eq("email subject")
-      end
-
-      it "sets the body" do
         expect(email.body).to eq("email body")
       end
     end
 
     context "with an ended subscription" do
-      let(:subscriber_lists) { [create(:subscription, :ended).subscriber_list] }
+      before do
+        create(:subscription, :ended, subscriber_list: subscriber_lists.first)
+      end
 
-      it "imports no emails" do
-        expect(email_import.count).to eq(0)
+      it "creates no emails" do
+        expect(email).to be_nil
       end
     end
 
     context "with many subscriptions" do
-      let(:subscriber_1) { create(:subscriber) }
-
-      let(:subscriber_lists) do
-        [
-          create(:subscription, subscriber: subscriber_1).subscriber_list,
-          create(:subscription, subscriber: subscriber_1).subscriber_list,
-        ]
+      before do
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.first)
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.second)
       end
 
       it "should only create one email per subscriber" do
-        expect(email_import.count).to eq(1)
+        expect(email).to be_present
+        expect(Email.count).to eq(1)
       end
     end
   end
