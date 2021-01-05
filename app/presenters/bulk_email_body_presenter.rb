07db679641b19b5bf739adcb27e8f237c7d29fc2
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-05 13:19:52 +0000

    Add tracking to substituted bulk email URL
    The tracking attributes are based on those for content changes [1],
    but with a unique campaign (since this is no ordinary email). Using
    the list slug for the utm_source makes sense here, since the email
    is essentially about the subscription itself.
    
    Note that this changes the arbitrary behaviour if one of the lists
    does not have a URL, due to the need to parse it in order to avoid
    overriding any existing query params. If one of the lists does not
    have a URL, we will now substitute an empty string.
    
    [1]: https://github.com/alphagov/email-alert-api/blob/d80bdd48198ffa37dfabe5ba630daa6c63190676/app/presenters/content_change_presenter.rb#L26-L33

--- app/presenters/bulk_email_body_presenter.rb
+++ app/presenters/bulk_email_body_presenter.rb
@@ -5,10 +5,24 @@ class BulkEmailBodyPresenter < ApplicationPresenter
   end
 
   def call
-    body.gsub("%LISTURL%", PublicUrls.url_for(base_path: subscriber_list.url.to_s))
+    body.gsub("%LISTURL%", list_url)
   end
 
 private
 
   attr_reader :body, :subscriber_list
+
+  def list_url
+    tracking_params = [
+      "utm_source=#{subscriber_list.slug}",
+      "utm_medium=email",
+      "utm_campaign=govuk-notifications-bulk",
+    ]
+
+    uri = URI.parse(subscriber_list.url)
+    uri.query = ([uri.query] + tracking_params).compact.join("&")
+    PublicUrls.url_for(base_path: uri.to_s)
+  rescue URI::InvalidURIError
+    ""
+  end
 end
