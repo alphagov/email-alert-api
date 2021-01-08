44673316f082b772f7f82e182e47253f7d04b1fc
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-08 16:42:58 +0000

    Revert "Add temporary rake task to send apology email"
    This reverts commit 400e7d8d7f4a52603515b0a1f2f99e47240307e3.
    
    The task has now been run.

--- lib/tasks/bulk_email.rake
+++ lib/tasks/bulk_email.rake
@@ -10,40 +10,4 @@ namespace :bulk_email do
       SendEmailWorker.perform_async_in_queue(id, queue: :send_email_immediate)
     end
   end
-
-  desc "Send an apology email following an incident"
-  task :temp_incident_new_subscribers, [] => :environment do
-    affected_emails = Email
-      .where(subject: "Confirm that you want to get emails from GOV.UK")
-      .where("created_at > ? AND created_at < ?", Time.zone.local(2021, 1, 7, 14, 33), Time.zone.local(2021, 1, 8, 13, 45))
-      .uniq(&:address)
-
-    explanation = <<~BODY
-      Hello,
-
-      You recently signed up to email notifications for a topic on GOV.UK. Unfortunately, the confirmation email you were sent contained an invalid link and your subscription was not confirmed.
-
-      If you still wish to subscribe, please sign up again through the GOV.UK website. You will then be sent a new confirmation link.
-
-      Apologies for any inconvenience,
-
-      GOV.UK emails
-    BODY
-
-    follow_up_emails = []
-
-    ApplicationRecord.transaction do
-      follow_up_emails = affected_emails.map do |email|
-        Email.create!(
-          subject: "Re: Confirm that you want to get emails from GOV.UK",
-          body: explanation,
-          address: email.address,
-        )
-      end
-    end
-
-    follow_up_emails.each do |email|
-      SendEmailWorker.perform_async_in_queue(email.id, queue: :send_email_immediate)
-    end
-  end
 end
--- spec/lib/tasks/bulk_email_spec.rb
+++ spec/lib/tasks/bulk_email_spec.rb
@@ -40,35 +40,4 @@ RSpec.describe "bulk_email" do
       Rake::Task["bulk_email:for_lists"].invoke(subscriber_list.id)
     end
   end
-
-  describe "temp_incident_new_subscribers" do
-    let(:affected_subject) { "Confirm that you want to get emails from GOV.UK" }
-
-    before do
-      Rake::Task["bulk_email:temp_incident_new_subscribers"].reenable
-    end
-
-    it "sends a single apology email to affected subscribers" do
-      # affected emails
-      create(:email, subject: affected_subject, created_at: Time.zone.local(2021, 1, 7, 14, 35))
-      create(:email, subject: affected_subject, created_at: Time.zone.local(2021, 1, 8, 13, 40))
-
-      # unaffected emails
-      create(:email, subject: affected_subject, created_at: Time.zone.local(2021, 1, 7, 14, 30))
-      create(:email, subject: affected_subject, created_at: Time.zone.local(2021, 1, 8, 13, 50))
-      create(:email, subject: "something else", created_at: Time.zone.local(2021, 1, 8, 13, 40))
-
-      expect(SendEmailWorker)
-        .to receive(:perform_async_in_queue)
-        .with(instance_of(String), queue: :send_email_immediate)
-        .once
-
-      expect { Rake::Task["bulk_email:temp_incident_new_subscribers"].invoke }
-        .to change { Email.count }.by(1)
-
-      apology_emails = Email.where(subject: "Re: #{affected_subject}")
-      expect(apology_emails.first.body).to include("please sign up again")
-      expect(apology_emails.first.address).to eq(Email.pick(:address))
-    end
-  end
 end
