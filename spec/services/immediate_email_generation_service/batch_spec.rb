8757836b630d3736c48646adf545763e560780d3
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-11 15:38:03 +0000

    Minimise setup in immediate email generation tests
    Previously the test setup involved two subscriptions / subscribers,
    even though this number is only strictly necessary in one of the
    contexts. Having two subscriptions made it hard to understand some
    of the contexts, as it was unclear which subscription was relevant.

--- spec/services/immediate_email_generation_service/batch_spec.rb
+++ spec/services/immediate_email_generation_service/batch_spec.rb
@@ -1,39 +1,25 @@
 RSpec.describe ImmediateEmailGenerationService::Batch do
-  let(:content_change) { create(:content_change) }
-  let(:message) { create(:message) }
-  let(:subscriber1) { create(:subscriber) }
-  let(:subscriber2) { create(:subscriber) }
+  let(:content) { create(:content_change) }
+  let(:subscriptions) { [create(:subscription, :immediately)] }
 
-  let(:subscriber1_subscription) do
-    create(:subscription, :immediately, subscriber: subscriber1)
+  let(:instance) do
+    described_class.new(content, subscriptions.map(&:id))
   end
-
-  let(:subscriber2_subscription) do
-    create(:subscription, :immediately, subscriber: subscriber2)
-  end
-
-  let(:subscriptions) do
-    [subscriber1_subscription, subscriber2_subscription]
-  end
-
-  let(:subscription_ids) { subscriptions.map(&:id) }
 
   describe "#generate_emails" do
-    let(:instance) { described_class.new(content_change, subscription_ids) }
-
     it "creates emails" do
       expect { instance.generate_emails }
         .to change { Email.count }
-        .by(2)
+        .by(1)
     end
 
     it "populates subscription_contents" do
-      scope = SubscriptionContent.where(content_change: content_change,
-                                        subscription_id: subscription_ids)
+      scope = SubscriptionContent.where(content_change: content,
+                                        subscription: subscriptions)
 
       expect { instance.generate_emails }
         .to change { scope.count }
-        .by(subscription_ids.count)
+        .by(1)
     end
 
     it "returns ids of emails created" do
@@ -42,11 +28,9 @@ RSpec.describe ImmediateEmailGenerationService::Batch do
     end
 
     context "when content is a content_change" do
-      let(:instance) { described_class.new(content_change, subscription_ids) }
-
       it "uses ImmediateEmailBuilder to build emails" do
         expect(ImmediateEmailBuilder).to receive(:call)
-           .with(content_change, subscriptions)
+           .with(content, subscriptions)
            .and_call_original
 
         instance.generate_emails
@@ -54,17 +38,18 @@ RSpec.describe ImmediateEmailGenerationService::Batch do
 
       it "sends stats about the generated emails" do
         expect(Metrics).to receive(:content_change_emails)
-                              .with(content_change, 2)
+          .with(content, 1)
+
         instance.generate_emails
       end
     end
 
     context "when content is a message" do
-      let(:instance) { described_class.new(message, subscription_ids) }
+      let(:content) { create(:message) }
 
       it "uses ImmediateEmailBuilder to build emails" do
         expect(ImmediateEmailBuilder).to receive(:call)
-           .with(message, subscriptions)
+           .with(content, subscriptions)
            .and_call_original
 
         instance.generate_emails
@@ -72,73 +57,55 @@ RSpec.describe ImmediateEmailGenerationService::Batch do
     end
 
     context "when a subscription was ended after determining which lists to email" do
-      let(:subscriber1_subscription) do
-        create(:subscription, :immediately, :ended, subscriber: subscriber1)
-      end
-
-      let(:subscriber2_subscription) do
-        create(:subscription, :immediately, subscriber: subscriber2)
-      end
+      let(:subscriptions) { [create(:subscription, :immediately, :ended)] }
 
       it "doesn't email a subscriber without active subscriptions" do
         expect { instance.generate_emails }
-          .not_to(change { Email.where(address: subscriber1.address).count })
+          .not_to(change { Email.count })
       end
 
       it "only uses active subscriptions to create the email" do
-        expect(ImmediateEmailBuilder).to receive(:call)
-           .with(content_change, [subscriber2_subscription])
-           .and_call_original
-
+        expect(ImmediateEmailBuilder).to_not receive(:call)
         instance.generate_emails
       end
     end
 
     context "when one of the specified subscriptions is not immediate" do
-      let(:subscriber1_subscription) do
-        create(:subscription, :daily, subscriber: subscriber1)
-      end
-
-      let(:subscriber2_subscription) do
-        create(:subscription, :immediately, subscriber: subscriber2)
-      end
+      let(:subscriptions) { [create(:subscription, :daily)] }
 
       it "doesn't use that subscription to create the email" do
-        expect(ImmediateEmailBuilder).to receive(:call)
-           .with(content_change, [subscriber2_subscription])
-           .and_call_original
-
+        expect(ImmediateEmailBuilder).to_not receive(:call)
         instance.generate_emails
       end
     end
 
-    context "when a previous attempt to generate emails failed and some of the " \
-      "emails were already created" do
+    context "when only some of the emails were generated due to a failure" do
+      let(:subscription) { create(:subscription, :immediately) }
+      let(:other_subscription) { create(:subscription, :immediately) }
+      let(:subscriptions) { [subscription, other_subscription] }
+
       before do
         email = create(:email)
-
         create(:subscription_content,
-               subscription: subscriber1_subscription,
+               subscription: other_subscription,
                email: email,
-               content_change: content_change)
+               content_change: content)
       end
 
       it "only creates emails that haven't been previously created" do
         expect { instance.generate_emails }
-          .to change { Email.where(address: subscriber2.address).count }.by(1)
-          .and change { Email.where(address: subscriber1.address).count }.by(0)
+          .to change { Email.where(subscriber_id: subscription.subscriber_id).count }.by(1)
+          .and change { Email.where(address: other_subscription.subscriber_id).count }.by(0)
       end
     end
 
     context "when a previous attempt created all the emails of this batch" do
       before do
         email = create(:email)
-        [subscriber1_subscription, subscriber2_subscription].each do |subscription|
-          create(:subscription_content,
-                 subscription: subscription,
-                 email: email,
-                 content_change: content_change)
-        end
+        create(:subscription_content,
+               subscription: subscriptions.first,
+               email: email,
+               content_change: content)
       end
 
       it "happily creates no emails" do
