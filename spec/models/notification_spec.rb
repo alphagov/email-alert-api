2c8b3d1bf61ee89aac15beef183e4fc1f7c090e6
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-18 15:49:04 +0000

    Avoid EmailTemplateContext in SubscriberListMover
    This switches to using the conventional PublicUrls module in order
    to generate an environment-sensitive link with UTM params. Removing
    the use of EmailTemplateContext here will make it easier to remove
    the class entirely in the next commits.

--- spec/models/notification_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ContentChange, type: :model do
-  describe "validations" do
-    subject { create(:content_change) }
-
-    it "is valid for the default factory" do
-      expect(subject).to be_valid
-    end
-  end
-end
