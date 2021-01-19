7a9368ee76100df17ff4ba07c79c4a3f7d446720
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-01-19 12:15:53 +0000

    DRY-up adapting params into matching criteria
    Previously we copied "find_exact_params" into the service from the
    controller, with the aim of removing it there, along with the "index"
    endpoint. However, the endpoint is still needed, so this modifies
    the interface of the service to accept the params from the controller.
    
    Note that the test for coping with legacy params is deleted in this
    commit. We will re-add it at the integration level in the following
    commits, after a bit of refactoring to make that easier.

--- app/services/create_subscriber_list_service.rb
+++ app/services/create_subscriber_list_service.rb
@@ -1,52 +1,38 @@
 class CreateSubscriberListService < ApplicationService
-  def initialize(params:, user:)
-    @params = params
+  def initialize(title:, url:, matching_criteria:, user:)
+    @title = title
+    @url = url
+    @matching_criteria = matching_criteria
     @user = user
   end
 
   def call
-    subscriber_list = FindExactQuery.new(**find_exact_query_params).exact_match
+    subscriber_list = FindExactQuery.new(**matching_criteria).exact_match
     return SubscriberList.create!(subscriber_list_params) unless subscriber_list
 
-    subscriber_list.update!(subscriber_list_params.slice(:title, :url).compact)
+    subscriber_list.update!(title: title, url: url)
     subscriber_list
   end
 
 private
 
-  attr_reader :params, :user
+  attr_reader :title, :url, :matching_criteria, :user
 
   def subscriber_list_params
-    find_exact_query_params.merge(
-      title: params[:title],
+    matching_criteria.merge(
+      title: title,
       slug: slug,
-      url: params[:url],
+      url: url,
       signon_user_uid: user.uid,
     )
   end
 
-  def convert_legacy_params(link_or_tags)
-    link_or_tags.transform_values do |link_or_tag|
-      link_or_tag.is_a?(Hash) ? link_or_tag : { any: link_or_tag }
-    end
-  end
-
-  def find_exact_query_params
-    {
-      tags: convert_legacy_params(params.permit(tags: {}).to_h.fetch(:tags, {})),
-      links: convert_legacy_params(params.permit(links: {}).to_h.fetch(:links, {})),
-      document_type: params.fetch(:document_type, ""),
-      email_document_supertype: params.fetch(:email_document_supertype, ""),
-      government_document_supertype: params.fetch(:government_document_supertype, ""),
-    }
-  end
-
   def slug
     @slug ||= begin
-      result = params[:title].parameterize.truncate(255, omission: "", separator: "-")
+      result = title.parameterize.truncate(255, omission: "", separator: "-")
 
       while SubscriberList.where(slug: result).exists?
-        result = params[:title].parameterize.truncate(244, omission: "", separator: "-")
+        result = title.parameterize.truncate(244, omission: "", separator: "-")
         result += "-#{SecureRandom.hex(5)}"
       end
 
