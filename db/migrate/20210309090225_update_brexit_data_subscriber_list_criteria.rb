b8a6abb97bce2964f4d739d5f79e79c11dcd8519
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-03-09 09:14:34 +0000

    Update Brexit subscriber list criteria
    [We removed an option from the checker](https://github.com/alphagov/finder-frontend/pull/2447)
    which means that a criterion will never get a match for notifications.
    
    Fortunately, this (`personal-eu-org`) was an umbrella criterion for:
      - personal-eu-org-process
      - personal-eu-org-use
      - personal-eu-org-provide
    
    This migration updates the subscriber lists that have the deprecated criterion
    and replaces that specific one with the other three.
    
    https://trello.com/c/NSgSLzOS/1341-review-wording-of-data-sharing-question-in-the-checker

---
+++ db/migrate/20210309090225_update_brexit_data_subscriber_list_criteria.rb
@@ -0,0 +1,23 @@
+class UpdateBrexitDataSubscriberListCriteria < ActiveRecord::Migration[6.1]
+  CSV_FILE = Rails.root.join("db/migrate/data/subscriber-list-criteria-2021-03-08.csv").freeze
+
+  def up
+    update_criteria("new_criteria")
+  end
+
+  def down
+    update_criteria("matching_criteria")
+  end
+
+private
+
+  def update_criteria(criteria_field_name)
+    CSV.foreach(CSV_FILE, headers: true) do |row|
+      criteria = JSON.parse(row[criteria_field_name]).to_h
+      tags = criteria["tags"]
+
+      subscriber_list = SubscriberList.find_by(slug: row["slug"])
+      subscriber_list.update!(tags: tags) if subscriber_list
+    end
+  end
+end
