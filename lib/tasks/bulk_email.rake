fbec8c19c36af9f35b93fda833fb9a30363d1856
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-11-08 21:50:40 +0000

    Allow bulk email to only send to specified addresses
    This allows us to send a specific bulk email to members of a subscriber list
    but only if their email address is in the file in config/bulk_email/email_addresses.txt.
    
    This is to allow us to segment the email that informs users of the retirement
    of the Brexit checker into those people with an account and those without.
    
    The spec file is 99% a copy and paste of bulk_subscriber_list_email_builder_spec.rb
    as the funtionality is basically identical apart from the inverse operation
    of the `accept_required_subscribers` method. I tried it with shared examples,
    but found it less readable.
    
    Also, I'm going to revert these commits once this has been sent. We may need it
    again, but I'd prefer a better solution to exist in future.

--- lib/tasks/bulk_email.rake
+++ lib/tasks/bulk_email.rake
@@ -12,4 +12,18 @@ namespace :bulk_email do
     end
     puts "Sending #{email_ids.count} emails to subscribers on the following lists: #{subscriber_lists.pluck(:slug).join(', ')}"
   end
+
+  desc "Send a bulk email to many subscriber lists but only if the subscriber's email addresses are in config/bulk_email/email_addresses.txt."
+  task :for_lists_and_explicitly_including_addresses, [] => :environment do |_t, args|
+    subscriber_lists = SubscriberList.where(id: args.extras)
+    email_ids = BulkSubscriberListEmailBuilderWithAccount.call(
+      subject: ENV.fetch("SUBJECT"),
+      body: ENV.fetch("BODY"),
+      subscriber_lists: subscriber_lists,
+    )
+    email_ids.each do |id|
+      SendEmailWorker.perform_async_in_queue(id, queue: :send_email_immediate)
+    end
+    puts "Sending #{email_ids.count} emails to subscribers on the following lists: #{subscriber_lists.pluck(:slug).join(', ')}"
+  end
 end
---
+++ spec/builders/bulk_subscriber_list_email_builder_with_account_spec.rb
@@ -0,0 +1,87 @@
+RSpec.describe BulkSubscriberListEmailBuilderWithAccount do
+  describe ".call" do
+    let(:subscriber) { create(:subscriber, address: "test@example.com") }
+
+    let(:subscriber_lists) do
+      [create(:subscriber_list, title: "My List"), create(:subscriber_list)]
+    end
+
+    let(:email) do
+      email_ids = described_class.call(
+        subject: "email subject",
+        body: "email body",
+        subscriber_lists: subscriber_lists,
+      )
+
+      Email.find(email_ids).first
+    end
+
+    before do
+      allow(BulkEmailBodyPresenter).to receive(:call)
+        .with("email body", subscriber_lists.first)
+        .and_return("presented body")
+
+      allow(FooterPresenter).to receive(:call)
+        .with(subscriber, subscription)
+        .and_return("presented_footer")
+    end
+
+    context "with one subscription" do
+      let(:subscription) do
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.first)
+      end
+
+      it "creates an email" do
+        expect(email.subject).to eq("email subject")
+
+        expect(email.body).to eq <<~BODY
+          presented body
+
+          ---
+
+          presented_footer
+        BODY
+      end
+    end
+
+    context "with an ended subscription" do
+      let(:subscription) do
+        create(:subscription, :ended, subscriber_list: subscriber_lists.first)
+      end
+
+      it "creates no emails" do
+        expect(email).to be_nil
+      end
+    end
+
+    context "with many subscriptions" do
+      let(:subscription) do
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.first, created_at: 1.hour.ago)
+      end
+
+      before do
+        create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.second, created_at: 2.days.ago)
+      end
+
+      it "should only create one email per subscriber" do
+        expect(email).to be_present
+        expect(Email.count).to eq(1)
+      end
+    end
+
+    context "with rejected subscribers in /config/accounts/email_addresses.txt" do
+      let(:excluded_subscriber) { create(:subscriber) }
+      let(:subscription) { create(:subscription, subscriber: subscriber, subscriber_list: subscriber_lists.first) }
+
+      before do
+        create(:subscription, subscriber: excluded_subscriber, subscriber_list: subscriber_lists.first)
+      end
+
+      it "should only create emails for subscribers not in the file" do
+        expect(email).to be_present
+        expect(Email.count).to eq(1)
+        expect(Email.first.address).to eq("test@example.com")
+      end
+    end
+  end
+end
