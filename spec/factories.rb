80d1eedbb43a0bd5f541d9a5e87d983ef4b08d0d
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-12-01 15:11:22 +0000

    Add task unsubscribing all from Brexit checker
    The Brexit checker has been retired and will not be sending out any further
    updates. Because of these, we're closing all the subscriptions.
    
    Once we're totally happy, then we'll delete them altogether, but we're not
    quite there yet.

--- spec/factories.rb
+++ spec/factories.rb
@@ -111,6 +111,10 @@ FactoryBot.define do
       tags { { format: %w[medical_safety_alert], alert_type: %w[devices drugs field-safety-notices company-led-drugs] } }
     end
 
+    trait :brexit_checker do
+      tags { { brexit_checklist_criteria: { any: %w[old other] } } }
+    end
+
     trait :with_content_id do
       content_id { SecureRandom.uuid }
     end
@@ -138,6 +142,10 @@ FactoryBot.define do
     subscriber
     subscriber_list
     frequency { Frequency::IMMEDIATELY }
+
+    trait :brexit_checker do
+      subscriber_list { build(:subscriber_list, :brexit_checker) }
+    end
 
     trait :immediately
 
