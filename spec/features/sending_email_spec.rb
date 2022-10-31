f8e4ee36aff0d5bfefea9443944c9e5d6e6d658b
kashifatcha <kashif.atcha@digital.cabinet-office.gov.uk>
2022-10-31 11:03:13 +0000

    removed messages_controller code and tests

--- spec/features/sending_email_spec.rb
+++ spec/features/sending_email_spec.rb
@@ -19,26 +19,4 @@ RSpec.describe "Sending an email", type: :request do
     expect(body).to include("[Unsubscribe](http://www.dev.gov.uk/email/unsubscribe")
     expect(body).to include("gov.uk/email/manage/authenticate")
   end
-
-  # scenario "sending an email for a message" do
-  #   login_with_internal_app
-
-  #   subscriber_list = create_subscriber_list(
-  #     tags: { brexit_checklist_criteria: { any: %w[eu-national] } },
-  #   )
-  #   subscribe_to_subscriber_list(subscriber_list[:id])
-  #   create_message(
-  #     criteria_rules: [{ type: "tag", key: "brexit_checklist_criteria", value: "eu-national" }],
-  #   )
-
-  #   email_data = expect_an_email_was_sent(
-  #     subject: "Update from GOV.UK for: Title",
-  #     address: "test@test.com",
-  #   )
-
-  #   body = email_data.dig(:personalisation, :body)
-  #   expect(body).to include("Body")
-  #   expect(body).to include("[Unsubscribe](http://www.dev.gov.uk/email/unsubscribe")
-  #   expect(body).to include("gov.uk/email/manage/authenticate")
-  # end
 end
