766800db40ca4b3fb0848d1264a58f548a71f104
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2023-01-11 17:13:15 +0000

    Update FindExactQuery
    This function matches subscriber list params from email alert frontend
    to existing subscriber lists.
    
    We have removed content_id from the FindWithoutLinksAndTagsAndContentId in
    previous commit, which is also used here. So these changes are required to
    retain matching when list params contain content id without links.

--- app/queries/find_exact_query.rb
+++ app/queries/find_exact_query.rb
@@ -22,36 +22,45 @@ class FindExactQuery
   def exact_match
     return find_exact(:links, @links) if @links.any?
     return find_exact(:tags, @tags) if @tags.any?
+    return find_exact(:content_id, @content_id) if @content_id.presence
 
-    FindWithoutLinksAndTags.new(scope: base_scope).call.first
+    FindWithoutLinksAndTagsAndContentId.new(scope: document_type_scope).call.first
   end
 
 private
 
   def base_scope
     @base_scope ||= begin
-      scope = SubscriberList
+      scope = document_type_scope
         .where(content_id: @content_id)
-        .where(document_type: @document_type)
-        .where(email_document_supertype: @email_document_supertype)
-        .where(government_document_supertype: @government_document_supertype)
       scope = scope.where(slug: @slug) if @slug.present?
       scope
     end
   end
 
+  def document_type_scope
+    SubscriberList
+      .where(document_type: @document_type)
+      .where(email_document_supertype: @email_document_supertype)
+      .where(government_document_supertype: @government_document_supertype)
+  end
+
   def find_exact(query_field, query)
-    raise ArgumentError, "query_field must be `:tags` or `:links`" unless %i[tags links].include?(query_field)
+    raise ArgumentError, "query_field must be `:tags` or `:links` or `:content_id`" unless %i[tags links content_id].include?(query_field)
 
     return if query.blank?
 
-    digest = HashDigest.new(query).generate
-
     case query_field
     when :tags
-      base_scope.find_by_tags_digest(digest)
+      base_scope.find_by_tags_digest(hash_digest(query))
     when :links
-      base_scope.find_by_links_digest(digest)
+      base_scope.find_by_links_digest(hash_digest(query))
+    when :content_id
+      base_scope.first
     end
+  end
+
+  def hash_digest(query)
+    HashDigest.new(query).generate
   end
 end
