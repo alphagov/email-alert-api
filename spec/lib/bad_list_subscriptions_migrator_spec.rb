34b18755686956a001e606b3b9656c6e04a1734f
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2023-06-08 10:31:19 +0100

    BadListSubscriptionsMigrator class
    In April 2023 a bug was detected in email alert frontend [1], that had resulted
    in users who should have been signed up to a links based subscriber list (eg on an organisation page,
    or topic page), being signed up to a content id based subscriberlist instead.
    
    These 'bad' subscriptions have a content id with no links or tags, for content types that are supposed
    to be links based. Which are these ones [2]
    
    This (temporary) class can be called to migrate users from a bad list to the equivalent good list for
    specialist topic email lists only. The task might be safe to run for all prefixes in ref [2] but some
    checks should be done first to make sure there are no strange edge cases.
    
    Implementation details
    
    - Only migrate users to lists that are active:
    When a specialist topic is archived, all of its email subscribers are bulk unsubscribed [3],
    leaving the list with an active_subscription_count of 0. It is possible that in the time since
    a bad lists was created for a specialist topic, the page itself has been archived.
    There is no point migrating users to these lists as they won't recieve any updates.
    
    - Don't call the SubscriberListMover if the bad list has no active subscribers:
    When the bad lists were created, an email was sent to users asking them to unsubscribe
    and resubscribe. So it's possible that there are some bad lists without subscribers. So
    let's skip those.
    
    [1] https://github.com/alphagov/email-alert-frontend/pull/1522
    [2] https://github.com/alphagov/email-alert-frontend/blob/main/app/services/subscriber_list_params/generate_links_based_list_params_service.rb#L22-L43
    [3] https://github.com/alphagov/collections-publisher/blob/24a7108e588f419892b52c4a8fa4584ea3c27496/app/services/tag_archiver.rb#L61

---
+++ spec/lib/bad_list_subscriptions_migrator_spec.rb
@@ -0,0 +1,127 @@
+RSpec.describe BadListSubscriptionsMigrator do
+  describe "#process_all_lists" do
+    let(:prefix) { "topic" }
+
+    let(:child_benefit_base_params) do
+      {
+        title: "Child Benefit",
+        url: "/topic/benefits-credits/child-benefit",
+      }
+    end
+
+    let(:tax_credit_base_params) do
+      {
+        title: "Tax credits",
+        url: "/topic/benefits-credits/tax-credits",
+      }
+    end
+
+    let!(:child_benefit_good_list) do
+      SubscriberList.create!(
+        child_benefit_base_params.merge(
+          slug: "tax-credits-and-child-benefit-child-benefit",
+          links: { "topics" => { "any" => %w[cc9eb8ab-7701-43a7-a66d-bdc5046224c0] } },
+          content_id: nil,
+        ),
+      )
+    end
+
+    let!(:child_benefit_bad_list) do
+      SubscriberList.create!(
+        child_benefit_base_params.merge(
+          slug: "child-benefit-f71e6de312",
+          links: {},
+          content_id: "cc9eb8ab-7701-43a7-a66d-bdc5046224c0",
+        ),
+      )
+    end
+
+    let!(:tax_credit_good_list) do
+      SubscriberList.create!(
+        tax_credit_base_params.merge(
+          slug: "tax-credits-and-child-benefit-tax-credits",
+          links: { "topics" => { "any" => %w[f881f972-6094-4c7d-849c-9143461a9307] } },
+          content_id: nil,
+        ),
+      )
+    end
+
+    let!(:tax_credit_bad_list) do
+      SubscriberList.create!(
+        tax_credit_base_params.merge(
+          slug: "tax-credits-c845c124bb",
+          links: {},
+          content_id: "f881f972-6094-4c7d-849c-9143461a9307",
+        ),
+      )
+    end
+
+    it "can only be called with valid prefix arguments" do
+      prefix = "foo"
+      remover = described_class.new(prefix)
+      message = "Subscription migration not possible for the provided prefix"
+      expect { remover.process_all_lists }.to raise_error(message)
+    end
+
+    context "when the destination subscriber list has active subscriptions" do
+      before do
+        create(:subscription, subscriber_list: tax_credit_good_list)
+        create(:subscription, subscriber_list: child_benefit_good_list)
+        create(:subscription, subscriber_list: tax_credit_bad_list)
+        create(:subscription, subscriber_list: child_benefit_bad_list)
+      end
+
+      it "calls the SubscriberListMover" do
+        remover = described_class.new(prefix)
+        list_mover_double = double("SubscriberListMover")
+        expect(list_mover_double).to receive(:call).twice
+        expect(tax_credit_good_list.active_subscriptions_count).to be 1
+        expect(child_benefit_good_list.active_subscriptions_count).to be 1
+
+        allow(SubscriberListMover)
+          .to receive(:new)
+          .with(from_slug: "tax-credits-c845c124bb", to_slug: "tax-credits-and-child-benefit-tax-credits")
+          .and_return(list_mover_double)
+
+        allow(SubscriberListMover)
+          .to receive(:new)
+          .with(from_slug: "child-benefit-f71e6de312", to_slug: "tax-credits-and-child-benefit-child-benefit")
+          .and_return(list_mover_double)
+
+        remover.process_all_lists
+      end
+    end
+
+    context "when the destination subscriber list has no active subscriptions" do
+      it "does not call the SubscriberListMover" do
+        remover = described_class.new(prefix)
+        list_mover_double = double("SubscriberListMover")
+
+        expect(tax_credit_good_list.active_subscriptions_count).to be 0
+        expect(child_benefit_good_list.active_subscriptions_count).to be 0
+        expect(list_mover_double).not_to receive(:call)
+
+        remover.process_all_lists
+      end
+    end
+
+    context "when the bad subscriber_list has no active subscriptions" do
+      before do
+        create(:subscription, subscriber_list: tax_credit_good_list)
+        create(:subscription, subscriber_list: child_benefit_good_list)
+      end
+
+      it "does not call the SubscriberListMover" do
+        remover = described_class.new(prefix)
+        list_mover_double = double("SubscriberListMover")
+
+        expect(list_mover_double).not_to receive(:call)
+
+        remover.process_all_lists
+
+        expect(tax_credit_good_list.active_subscriptions_count).to be 1
+        expect(child_benefit_good_list.active_subscriptions_count).to be 1
+      end
+    end
+  end
+end
