a3e0c499943dec110f16b0583cc91b73253f3efb
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2023-09-26 09:15:35 +0100

    Temporary Rake task
    On 22nd Sept a specialist topic page was archived and redirected to the wrong
    document collection page. The logic in collections publisher with automates
    the migration or unsubscription of emails subs therefore didn't not behave as
    desired. In other words subscribers were bulk unsubscribed instead of bulk
    migrated. The decision has been taken to email these users with a link to the
    redirected page, and give them the opportunity to resubscribe to the correct page
    themselves.
    
    list = SubscriberList.find_by(slug: "intellectual-property-trade-marks")
    list.active_subscriptions_count = 0
    list.subscriptions.where(ended_reason: "bulk_unsubscribed").count = 456
    list.subscriptions.where(ended_reason: "bulk_unsubscribed").pluck(:ended_at).uniq
    => [Fri, 22 Sep 2023 08:54:15.168387000 BST +01:00]

--- spec/lib/tasks/bulk_email_spec.rb
+++ spec/lib/tasks/bulk_email_spec.rb
@@ -52,4 +52,55 @@ RSpec.describe "bulk_email" do
         ).to_stdout
     end
   end
+
+  describe "for_users_unsubscribed_in_error" do
+    before do
+      Rake::Task["bulk_email:for_users_unsubscribed_in_error"].reenable
+    end
+
+    it "builds emails for a the trade marks subscriber list" do
+      trade_marks_subscriber_list = create(:subscriber_list, title: "Trade marks", slug: "intellectual-property-trade-marks")
+
+      expect(BulkSubscriberListEmailBuilderForFilteredSubscriptions)
+        .to receive(:new)
+        .with(trade_marks_subscriber_list)
+        .and_call_original
+
+      Rake::Task["bulk_email:for_users_unsubscribed_in_error"].invoke(trade_marks_subscriber_list)
+    end
+
+    it "enqueues the emails for delivery" do
+      trade_marks_subscriber_list = create(:subscriber_list, title: "Trade marks", slug: "intellectual-property-trade-marks")
+      builder_double = instance_double(BulkSubscriberListEmailBuilderForFilteredSubscriptions)
+      allow(BulkSubscriberListEmailBuilderForFilteredSubscriptions).to receive(:new).and_return(builder_double)
+
+      allow(builder_double).to receive(:call)
+        .and_return([1, 2])
+
+      expect(SendEmailWorker)
+        .to receive(:perform_async_in_queue)
+        .with(1, queue: :send_email_immediate)
+
+      expect(SendEmailWorker)
+        .to receive(:perform_async_in_queue)
+        .with(2, queue: :send_email_immediate)
+
+      Rake::Task["bulk_email:for_users_unsubscribed_in_error"].invoke(trade_marks_subscriber_list)
+    end
+
+    it "states how many emails are being sent, and to where" do
+      trade_marks_subscriber_list = create(:subscriber_list, title: "Trade marks", slug: "intellectual-property-trade-marks")
+
+      builder_double = instance_double(BulkSubscriberListEmailBuilderForFilteredSubscriptions)
+      allow(BulkSubscriberListEmailBuilderForFilteredSubscriptions).to receive(:new).and_return(builder_double)
+
+      allow(builder_double).to receive(:call)
+        .and_return([1, 2, 3, 4, 5])
+
+      expect { Rake::Task["bulk_email:for_users_unsubscribed_in_error"].invoke(trade_marks_subscriber_list) }
+      .to output(
+        /Sending 5 emails to subscribers on subscriber list: Trade marks/,
+      ).to_stdout
+    end
+  end
 end
