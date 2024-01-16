8b35f98ca16482869f95aba75e2b41354a22df21
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-01-16 12:02:51 +0000

    Remove unused welcome route and controller

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    message = "Welcome to Email Alert API. For source code and documentation"\
-      " please visit: https://github.com/alphagov/email-alert-api"
-    render json: { message: }
-  end
-end
