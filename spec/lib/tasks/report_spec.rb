d2549ab1a847d0cc0c00c2a0a28d6221a8f5167f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-23 15:50:34 +0100

    improve coverage

--- spec/lib/tasks/report_spec.rb
+++ spec/lib/tasks/report_spec.rb
@@ -1,4 +1,9 @@
+require "gds_api/test_helpers/content_store"
+
 RSpec.describe "report" do
+  include ContentItemHelpers
+  include GdsApi::TestHelpers::ContentStore
+
   describe "matched_content_changes" do
     it "outputs a CSV of matched content changes" do
       expect { Rake::Task["report:matched_content_changes"].invoke }
@@ -30,6 +35,53 @@ RSpec.describe "report" do
   describe "single_page_notifications_top_subscriber_lists" do
     it "outputs a report of single page notification subscriber lists" do
       expect { Rake::Task["report:single_page_notifications_top_subscriber_lists"].invoke }
+        .to output.to_stdout
+    end
+  end
+
+  describe "historical_content_change_statistics" do
+    it "outputs a report of people notified for recent content change" do
+      expect { Rake::Task["report:historical_content_change_statistics"].invoke("/url") }
+        .to output.to_stdout
+    end
+  end
+
+  describe "future_content_change_statistics" do
+    context "with a valid content item" do
+      before do
+        subscriber_list = create(:subscriber_list, :for_single_page_subscription)
+        # match_by_tags_content_item_for_subscriber_list(subscriber_list:)
+        @path = subscriber_list_path(subscriber_list)
+      end
+
+      it "outputs a report of people who would be notified of a major change" do
+        expect { Rake::Task["report:future_content_change_statistics"].invoke(@path) }
+          .to output(/title 1 \([0-9]+ active subscribers\)/).to_stdout
+      end
+    end
+
+    context "with a content item that wouldn't trigger an alert" do
+      before do
+        subscriber_list = create(:subscriber_list, :for_single_page_subscription)
+        match_by_tags_non_triggering_content_item_for_subscriber_list(subscriber_list:)
+        @path_2 = subscriber_list_path(subscriber_list)
+      end
+
+      it "outputs a report of people who would be notified of a major change" do
+        expect { Rake::Task["report:future_content_change_statistics"].invoke(@path_2) }
+          .to output(/would not trigger an email alert/).to_stdout
+      end
+    end
+  end
+
+  describe "finder_statistics" do
+    before do
+      content_item = content_item_for_base_path("/url")
+      stub_content_store_has_item("/url", content_item)
+    end
+
+    it "outputs a report of people who are subscribed to lists created from a finder" do
+      expect { Rake::Task["report:finder_statistics"].invoke("/url") }
         .to output.to_stdout
     end
   end
