65d696c9719d540e1b34b99734f6fc68e1e38dc7
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2024-05-10 12:59:25 +0100

    Use the BulkMigrateListWorker
    Consequences of this change:
    - an internal confirmation email will now be sent when a bulk migration is run
    - subscriber lists will be ended with the reason "bulk_migrated" instead of "subscriber_list_changed"

--- spec/lib/subscriber_list_mover_spec.rb
+++ spec/lib/subscriber_list_mover_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe SubscriberListMover do
   let(:list_1) { create :subscriber_list }
   let(:list_2) { create :subscriber_list }
+  let(:email) { create :email }
 
   before do
     list_1.subscriptions << create_list(:subscription, 2)
@@ -8,6 +9,12 @@ RSpec.describe SubscriberListMover do
 
     allow($stdout).to receive(:puts)
     allow(SendEmailWorker).to receive(:perform_async_in_queue)
+  end
+
+  around(:each) do |example|
+    ClimateControl.modify(BULK_MIGRATE_CONFIRMATION_EMAIL_ACCOUNT: "test@test.uk") do
+      example.run
+    end
   end
 
   describe "#with_send_email_false" do
@@ -36,14 +43,32 @@ RSpec.describe SubscriberListMover do
       expect(list_2.subscriptions.count).to eq 3
     end
 
-    it "ends from_slug subscriptions with reason: subscriber_list_changed" do
+    it "ends from_slug subscriptions with reason: bulk_migrated" do
       expect(list_1.subscriptions.count).to eq 2
       expect(list_1.subscriptions[1].ended_reason).to eq nil
 
       described_class.new(from_slug: list_1.slug, to_slug: list_2.slug).call
 
       expect(list_1.subscriptions.count).to eq 2
-      expect(list_1.subscriptions[1].reload.ended_reason).to eq "subscriber_list_changed"
+      expect(list_1.subscriptions[1].reload.ended_reason).to eq "bulk_migrated"
+    end
+
+    it "sends a confirmation email to an internal mailbox" do
+      allow(BulkMigrateConfirmationEmailBuilder).to receive(:call).and_return(email)
+      source_subscriber_list = SubscriberList.find_by(slug: list_1.slug)
+      destination_subscriber_list = SubscriberList.find_by(slug: list_2.slug)
+
+      expect(BulkMigrateConfirmationEmailBuilder)
+      .to receive(:call)
+      .with(source_id: source_subscriber_list.id,
+            destination_id: destination_subscriber_list.id,
+            count: 2)
+
+      expect(SendEmailWorker)
+        .to receive(:perform_async_in_queue)
+        .with(email.id, queue: :send_email_transactional)
+
+      described_class.new(from_slug: list_1.slug, to_slug: list_2.slug).call
     end
   end
 
