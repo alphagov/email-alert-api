4dd30cbd231f955536486a786554adaa02e0c6d3
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-11-19 17:01:33 +0000

    Add Prometheus metrics

---
+++ spec/lib/prometheus_metrics_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe PrometheusMetrics do
+  describe ".register" do
+    it "registers the prometheus metrics" do
+      described_class::GAUGES.each do |gauge|
+        expect(PrometheusExporter::Client.default)
+          .to receive(:register)
+          .with(:gauge, "#{described_class::PREFIX}#{gauge[:name]}", gauge[:description])
+      end
+
+      described_class.register
+    end
+  end
+
+  describe ".name_with_prefix" do
+    it "prefixes the metric name with 'email_alert_api_'" do
+      expect(described_class.name_with_prefix("total_unprocessed_content_changes"))
+        .to eq("email_alert_api_total_unprocessed_content_changes")
+    end
+  end
+
+  describe ".gauge" do
+    let(:metric) { instance_double(PrometheusExporter::Client::RemoteMetric) }
+
+    before do
+      allow(metric).to receive(:observe)
+    end
+
+    it "updates the gauge if a gauge with that name exists" do
+      allow(PrometheusExporter::Client.default).to receive(:find_registered_metric).and_return(metric)
+
+      described_class.gauge("total_unprocessed_content_changes", 1)
+
+      expect(metric).to have_received(:observe).with(1)
+    end
+  end
+end
